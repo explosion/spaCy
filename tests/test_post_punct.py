9bef797afed174ffbec240ec456275a935a9f445
Matthew Honnibal <matthew@honnibal.com>
2014-07-06 20:02:00 +0200

    * Rejigged tests. Working possessives, but no other contractions

--- tests/test_post_punct.py
+++ tests/test_post_punct.py
@@ -18,11 +18,10 @@ def test_close(close_puncts):
     for p in close_puncts:
         string = word_str + p
         token = lookup(string)
-        assert unhash(lex_of(token)) == word_str
         tokens = expand_chunk(token)
         assert len(tokens) == 2
-        assert unhash(lex_of(tokens[0])) == word_str
         assert unhash(lex_of(tokens[1])) == p
+        assert unhash(lex_of(tokens[0])) == word_str
 
 
 def test_two_different_close(close_puncts):
@@ -43,6 +42,6 @@ def test_three_same_close(close_puncts):
     for p in close_puncts:
         string = word_str + p + p + p
         tokens = expand_chunk(lookup(string))
-        assert len(tokens) == 2
+        assert len(tokens) == 4
         assert unhash(lex_of(tokens[0])) == word_str
-        assert unhash(lex_of(tokens[1])) == p + p + p
+        assert unhash(lex_of(tokens[1])) == p
