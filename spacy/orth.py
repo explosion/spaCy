f685218e2129db11844b698007edb8d7567a3dfe
Matthew Honnibal <honnibal@gmail.com>
2014-11-01 17:39:34 +1100

    * Add is_urlish function

--- spacy/orth.py
+++ spacy/orth.py
@@ -49,6 +49,46 @@ def is_lower(string):
 def is_upper(string):
     return string.isupper()
 
+TLDs = set("com|org|edu|gov|net|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|"
+        "name|pro|tel|travel|xxx|"
+        "ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|"
+        "bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|"
+        "co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|"
+        "fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|"
+        "hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|"
+        "km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|"
+        "mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|"
+        "nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|"
+        "sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|ss|st|su|sv|sy|sz|tc|td|tf|tg|th|"
+        "tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|"
+        "wf|ws|ye|yt|za|zm|zw".split('|'))
+
+def is_urlish(string):
+    # We're looking for things that function in text like URLs. So, valid URL
+    # or not, anything they say http:// is going to be good.
+    if string.startswith('http://'):
+        return True
+    elif string.startswith('www.') and len(string) >= 5:
+        return True
+    # No dots? Not URLish enough
+    if string[0] == '.' or string[-1] == '.' or '.' not in string:
+        return False
+    tld = string.rsplit('.', 1)[1].split(':', 1)[0]
+    if tld.endswith('/'):
+        return True
+
+    if tld.isalpha() and tld in TLDs:
+        return True
+    return False
+
+
+def is_number(string):
+    pass
+
+
+def is_emoticon(string):
+    pass
+
 
 # Statistics features
 def oft_case(name, thresh):
@@ -110,4 +150,6 @@ def non_sparse(string, prob, cluster, upper_pc, title_pc, lower_pc):
 
 def asciied(string):
     ascii_string = unidecode(string)
+    if not ascii_string:
+        return '???'
     return ascii_string.decode('ascii')
