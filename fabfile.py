edf739134c94fa6a7215c6f07b931aed0d52ba41
Matthew Honnibal <honnibal@gmail.com>
2014-11-05 20:46:29 +1100

    * Make make quiet by default, and add a vmake option for verbose make

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,11 @@ import json
 from fabric.api import local, run, lcd, cd, env
 
 def make():
+    local('python setup.py build_ext --inplace > /dev/null 2> /tmp/err')
+
+def vmake():
     local('python setup.py build_ext --inplace')
+
 
 
 def clean():
@@ -23,3 +27,15 @@ def sbox():
 def sbclean():
     local('python sb_setup.py clean --all')
 
+
+def pos():
+    local('python tools/train.py pos ~/work_data/docparse/wsj02-21.conll data/en/pos')
+    local('python tools/tag.py ~/work_data/docparse/wsj22.raw /tmp/tmp')
+    local('python tools/eval_pos.py ~/work_data/docparse/wsj22.conll /tmp/tmp')
+
+
+def ner():
+    local('rm -rf data/en/ner')
+    local('python tools/train.py ner ~/work_data/ner/muc7.train data/en/ner')
+    local('python tools/tag.py ~/work_data/ner/muc7.raw /tmp/ner')
+    local('python tools/eval_ner.py ~/work_data/ner/muc7.dev /tmp/ner')
