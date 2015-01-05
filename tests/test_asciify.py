64f33a8705e56890fe3937f14d87b46f17aa9dc1
Matthew Honnibal <honnibal@gmail.com>
2015-01-06 01:03:29 +1100

    * Upd asciify test, fixing type error

--- tests/test_asciify.py
+++ tests/test_asciify.py
@@ -8,11 +8,11 @@ from spacy.orth import asciied
 
 def test_tilde():
     string = u'hõmbre'
-    assert asciied(string) == u'hombre'
+    assert asciied(string) == b'hombre'
 
 
 def test_smart_quote():
     string = u'“'
-    assert asciied(string) == '"'
-    string = u'”'
-    assert asciied(string) == '"'
+    assert asciied(string) == b'"'
+    string = b'”'
+    assert asciied(string) == b'"'
