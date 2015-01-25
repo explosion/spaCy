5049d4c2e622020e965466431ec810f82a01289d
Matthew Honnibal <honnibal@gmail.com>
2015-01-25 16:32:26 +1100

    * Add parts_of_speech.pyx

---
+++ spacy/parts_of_speech.pyx
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+
+UNIV_POS_NAMES = {
+    "NO_TAG": NO_TAG,
+    "ADJ": ADJ,
+    "ADV": ADV,
+    "ADP": ADP,
+    "CONJ": CONJ,
+    "DET": DET,
+    "NOUN": NOUN,
+    "NUM": NUM,
+    "PRON": PRON,
+    "PRT": PRT,
+    "VERB": VERB,
+    "X": X,
+    "PUNCT": PUNCT,
+    "EOL": EOL
+}
