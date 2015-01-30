48b98e3fb53a863d22d67ecbb6b13ec9c9337d99
Matthew Honnibal <honnibal@gmail.com>
2015-01-30 16:45:11 +1100

    * Add test for tag names

---
+++ tests/test_tag_names.py
@@ -0,0 +1,11 @@
+from spacy.en import English
+
+def test_tag_names():
+    nlp = English()
+    tokens = nlp(u'I ate pizzas with anchovies.', parse=True, tag=True)
+    pizza = tokens[2]
+    assert type(pizza.pos) == int
+    assert type(pizza.pos_) == unicode
+    assert type(pizza.dep) == int
+    assert type(pizza.dep_) == unicode
+    assert pizza.tag_ == u'NNS'
