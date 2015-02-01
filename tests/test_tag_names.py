1f317740784fcf5bfc7b2fcc992fbad6679aff63
Matthew Honnibal <honnibal@gmail.com>
2015-02-01 13:14:20 +1100

    * Python3 fix for test_tag_names

--- tests/test_tag_names.py
+++ tests/test_tag_names.py
@@ -5,7 +5,7 @@ def test_tag_names():
     tokens = nlp(u'I ate pizzas with anchovies.', parse=True, tag=True)
     pizza = tokens[2]
     assert type(pizza.pos) == int
-    assert type(pizza.pos_) == unicode
+    assert type(pizza.pos_) == str
     assert type(pizza.dep) == int
-    assert type(pizza.dep_) == unicode
+    assert type(pizza.dep_) == str
     assert pizza.tag_ == u'NNS'
