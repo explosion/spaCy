5ff2b5c8f09f224890014aec83ffc2aa2d7d1d18
Matthew Honnibal <honnibal@gmail.com>
2015-02-10 10:16:09 -0500

    * Inc version

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def run_setup(exts):
         description="Industrial-strength NLP",
         author='Matthew Honnibal',
         author_email='honnibal@gmail.com',
-        version='0.60',
+        version='0.61',
         url="http://honnibal.github.io/spaCy/",
         package_data={"spacy": ["*.pxd"],
                       "spacy.en": ["*.pxd", "data/pos/*",
