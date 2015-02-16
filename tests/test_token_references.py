9dbc31d72c256a6a581ae883a9781812cc30280d
Matthew Honnibal <honnibal@gmail.com>
2015-02-16 11:49:31 -0500

    * Add test from NSchrading

--- tests/test_token_references.py
+++ tests/test_token_references.py
@@ -22,3 +22,26 @@ def test_orphan():
     assert orphan.orth_ == 'orphan'
     assert orphan.pos_ == 'ADJ'
     assert orphan.head.orth_ == 'token'
+
+
+def _orphan_from_list(toks):
+    ''' Take the tokens from nlp(), append them to a list, return the list '''
+    lst = []
+    for tok in toks:
+        lst.append(tok)
+    return lst
+
+def test_list_orphans():
+    # Test case from NSchrading
+    nlp = English()
+    samples = ["a", "test blah wat okay"]
+    lst = []
+    for sample in samples:
+        # Go through all the samples, call nlp() on each to get tokens,
+        # pass those tokens to the _orphan_from_list() function, get a list back
+        # and put all results in another list
+        lst.extend(_orphan_from_list(nlp(sample)))
+    # go through the list of all tokens and try to print orth_
+    orths = ['a', 'test', 'blah', 'wat', 'okay']
+    for i, l in enumerate(lst):
+        assert l.orth_  == orths[i]
