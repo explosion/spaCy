231ce2dae5b110087085f3d9353bac4ed287215f
Matthew Honnibal <honnibal@gmail.com>
2015-03-20 01:16:12 +0100

    * Assign ROOT label by default. May be papering over another bug.

--- spacy/tokens.pyx
+++ spacy/tokens.pyx
@@ -249,6 +249,8 @@ cdef class Tokens:
         self.is_parsed = True
         for i in range(self.length):
             self.data[i] = parsed[i]
+            if self.data[i].dep == 0:
+                self.data[i].dep = self.vocab.strings['ROOT']
 
 
 cdef class Span:
