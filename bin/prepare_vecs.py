bb279793524bd1c20a8dd0a6871e6dc5e4aa4944
Matthew Honnibal <honnibal@gmail.com>
2015-04-02 06:19:39 +0200

    * Add prepare_vecs script

---
+++ bin/prepare_vecs.py
@@ -0,0 +1,15 @@
+"""Read a vector file, and prepare it as binary data, for easy consumption"""
+
+import bz2
+import plac
+import struct
+
+from spacy.vocab import write_binary_vectors
+
+
+def main(in_loc, out_loc):
+    write_binary_vectors(in_loc, out_loc)
+    
+
+if __name__ == '__main__':
+    plac.call(main)
