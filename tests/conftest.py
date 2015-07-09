f95da0bd52cee922db7546e5a6c531aae22f71ba
Matthew Honnibal <honnibal@gmail.com>
2015-07-09 12:18:02 +0200

    * Allow tests to read model dir from SPACY_DATA environment variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
-from spacy.en import English
+from spacy.en import English, LOCAL_DATA_DIR
+import os
 
 
 @pytest.fixture(scope="session")
 def EN():
-    return English()
+    data_dir = os.environ.get('SPACY_DATA', LOCAL_DATA_DIR)
+    return English(data_dir=data_dir)
