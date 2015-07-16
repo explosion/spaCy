d8458d6a2513c906606fdb766e4ac2140bb28688
Matthew Honnibal <honnibal@gmail.com>
2015-07-16 19:55:21 +0200

    * Fix attr_id_t import in Spans

--- spacy/tokens/spans.pyx
+++ spacy/tokens/spans.pyx
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 from collections import defaultdict
 
 from ..structs cimport Morphology, TokenC, LexemeC
-from ..typedefs cimport flags_t, attr_id_t, attr_t
+from ..typedefs cimport flags_t, attr_t
+from ..attrs cimport attr_id_t
 from ..parts_of_speech cimport univ_pos_t
 
 
