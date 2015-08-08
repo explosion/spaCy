c044c0e18eee07ef0a6cbb76dd3debdc0262d292
Matthew Honnibal <honnibal@gmail.com>
2015-08-08 23:38:19 +0200

    * Fix partial actions test

--- tests/parser/test_initial_actions_parse.py
+++ tests/parser/test_initial_actions_parse.py
@@ -4,5 +4,9 @@ import pytest
 def test_initial(EN):
     doc = EN.tokenizer(u'I ate the pizza with anchovies.')
     EN.tagger(doc)
-    EN.parser(doc, initial_actions=['L-nsubj', 'S', 'L-det'])
+    next_actions = EN.parser.partial(doc, actions=['L-nsubj', 'S', 'L-det'])
     assert doc[0].head.i == 1
+    assert doc[1].head.i == 1
+    assert doc[2].head.i == 2
+    assert doc[3].head.i == 2
+    assert doc
