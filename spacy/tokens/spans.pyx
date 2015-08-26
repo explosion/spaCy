d30029979ed7d24cb56ed74f2ec3f2b910550173
Matthew Honnibal <honnibal@gmail.com>
2015-08-26 19:20:46 +0200

    * Avoid import of morphology in spans

--- spacy/tokens/spans.pyx
+++ spacy/tokens/spans.pyx
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from collections import defaultdict
 
-from ..structs cimport Morphology, TokenC, LexemeC
+from ..structs cimport TokenC, LexemeC
 from ..typedefs cimport flags_t, attr_t
 from ..attrs cimport attr_id_t
 from ..parts_of_speech cimport univ_pos_t
