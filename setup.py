0b7d2a6c6206ea5778ef39a473387c2c69aeadf1
Matthew Honnibal <honnibal@gmail.com>
2015-09-13 01:26:29 +0200

    * Inc version

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ def run_setup(exts):
     headers_workaround.install_headers('numpy')
 
 
-VERSION = '0.89'
+VERSION = '0.90'
 def main(modules, is_pypy):
     language = "cpp"
     includes = ['.', path.join(sys.prefix, 'include')]
--- spacy/en/download.py
+++ spacy/en/download.py
@@ -7,7 +7,7 @@ import wget
 import plac
 
 # TODO: Read this from the same source as the setup
-VERSION = '0.8.9'
+VERSION = '0.9.0'
 
 AWS_STORE = 'http://s3-us-west-1.amazonaws.com/media.spacynlp.com'
 
