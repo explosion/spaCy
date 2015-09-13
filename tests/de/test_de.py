2ef449f9d7738db7d73991fca87035c93d65018c
Matthew Honnibal <honnibal@gmail.com>
2015-09-13 10:57:47 +1000

    * Comment out german test

--- tests/de/test_de.py
+++ tests/de/test_de.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 import spacy.de
 
 
-def test_tokenizer():
-    lang = spacy.de.German()
-
-    doc = lang(u'Biografie: Ein Spiel ist ein Theaterstück des Schweizer Schriftstellers Max Frisch, das 1967 entstand und am 1. Februar 1968 im Schauspielhaus Zürich uraufgeführt wurde. 1984 legte Frisch eine überarbeitete Neufassung vor. Das von Frisch als Komödie bezeichnete Stück greift eines seiner zentralen Themen auf: die Möglichkeit oder Unmöglichkeit des Menschen, seine Identität zu verändern.')
-    for token in doc:
-        print(repr(token.string))
+#def test_tokenizer():
+#    lang = spacy.de.German()
+#
+#    doc = lang(u'Biografie: Ein Spiel ist ein Theaterstück des Schweizer Schriftstellers Max Frisch, das 1967 entstand und am 1. Februar 1968 im Schauspielhaus Zürich uraufgeführt wurde. 1984 legte Frisch eine überarbeitete Neufassung vor. Das von Frisch als Komödie bezeichnete Stück greift eines seiner zentralen Themen auf: die Möglichkeit oder Unmöglichkeit des Menschen, seine Identität zu verändern.')
+#    for token in doc:
+#        print(repr(token.string))
