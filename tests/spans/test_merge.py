22a1ad8a5536a012433044ef20a052b913c6558f
Matthew Honnibal <honnibal@gmail.com>
2015-09-15 14:55:23 +1000

    * Upd test_merge

--- tests/spans/test_merge.py
+++ tests/spans/test_merge.py
@@ -30,7 +30,7 @@ def test_merge_heads(EN):
 
 def test_issue_54(EN):
     text = u'Talks given by women had a slightly higher number of questions asked (3.2$\pm$0.2) than talks given by men (2.6$\pm$0.1).'
-    tokens = EN(text, merge_mwes=True)
+    tokens = EN(text)
 
 @pytest.mark.models
 def test_np_merges(EN):
