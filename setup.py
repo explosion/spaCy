af8d0a2a0901bb8faf77f5a74209fa102f261995
Matthew Honnibal <honnibal@gmail.com>
2015-10-09 12:42:41 +0200

    * Increment version

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ def run_setup(exts):
     headers_workaround.install_headers('numpy')
 
 
-VERSION = '0.93'
+VERSION = '0.94'
 def main(modules, is_pypy):
     language = "cpp"
     includes = ['.', path.join(sys.prefix, 'include')]
