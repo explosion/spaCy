5aeb8ac4e0e7ec6550cb69e0438177d5745a76bf
maxirmx <m.samsonov@computer.org>
2015-10-11 20:25:42 +0300

    Added appveyor Python installation script

--- test-Python27.bat
+++ test-Python27.bat
@@ -16,15 +16,17 @@ PATH = D:\Python27\;D:\Python27\Scripts;%PATH%
 
 :run
 
+@echo This shall be fast ...
 python tests\test_basic_create.py
+
+@echo Patience ...
+python tests\test_basic_load.py
 
 rem py.test tests/ -x
 
 rem python -m spacy.en.download
 
-@echo Patience ...
 rem python -c "import spacy.en; nlp = spacy.en.English(); print([w.text for w in nlp(u'I would like to be a dog')])"
-@echo Patience ...
 rem python examples\information_extraction.py
 @echo Patience ...
 rem python examples\matcher_example.py
