ee3f9ba58141ef5e2e1c7d7cd2f13282751221bb
Matthew Honnibal <honnibal@gmail.com>
2015-11-03 19:45:16 +1100

    * Fix test of serializer

--- spacy/tests/serialize/test_packer.py
+++ spacy/tests/serialize/test_packer.py
@@ -6,6 +6,7 @@ import pytest
 import numpy
 
 from spacy.language import Language
+from spacy.en import English
 from spacy.vocab import Vocab
 from spacy.tokens.doc import Doc
 from spacy.tokenizer import Tokenizer
@@ -20,7 +21,7 @@ from spacy.serialize.bits import BitArray
 
 @pytest.fixture
 def vocab():
-    vocab = Language.default_vocab()
+    vocab = English.default_vocab()
     lex = vocab['dog']
     assert vocab[vocab.strings['dog']].orth_ == 'dog'
     lex  = vocab['the']
