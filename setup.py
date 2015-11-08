6dd37c5ee4af36a685cf707bd3772db0724f869e
Matthew Honnibal <honnibal@gmail.com>
2015-11-08 18:09:21 +0100

    * Fix requirement of preshed

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def run_setup(exts):
         package_data=PACKAGE_DATA,
         ext_modules=exts,
         license="MIT",
-        install_requires=['numpy', 'murmurhash', 'cymem == 1.30', 'preshed == 0.43',
+        install_requires=['numpy', 'murmurhash', 'cymem == 1.30', 'preshed == 0.44',
                           'thinc == 4.0.0', "text_unidecode", 'plac', 'six',
                           'ujson', 'cloudpickle'],
         setup_requires=["headers_workaround"],
