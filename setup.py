1e166eb9cd29eb122259da3363ffac8062e5b83e
Matthew Honnibal <honnibal@gmail.com>
2015-11-18 17:42:56 +0100

    * Upgrade spacy version

--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ def run_setup(exts):
     headers_workaround.install_headers('numpy')
 
 
-VERSION = '0.99'
+VERSION = '0.100'
 def main(modules, is_pypy):
     language = "cpp"
     includes = ['.', path.join(sys.prefix, 'include')]
