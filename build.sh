4ab97c477a551de4041f5d90ca0aa7cdbba9b6d3
Henning Peters <pete@dexterslab.de>
2015-12-15 15:09:24 +0100

    get buildbot running

--- build.sh
+++ build.sh
@@ -1,8 +1,35 @@
-#!/bin/bash
+#!/bin/bash -x
+set -e
+
+if [[ $# < 3 ]]; then
+    echo "usage: $0 <python-path> <pip-date> <install-mode>"
+    exit
+fi
+
 if [ ! -d ".build" ]; then
-    virtualenv .build
+    virtualenv .build --python $1
 fi
 . .build/bin/activate
-pip install -U -r requirements.txt
-pip install -U tox
-python -m tox $@
+
+# install
+pip install -U pip
+python pip-date.py $2 pip setuptools wheel six
+pip install -r requirements.txt
+if [[ "$3" == "pip" ]]; then
+  python setup.py sdist;
+  pip install dist/*;
+fi
+if [[ "$3" == "setup-install" ]]; then
+  python setup.py install;
+fi
+if [[ "$3" == "setup-develop" ]]; then
+  python setup.py develop;
+  pip install -e .;
+fi
+pip install pytest
+pip list
+
+# script
+mkdir tmp; cd tmp
+python -m spacy.en.download
+python -m pytest ../spacy/ -x --models --vectors --slow
