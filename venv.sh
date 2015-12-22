e6fc51d6f3ea1db2cec7e23c605cad1514b553d1
Henning Peters <pete@dexterslab.de>
2015-12-22 01:50:58 +0100

    make venv work for mingw

--- venv.sh
+++ venv.sh
@@ -5,7 +5,12 @@ if [ ! -d ".build" ]; then
     virtualenv .build --python $1
 fi
 
-. .build/bin/activate
+if [ -d ".build/bin" ]; then
+    source .build/bin/activate
+elif [ -d ".build/Scripts" ]; then
+    source .build/Scripts/activate
+fi
+
 python build.py prepare $3
 python build.py $2
 python build.py test
