eb7ae61b1c6cafadc55743cfc733c73c73c0fa3f
Henning Peters <pete@dexterslab.de>
2016-03-08 12:59:18 +0100

    cleanup api

--- spacy/__init__.py
+++ spacy/__init__.py
@@ -5,4 +5,4 @@ from .en import English
 def load(name, vectors=None, via=None):
     return English(
         package=util.get_package_by_name(name, via=via),
-        vectors_package=util.get_package_by_name(vectors_name, via=via))
+        vectors_package=util.get_package_by_name(vectors, via=via))
