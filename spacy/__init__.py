8b4677d34d1ca18c9a0c2e18fd05cc8901a20bfc
Matthew Honnibal <honnibal+gh@gmail.com>
2016-04-17 21:31:50 +0200

    * Add missing keyword arguments to spacy.load() function

--- spacy/__init__.py
+++ spacy/__init__.py
@@ -8,8 +8,18 @@ set_lang_class(en.English.lang, en.English)
 set_lang_class(de.German.lang, de.German)
 
 
-def load(name, vectors=None, via=None):
+def load(name, vocab=None, tokenizer=None, parser=None, tagger=None, entity=None,
+         matcher=None, serializer=None, vectors=None, via=None):
     package = get_package_by_name(name, via=via)
     vectors_package = get_package_by_name(vectors, via=via)
     cls = get_lang_class(name)
-    return cls(package=package, vectors_package=vectors_package)
+    return cls(
+        package=package,
+        vectors_package=vectors_package,
+        vocab=vocab,
+        tokenizer=tokenizer,
+        tagger=tagger,
+        parser=parser,
+        entity=entity,
+        matcher=matcher,
+        serializer=serializer)
