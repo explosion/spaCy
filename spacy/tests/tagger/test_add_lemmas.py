12024b0b0a12b0beb3ee1b22f89988bdde2aca95
Wolfgang Seeker <seeker@spacy.io>
2016-04-20 16:40:36 +0200

    bugfix: introducing multiple roots now updates original head's properties adjust tests to rely less on statistical model

--- spacy/tests/tagger/test_add_lemmas.py
+++ spacy/tests/tagger/test_add_lemmas.py
@@ -3,30 +3,10 @@ from __future__ import unicode_literals
 from spacy.en import English
 import pytest
 
-
-
-@pytest.fixture
-def tagged(EN):
-    string = u'Bananas in pyjamas are geese.'
-    tokens = EN(string, tag=True, parse=False)
-    return tokens
-
-
-@pytest.fixture
-def lemmas(tagged):
-    return [t.lemma_ for t in tagged]
-
-
 @pytest.mark.models
-def test_lemmas(lemmas, tagged):
-    assert lemmas[0] == 'banana'
-    assert lemmas[1] == 'in'
-    assert lemmas[2] == 'pyjama'
-    assert lemmas[3] == 'be'
-    if tagged[2].tag == tagged[4].tag:
-        assert lemmas[4] == 'goose'
-
-
-def test_didnt(EN):
-    tokens = EN(u"I didn't do it")
-    assert tokens[1].lemma_ != u""
+def test_lemma_assignment(EN):
+    tokens = u'Bananas in pyjamas are geese .'.split(' ')
+    doc = EN.tokenizer.tokens_from_list(tokens)
+    assert all( t.lemma_ == u'' for t in doc )
+    EN.tagger(doc)
+    assert all( t.lemma_ != u'' for t in doc )
