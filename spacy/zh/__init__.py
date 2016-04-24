b6ccd8d76a0de847bb298bee73e741103b68bbd1
Matthew Honnibal <honnibal+gh@gmail.com>
2016-04-24 19:11:38 +0200

    * Use Jieba tokenizer in Chinese class

--- spacy/zh/__init__.py
+++ spacy/zh/__init__.py
@@ -1,5 +1,13 @@
 from ..language import Language
+from .jieba import JiebaTokenizer
 
 
 class Chinese(Language):
     lang = u'zh'
+
+    @classmethod
+    def default_tokenizer(cls, package, vocab):
+        '''Return Jieba-wrapper tokenizer.'''
+        return JiebaTokenizer.from_package(package, vocab)
+
+
