857454ffa0c7d66a788d15e1f6feb1ba6ba1ba59
Wolfgang Seeker <seeker@spacy.io>
2016-05-02 17:10:41 +0200

    fix indentation -.-

--- spacy/de/__init__.py
+++ spacy/de/__init__.py
@@ -12,9 +12,10 @@ from .. import about
 class German(Language):
     lang = 'de'
 
-	@classmethod
+    @classmethod
     def default_vocab(cls, package, get_lex_attr=None, vectors_package=None):
         vocab = super(German,cls).default_vocab(package,get_lex_attr,vectors_package)
-        # for now until the morphology is done for German
+        # set a dummy lemmatizer for now that simply returns the same string
+        # until the morphology is done for German
         vocab.morphology.lemmatizer = lambda string,pos: set([string])
         return vocab
