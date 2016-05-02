d2f469b809bcc9f4ba4d098ac3de6e465670282c
Matthew Honnibal <honnibal+gh@gmail.com>
2016-05-02 15:25:27 +0200

    * Fix parsing tests, so that labels are added if they're missing, and so that the branching test values are correct

--- spacy/tests/parser/test_parse.py
+++ spacy/tests/parser/test_parse.py
@@ -22,6 +22,10 @@ def test_one_word_sentence(EN):
 
 
 def apply_transition_sequence(model, doc, sequence):
+    for action_name in sequence:
+        if '-' in action_name:
+            move, label = action_name.split('-')
+            model.parser.add_label(label)
     with model.parser.step_through(doc) as stepwise:
         for transition in sequence:
             stepwise.transition(transition)
@@ -29,70 +33,70 @@ def apply_transition_sequence(model, doc, sequence):
 
 @pytest.mark.models
 def test_arc_eager_finalize_state(EN):
-	# right branching
-	example = EN.tokenizer.tokens_from_list(u"a b c d e".split(' '))
-	apply_transition_sequence(EN, example, ['R-nsubj','D','R-nsubj','R-nsubj','D','R-ROOT'])
+    # right branching
+    example = EN.tokenizer.tokens_from_list(u"a b c d e".split(' '))
+    apply_transition_sequence(EN, example, ['R-nsubj','D','R-nsubj','R-nsubj','D','R-ROOT'])
 
-	assert example[0].n_lefts == 0
-	assert example[0].n_rights == 2
-	assert example[0].left_edge.i == 0
-	assert example[0].right_edge.i == 3
-	assert example[0].head.i == 0
+    assert example[0].n_lefts == 0
+    assert example[0].n_rights == 2
+    assert example[0].left_edge.i == 0
+    assert example[0].right_edge.i == 4
+    assert example[0].head.i == 0
 
-	assert example[1].n_lefts == 0
-	assert example[1].n_rights == 0
-	assert example[1].left_edge.i == 1
-	assert example[1].right_edge.i == 1
-	assert example[1].head.i == 0
+    assert example[1].n_lefts == 0
+    assert example[1].n_rights == 0
+    assert example[1].left_edge.i == 1
+    assert example[1].right_edge.i == 1
+    assert example[1].head.i == 0
 
-	assert example[2].n_lefts == 0
-	assert example[2].n_rights == 1
-	assert example[2].left_edge.i == 2
-	assert example[2].right_edge.i == 3
-	assert example[2].head.i == 0
+    assert example[2].n_lefts == 0
+    assert example[2].n_rights == 2
+    assert example[2].left_edge.i == 2
+    assert example[2].right_edge.i == 4
+    assert example[2].head.i == 0
 
-	assert example[3].n_lefts == 0
-	assert example[3].n_rights == 0
-	assert example[3].left_edge.i == 3
-	assert example[3].right_edge.i == 3
-	assert example[3].head.i == 2
+    assert example[3].n_lefts == 0
+    assert example[3].n_rights == 0
+    assert example[3].left_edge.i == 3
+    assert example[3].right_edge.i == 3
+    assert example[3].head.i == 2
 
-	assert example[4].n_lefts == 0
-	assert example[4].n_rights == 0
-	assert example[4].left_edge.i == 4
-	assert example[4].right_edge.i == 4
-	assert example[4].head.i == 4
+    assert example[4].n_lefts == 0
+    assert example[4].n_rights == 0
+    assert example[4].left_edge.i == 4
+    assert example[4].right_edge.i == 4
+    assert example[4].head.i == 2
 
-	# left branching
-	example = EN.tokenizer.tokens_from_list(u"a b c d e".split(' '))
-	apply_transition_sequence(EN, example, ['S','L-nsubj','L-ROOT','S','L-nsubj','L-nsubj'])
+    # left branching
+    example = EN.tokenizer.tokens_from_list(u"a b c d e".split(' '))
+    apply_transition_sequence(EN, example, ['S', 'S', 'S', 'L-nsubj','L-nsubj','L-nsubj', 'L-nsubj'])
 
-	assert example[0].n_lefts == 0
-	assert example[0].n_rights == 0
-	assert example[0].left_edge.i == 0
-	assert example[0].right_edge.i == 0
-	assert example[0].head.i == 0
+    assert example[0].n_lefts == 0
+    assert example[0].n_rights == 0
+    assert example[0].left_edge.i == 0
+    assert example[0].right_edge.i == 0
+    assert example[0].head.i == 4
 
-	assert example[1].n_lefts == 0
-	assert example[1].n_rights == 0
-	assert example[1].left_edge.i == 1
-	assert example[1].right_edge.i == 1
-	assert example[1].head.i == 2
+    assert example[1].n_lefts == 0
+    assert example[1].n_rights == 0
+    assert example[1].left_edge.i == 1
+    assert example[1].right_edge.i == 1
+    assert example[1].head.i == 4
 
-	assert example[2].n_lefts == 1
-	assert example[2].n_rights == 0
-	assert example[2].left_edge.i == 1
-	assert example[2].right_edge.i == 2
-	assert example[2].head.i == 4
+    assert example[2].n_lefts == 0
+    assert example[2].n_rights == 0
+    assert example[2].left_edge.i == 2
+    assert example[2].right_edge.i == 2
+    assert example[2].head.i == 4
 
-	assert example[3].n_lefts == 0
-	assert example[3].n_rights == 0
-	assert example[3].left_edge.i == 3
-	assert example[3].right_edge.i == 3
-	assert example[3].head.i == 4
+    assert example[3].n_lefts == 0
+    assert example[3].n_rights == 0
+    assert example[3].left_edge.i == 3
+    assert example[3].right_edge.i == 3
+    assert example[3].head.i == 4
 
-	assert example[4].n_lefts == 2
-	assert example[4].n_rights == 0
-	assert example[4].left_edge.i == 1
-	assert example[4].right_edge.i == 4
-	assert example[4].head.i == 4
+    assert example[4].n_lefts == 4
+    assert example[4].n_rights == 0
+    assert example[4].left_edge.i == 0
+    assert example[4].right_edge.i == 4
+    assert example[4].head.i == 4
