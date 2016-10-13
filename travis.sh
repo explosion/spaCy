dd26035db9572c9c81ad8482091841ca4cc41c0f
Matthew Honnibal <honnibal+gh@gmail.com>
2016-10-13 12:39:41 +0200

    Refactor travis, to try to test PyPi compilation as well.

---
+++ travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${VIA}" == "pypi"]; then
+    rm -rf *
+    pip install spacy
+fi
+
+
+if "${VIA}" == "compile"]; then
+  pip install -r requirements.txt
+  pip install -e .
+  mkdir -p corpora/en
+  cd corpora/en
+  wget --no-check-certificate http://wordnetcode.princeton.edu/3.0/WordNet-3.0.tar.gz
+  tar -xzf WordNet-3.0.tar.gz
+  mv WordNet-3.0 wordnet
+  cd ../../
+  mkdir models/
+  python bin/init_model.py en lang_data/ corpora/ models/en
+fi
