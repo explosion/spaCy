8c8f5c62c6680f2700d7488555916dd89d4befcd
Matthew Honnibal <honnibal+gh@gmail.com>
2016-10-18 18:52:48 +0200

    Add LANG attribute to English and German

--- spacy/en/__init__.py
+++ spacy/en/__init__.py
@@ -8,6 +8,7 @@ from .. import util
 from ..lemmatizer import Lemmatizer
 from ..vocab import Vocab
 from ..tokenizer import Tokenizer
+from ..attrs import LANG
 
 
 class English(Language):
@@ -15,13 +16,14 @@ class English(Language):
 
     class Defaults(Language.Defaults):
         lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+        lex_attr_getters[LANG] = lambda text: 'en'
 
         tokenizer_exceptions = dict(language_data.TOKENIZER_EXCEPTIONS)
-        
+ 
         prefixes = tuple(language_data.TOKENIZER_PREFIXES)
-        
+
         suffixes = tuple(language_data.TOKENIZER_SUFFIXES)
-        
+
         infixes = tuple(language_data.TOKENIZER_INFIXES)
 
         tag_map = dict(language_data.TAG_MAP)
