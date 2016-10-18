622b0a96748c8155add3337b59b68c6458c9aeed
Matthew Honnibal <honnibal+gh@gmail.com>
2016-10-19 00:52:16 +0200

    Tweak download script

--- spacy/en/download.py
+++ spacy/en/download.py
@@ -4,11 +4,14 @@ from ..download import download
 
 @plac.annotations(
     force=("Force overwrite", "flag", "f", bool),
-    ignore_glove=("Don't install the GloVe vectors", "flag", "g", bool),
 )
-def main(data_size='all', force=False, ignore_glove=False):
-    download('en', force)
-    download('en_glove_cc_300_1m_vectors', force)
+def main(data_size='all', force=False):
+    if data_size in ('all', 'parser'):
+        print("Downloading parsing model")
+        download('en', force)
+    if data_size in ('all', 'glove'):
+        print("Downloading GloVe vectors")
+        download('en_glove_cc_300_1m_vectors', force)
 
 
 if __name__ == '__main__':
