edc45c19d69de0f0d7105873c19b52f2fa8bb077
Matthew Honnibal <honnibal+gh@gmail.com>
2016-10-19 00:41:14 +0200

    Update download script

--- spacy/en/download.py
+++ spacy/en/download.py
@@ -4,9 +4,11 @@ from ..download import download
 
 @plac.annotations(
     force=("Force overwrite", "flag", "f", bool),
+    ignore_glove=("Don't install the GloVe vectors", "flag", "g", bool),
 )
-def main(data_size='all', force=False):
+def main(data_size='all', force=False, ignore_glove=False):
     download('en', force)
+    download('en_glove_cc_300_1m_vectors', force)
 
 
 if __name__ == '__main__':
