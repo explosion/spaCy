7e5f63a59596a7951af4ccfb286cc831c3972822
Matthew Honnibal <honnibal+gh@gmail.com>
2016-10-28 17:41:16 +0200

    Improve test slightly

--- spacy/tests/regression/test_issue587.py
+++ spacy/tests/regression/test_issue587.py
@@ -1,5 +1,6 @@
 import spacy
 import spacy.matcher
+from spacy.attrs import IS_PUNCT, ORTH
 
 import pytest
 
@@ -8,9 +9,9 @@ def test_matcher_segfault():
     nlp = spacy.load('en', parser=False, entity=False)
     matcher = spacy.matcher.Matcher(nlp.vocab)
     content = u'''a b; c'''
-    matcher.add(entity_key='1', label='TEST', attrs={}, specs=[[{65: 'a'}, {65: 'b'}]])
+    matcher.add(entity_key='1', label='TEST', attrs={}, specs=[[{ORTH: 'a'}, {ORTH: 'b'}]])
     matcher(nlp(content))
-    matcher.add(entity_key='2', label='TEST', attrs={}, specs=[[{65: 'a'}, {65: 'b'}, {5: True}, {65: 'c'}]])
+    matcher.add(entity_key='2', label='TEST', attrs={}, specs=[[{ORTH: 'a'}, {ORTH: 'b'}, {IS_PUNCT: True}, {ORTH: 'c'}]])
     matcher(nlp(content))
-    matcher.add(entity_key='3', label='TEST', attrs={}, specs=[[{65: 'a'}, {65: 'b'}, {5: True}, {65: 'd'}]])
+    matcher.add(entity_key='3', label='TEST', attrs={}, specs=[[{ORTH: 'a'}, {ORTH: 'b'}, {IS_PUNCT: True}, {ORTH: 'd'}]])
     matcher(nlp(content))
