c09a8ce5bb47db4ea4381925ec07199415ae5c39
Matthew Honnibal <honnibal+gh@gmail.com>
2016-11-02 20:40:31 +0100

    Add test for french tokenizer

--- spacy/tests/integration/test_load_languages.py
+++ spacy/tests/integration/test_load_languages.py
@@ -5,3 +5,8 @@ from ...fr import French
 def test_load_french():
     nlp = French()
     doc = nlp(u'Parlez-vous français?')
+    assert doc[0].text == u'Parlez'
+    assert doc[1].text == u'-'
+    assert doc[2].text == u'vouz'
+    assert doc[3].text == u'français'
+    assert doc[4].text == u'?'
