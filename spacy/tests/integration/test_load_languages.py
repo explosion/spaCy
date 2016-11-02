80824f6d297928c03ea429d030cf41252d8b7988
Matthew Honnibal <honnibal+gh@gmail.com>
2016-11-02 20:48:40 +0100

    Fix test

--- spacy/tests/integration/test_load_languages.py
+++ spacy/tests/integration/test_load_languages.py
@@ -7,6 +7,6 @@ def test_load_french():
     doc = nlp(u'Parlez-vous français?')
     assert doc[0].text == u'Parlez'
     assert doc[1].text == u'-'
-    assert doc[2].text == u'vouz'
+    assert doc[2].text == u'vous'
     assert doc[3].text == u'français'
     assert doc[4].text == u'?'
