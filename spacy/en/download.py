3871007c72003258b5312b291848bcb4767404b3
Mark Amery <markamery@btinternet.com>
2016-11-20 15:48:04 +0000

    Let --data-path be specified when running download.py scripts
    Resolves https://github.com/explosion/spaCy/issues/637

--- spacy/en/download.py
+++ spacy/en/download.py
@@ -7,17 +7,18 @@ from .. import about
 
 @plac.annotations(
     force=("Force overwrite", "flag", "f", bool),
+    data_path=("Path to download model", "option", "d", str)
 )
-def main(data_size='all', force=False):
+def main(data_size='all', force=False, data_path=None):
     if force:
         sputnik.purge(about.__title__, about.__version__)
 
     if data_size in ('all', 'parser'):
         print("Downloading parsing model")
-        download('en', False)
+        download('en', force=False, data_path=data_path)
     if data_size in ('all', 'glove'):
         print("Downloading GloVe vectors")
-        download('en_glove_cc_300_1m_vectors', False)
+        download('en_glove_cc_300_1m_vectors', force=False, data_path=data_path)
 
 
 if __name__ == '__main__':
