8863e504eb53b9f83513d54149360a5284af7e09
Ines Montani <ines@ines.io>
2016-12-08 20:07:14 +0100

    Update French language data

--- spacy/fr/__init__.py
+++ spacy/fr/__init__.py
@@ -3,25 +3,37 @@ from __future__ import unicode_literals, print_function
 from os import path
 
 from ..language import Language
-from ..attrs import LANG
 from . import language_data
+from ..attrs import LANG
+from ..util import update_exc
+
+from ..language_data import EMOTICONS
+from .language_data import ORTH_ONLY
+from .language_data import strings_to_exc
+
+
+TOKENIZER_EXCEPTIONS = dict(language_data.TOKENIZER_EXCEPTIONS)
+TOKENIZER_PREFIXES = tuple(language_data.TOKENIZER_PREFIXES)
+TOKENIZER_SUFFIXES = tuple(language_data.TOKENIZER_SUFFIXES)
+TOKENIZER_INFIXES = tuple(language_data.TOKENIZER_INFIXES)
+TAG_MAP = dict(language_data.TAG_MAP)
+STOP_WORDS = set(language_data.STOP_WORDS)
+
+
+update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(EMOTICONS))
+update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(ORTH_ONLY))
 
 
 class French(Language):
     lang = 'fr'
-    
+
     class Defaults(Language.Defaults):
-        tokenizer_exceptions = dict(language_data.TOKENIZER_EXCEPTIONS)
         lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
         lex_attr_getters[LANG] = lambda text: 'fr'
-        
-        prefixes = tuple(language_data.TOKENIZER_PREFIXES)
-        
-        suffixes = tuple(language_data.TOKENIZER_SUFFIXES)
-        
-        infixes = tuple(language_data.TOKENIZER_INFIXES)
 
-        tag_map = dict(language_data.TAG_MAP)
-
-        stop_words = set(language_data.STOP_WORDS)
-
+        tokenizer_exceptions = TOKENIZER_EXCEPTIONS
+        prefixes = TOKENIZER_PREFIXES
+        suffixes = TOKENIZER_SUFFIXES
+        infixes = TOKENIZER_INFIXES
+        tag_map = TAG_MAP
+        stop_words = STOP_WORDS
