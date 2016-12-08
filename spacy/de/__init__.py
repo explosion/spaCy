1256232fad5d45fae1347edabfaee0a0bdb5fd0c
Ines Montani <ines@ines.io>
2016-12-08 13:56:40 +0100

    Fix formatting

--- spacy/de/__init__.py
+++ spacy/de/__init__.py
@@ -9,7 +9,7 @@ from . import language_data
 
 class German(Language):
     lang = 'de'
-    
+
     class Defaults(Language.Defaults):
         tokenizer_exceptions = dict(language_data.TOKENIZER_EXCEPTIONS)
         lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
