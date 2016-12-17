afda532595651fce803bde9689edff64b0ebb6cf
Ines Montani <ines@ines.io>
2016-12-17 13:56:24 +0100

    Use symbols in tag map

--- spacy/nl/language_data.py
+++ spacy/nl/language_data.py
@@ -11,22 +11,22 @@ from ..language_data import TOKENIZER_INFIXES
 # TODO insert TAG_MAP for Dutch
 
 TAG_MAP = {
-    "ADV":      {POS: "ADV"},
-    "NOUN":     {POS: "NOUN"},
-    "ADP":      {POS: "ADP"},
-    "PRON":     {POS: "PRON"},
-    "SCONJ":    {POS: "SCONJ"},
-    "PROPN":    {POS: "PROPN"},
-    "DET":      {POS: "DET"},
-    "SYM":      {POS: "SYM"},
-    "INTJ":     {POS: "INTJ"},
-    "PUNCT":    {POS: "PUNCT"},
-    "NUM":      {POS: "NUM"},
-    "AUX":      {POS: "AUX"},
-    "X":        {POS: "X"},
-    "CONJ":     {POS: "CONJ"},
-    "ADJ":      {POS: "ADJ"},
-    "VERB":     {POS: "VERB"}
+    "ADV":      {POS: ADV},
+    "NOUN":     {POS: NOUN},
+    "ADP":      {POS: ADP},
+    "PRON":     {POS: PRON},
+    "SCONJ":    {POS: SCONJ},
+    "PROPN":    {POS: PROPN},
+    "DET":      {POS: DET},
+    "SYM":      {POS: SYM},
+    "INTJ":     {POS: INTJ},
+    "PUNCT":    {POS: PUNCT},
+    "NUM":      {POS: NUM},
+    "AUX":      {POS: AUX},
+    "X":        {POS: X},
+    "CONJ":     {POS: CONJ},
+    "ADJ":      {POS: ADJ},
+    "VERB":     {POS: VERB}
 }
 
 
