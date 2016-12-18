2b2ea8ca11d1658325a855861150e2dbd5d709fb
Ines Montani <ines@ines.io>
2016-12-18 16:54:19 +0100

    Reorganise language data

---
+++ spacy/es/stop_words.py
@@ -0,0 +1,84 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set("""
+actualmente acuerdo adelante ademas además adrede afirmó agregó ahi ahora ahí
+al algo alguna algunas alguno algunos algún alli allí alrededor ambos ampleamos
+antano antaño ante anterior antes apenas aproximadamente aquel aquella aquellas
+aquello aquellos aqui aquél aquélla aquéllas aquéllos aquí arriba arribaabajo
+aseguró asi así atras aun aunque ayer añadió aún
+
+bajo bastante bien breve buen buena buenas bueno buenos
+
+cada casi cerca cierta ciertas cierto ciertos cinco claro comentó como con
+conmigo conocer conseguimos conseguir considera consideró consigo consigue
+consiguen consigues contigo contra cosas creo cual cuales cualquier cuando
+cuanta cuantas cuanto cuantos cuatro cuenta cuál cuáles cuándo cuánta cuántas
+cuánto cuántos cómo
+
+da dado dan dar de debajo debe deben debido decir dejó del delante demasiado
+demás dentro deprisa desde despacio despues después detras detrás dia dias dice
+dicen dicho dieron diferente diferentes dijeron dijo dio donde dos durante día
+días dónde
+
+ejemplo el ella ellas ello ellos embargo empleais emplean emplear empleas
+empleo en encima encuentra enfrente enseguida entonces entre era eramos eran
+eras eres es esa esas ese eso esos esta estaba estaban estado estados estais
+estamos estan estar estará estas este esto estos estoy estuvo está están ex
+excepto existe existen explicó expresó él ésa ésas ése ésos ésta éstas éste
+éstos
+
+fin final fue fuera fueron fui fuimos
+
+general gran grandes gueno
+
+ha haber habia habla hablan habrá había habían hace haceis hacemos hacen hacer
+hacerlo haces hacia haciendo hago han hasta hay haya he hecho hemos hicieron
+hizo horas hoy hubo
+
+igual incluso indicó informo informó intenta intentais intentamos intentan
+intentar intentas intento ir
+
+junto
+
+la lado largo las le lejos les llegó lleva llevar lo los luego lugar
+
+mal manera manifestó mas mayor me mediante medio mejor mencionó menos menudo mi
+mia mias mientras mio mios mis misma mismas mismo mismos modo momento mucha
+muchas mucho muchos muy más mí mía mías mío míos
+
+nada nadie ni ninguna ningunas ninguno ningunos ningún no nos nosotras nosotros
+nuestra nuestras nuestro nuestros nueva nuevas nuevo nuevos nunca
+
+ocho os otra otras otro otros
+
+pais para parece parte partir pasada pasado paìs peor pero pesar poca pocas
+poco pocos podeis podemos poder podria podriais podriamos podrian podrias podrá
+podrán podría podrían poner por porque posible primer primera primero primeros
+principalmente pronto propia propias propio propios proximo próximo próximos
+pudo pueda puede pueden puedo pues
+
+qeu que quedó queremos quien quienes quiere quiza quizas quizá quizás quién quiénes qué
+
+raras realizado realizar realizó repente respecto
+
+sabe sabeis sabemos saben saber sabes salvo se sea sean segun segunda segundo
+según seis ser sera será serán sería señaló si sido siempre siendo siete sigue
+siguiente sin sino sobre sois sola solamente solas solo solos somos son soy
+soyos su supuesto sus suya suyas suyo sé sí sólo
+
+tal tambien también tampoco tan tanto tarde te temprano tendrá tendrán teneis
+tenemos tener tenga tengo tenido tenía tercera ti tiempo tiene tienen toda
+todas todavia todavía todo todos total trabaja trabajais trabajamos trabajan
+trabajar trabajas trabajo tras trata través tres tu tus tuvo tuya tuyas tuyo
+tuyos tú
+
+ultimo un una unas uno unos usa usais usamos usan usar usas uso usted ustedes
+última últimas último últimos
+
+va vais valor vamos van varias varios vaya veces ver verdad verdadera verdadero
+vez vosotras vosotros voy vuestra vuestras vuestro vuestros
+
+ya yo
+""".split())
---
+++ spacy/it/stop_words.py
@@ -0,0 +1,85 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set("""
+a abbastanza abbia abbiamo abbiano abbiate accidenti ad adesso affinche agl
+agli ahime ahimè ai al alcuna alcuni alcuno all alla alle allo allora altri
+altrimenti altro altrove altrui anche ancora anni anno ansa anticipo assai
+attesa attraverso avanti avemmo avendo avente aver avere averlo avesse
+avessero avessi avessimo aveste avesti avete aveva avevamo avevano avevate
+avevi avevo avrai avranno avrebbe avrebbero avrei avremmo avremo avreste
+avresti avrete avrà avrò avuta avute avuti avuto
+
+basta bene benissimo brava bravo
+
+casa caso cento certa certe certi certo che chi chicchessia chiunque ci
+ciascuna ciascuno cima cio cioe circa citta città co codesta codesti codesto
+cogli coi col colei coll coloro colui come cominci comunque con concernente
+conciliarsi conclusione consiglio contro cortesia cos cosa cosi così cui
+
+da dagl dagli dai dal dall dalla dalle dallo dappertutto davanti degl degli
+dei del dell della delle dello dentro detto deve di dice dietro dire
+dirimpetto diventa diventare diventato dopo dov dove dovra dovrà dovunque due
+dunque durante
+
+ebbe ebbero ebbi ecc ecco ed effettivamente egli ella entrambi eppure era
+erano eravamo eravate eri ero esempio esse essendo esser essere essi ex
+
+fa faccia facciamo facciano facciate faccio facemmo facendo facesse facessero
+facessi facessimo faceste facesti faceva facevamo facevano facevate facevi
+facevo fai fanno farai faranno fare farebbe farebbero farei faremmo faremo
+fareste faresti farete farà farò fatto favore fece fecero feci fin finalmente
+finche fine fino forse forza fosse fossero fossi fossimo foste fosti fra
+frattempo fu fui fummo fuori furono futuro generale
+
+gia già giacche giorni giorno gli gliela gliele glieli glielo gliene governo
+grande grazie gruppo
+
+ha haha hai hanno ho
+
+ieri il improvviso in inc infatti inoltre insieme intanto intorno invece io
+
+la là lasciato lato lavoro le lei li lo lontano loro lui lungo luogo
+
+ma macche magari maggior mai male malgrado malissimo mancanza marche me
+medesimo mediante meglio meno mentre mesi mezzo mi mia mie miei mila miliardi
+milioni minimi ministro mio modo molti moltissimo molto momento mondo mosto
+
+nazionale ne negl negli nei nel nell nella nelle nello nemmeno neppure nessun
+nessuna nessuno niente no noi non nondimeno nonostante nonsia nostra nostre
+nostri nostro novanta nove nulla nuovo
+
+od oggi ogni ognuna ognuno oltre oppure ora ore osi ossia ottanta otto
+
+paese parecchi parecchie parecchio parte partendo peccato peggio per perche
+perché percio perciò perfino pero persino persone però piedi pieno piglia piu
+piuttosto più po pochissimo poco poi poiche possa possedere posteriore posto
+potrebbe preferibilmente presa press prima primo principalmente probabilmente
+proprio puo può pure purtroppo
+
+qualche qualcosa qualcuna qualcuno quale quali qualunque quando quanta quante
+quanti quanto quantunque quasi quattro quel quella quelle quelli quello quest
+questa queste questi questo qui quindi
+
+realmente recente recentemente registrazione relativo riecco salvo
+
+sara sarà sarai saranno sarebbe sarebbero sarei saremmo saremo sareste
+saresti sarete saro sarò scola scopo scorso se secondo seguente seguito sei
+sembra sembrare sembrato sembri sempre senza sette si sia siamo siano siate
+siete sig solito solo soltanto sono sopra sotto spesso srl sta stai stando
+stanno starai staranno starebbe starebbero starei staremmo staremo stareste
+staresti starete starà starò stata state stati stato stava stavamo stavano
+stavate stavi stavo stemmo stessa stesse stessero stessi stessimo stesso
+steste stesti stette stettero stetti stia stiamo stiano stiate sto su sua
+subito successivamente successivo sue sugl sugli sui sul sull sulla sulle
+sullo suo suoi
+
+tale tali talvolta tanto te tempo ti titolo torino tra tranne tre trenta
+troppo trovato tu tua tue tuo tuoi tutta tuttavia tutte tutti tutto
+
+uguali ulteriore ultimo un una uno uomo
+
+va vale vari varia varie vario verso vi via vicino visto vita voi volta volte
+vostra vostre vostri vostro
+""".split())
