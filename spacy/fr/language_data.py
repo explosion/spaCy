0dec90e9f77cfe124d0fde8e994f61de4222064d
Ines Montani <ines@ines.io>
2017-01-08 20:36:00 +0100

    Use global abbreviation data languages and remove duplicates

--- spacy/fr/language_data.py
+++ spacy/fr/language_data.py
@@ -2,13 +2,16 @@
 from __future__ import unicode_literals
 
 from .. import language_data as base
-from ..language_data import strings_to_exc
+from ..language_data import strings_to_exc, update_exc
 
 from .stop_words import STOP_WORDS
 
 
-TOKENIZER_EXCEPTIONS = strings_to_exc(base.EMOTICONS)
 STOP_WORDS = set(STOP_WORDS)
+
+
+TOKENIZER_EXCEPTIONS = strings_to_exc(base.EMOTICONS)
+update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(base.ABBREVIATIONS))
 
 
 __all__ = ["TOKENIZER_EXCEPTIONS", "STOP_WORDS"]
--- spacy/hu/language_data.py
+++ spacy/hu/language_data.py
@@ -11,13 +11,14 @@ from .tokenizer_exceptions import OTHER_EXC
 from .. import language_data as base
 
 STOP_WORDS = set(STOP_WORDS)
-TOKENIZER_EXCEPTIONS = strings_to_exc(base.EMOTICONS)
 TOKENIZER_PREFIXES = base.TOKENIZER_PREFIXES + TOKENIZER_PREFIXES
 TOKENIZER_SUFFIXES = TOKENIZER_SUFFIXES
 TOKENIZER_INFIXES = TOKENIZER_INFIXES
 
 # HYPHENS = [six.unichr(cp) for cp in [173, 8211, 8212, 8213, 8722, 9472]]
 
+TOKENIZER_EXCEPTIONS = strings_to_exc(base.EMOTICONS)
+update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(base.ABBREVIATIONS))
 update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(OTHER_EXC))
 update_exc(TOKENIZER_EXCEPTIONS, strings_to_exc(ABBREVIATIONS))
 
