6935d5540942fdc07902fe6223a992fe9f089483
Ines Montani <ines@ines.io>
2017-01-12 22:56:20 +0100

    Fix formatting

--- spacy/tests/regression/test_issue736.py
+++ spacy/tests/regression/test_issue736.py
@@ -1,7 +1,4 @@
 # coding: utf-8
-"""Test that times like "7am" are tokenized correctly and that numbers are converted to string."""
-
-
 from __future__ import unicode_literals
 
 import pytest
@@ -9,6 +6,7 @@ import pytest
 
 @pytest.mark.parametrize('text,number', [("7am", "7"), ("11p.m.", "11")])
 def test_issue736(en_tokenizer, text, number):
+    """Test that times like "7am" are tokenized correctly and that numbers are converted to string."""
     tokens = en_tokenizer(text)
     assert len(tokens) == 2
     assert tokens[0].text == number
