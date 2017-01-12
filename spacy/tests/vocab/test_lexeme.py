c3d4516fc256b92d2928b404029016b00363d730
Ines Montani <ines@ines.io>
2017-01-12 15:51:03 +0100

    Move test for #361 to regression tests

--- spacy/tests/vocab/test_lexeme.py
+++ spacy/tests/vocab/test_lexeme.py
@@ -5,21 +5,7 @@ import pytest
 from spacy.attrs import *
 
 
-def test_lexeme_eq(en_vocab):
-    '''Test Issue #361: Equality of lexemes'''
-    cat1 = en_vocab['cat']
 
-    cat2 = en_vocab['cat']
-
-    assert cat1 == cat2
-
-def test_lexeme_neq(en_vocab):
-    '''Inequality of lexemes'''
-    cat = en_vocab['cat']
-
-    dog = en_vocab['dog']
-
-    assert cat != dog
 
 def test_lexeme_lt(en_vocab):
     '''More frequent is l.t. less frequent'''
