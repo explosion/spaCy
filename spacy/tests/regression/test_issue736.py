c5914c6fe556e5cdf06f6c3930da1c6f088299fb
Ines Montani <ines@ines.io>
2017-01-12 11:48:56 +0100

    Fix and pass regression test for #736

--- spacy/tests/regression/test_issue736.py
+++ spacy/tests/regression/test_issue736.py
@@ -7,11 +7,8 @@ from __future__ import unicode_literals
 import pytest
 
 
-@pytest.mark.xfail
 @pytest.mark.parametrize('text,number', [("7am", "7"), ("11p.m.", "11")])
 def test_issue736(en_tokenizer, text, number):
     tokens = en_tokenizer(text)
     assert len(tokens) == 2
     assert tokens[0].text == number
-    assert tokens[0].lemma_ == number
-    assert tokens[0].pos_ == 'NUM'
