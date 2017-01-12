359f73a96b94a1ae6fcd81f80b9c5fcb3f458821
Ines Montani <ines@ines.io>
2017-01-12 12:25:51 +0100

    Move test for #54 to regression tests

--- spacy/tests/spans/test_merge.py
+++ spacy/tests/spans/test_merge.py
@@ -31,11 +31,6 @@ def test_merge_heads(EN):
     assert tokens[5].head.i == 4
 
 
-@pytest.mark.models
-def test_issue_54(EN):
-    text = u'Talks given by women had a slightly higher number of questions asked (3.2$\pm$0.2) than talks given by men (2.6$\pm$0.1).'
-    tokens = EN(text)
-
 
 @pytest.mark.models
 def test_np_merges(EN):
