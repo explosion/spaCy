7ccf490c73ec1bfa89c2c003298333a750a34d53
Matthew Honnibal <honnibal+gh@gmail.com>
2017-01-16 13:17:58 +0100

    Increment version

--- spacy/about.py
+++ spacy/about.py
@@ -4,7 +4,7 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __title__ = 'spacy'
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 __summary__ = 'Industrial-strength NLP'
 __uri__ = 'https://spacy.io'
 __author__ = 'Matthew Honnibal'
