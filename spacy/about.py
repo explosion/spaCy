64e142f4606250bf03fa65a899da900a6c712ebd
Ines Montani <ines.montani@gmail.com>
2017-01-16 14:23:08 +0100

    Update about.py

--- spacy/about.py
+++ spacy/about.py
@@ -3,12 +3,12 @@
 # https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
-__title__ = 'spacy'
+__title__ = 'spaCy'
 __version__ = '1.6.0'
-__summary__ = 'Industrial-strength NLP'
+__summary__ = 'Industrial-strength Natural Language Processing (NLP) with Python and Cython'
 __uri__ = 'https://spacy.io'
 __author__ = 'Matthew Honnibal'
-__email__ = 'matt@spacy.io'
+__email__ = 'matt@explosion.ai'
 __license__ = 'MIT'
 __models__ = {
     'en': 'en>=1.1.0,<1.2.0',
