012f4820cb1cfb052e4d3bd2b6e1031ec9f30882
Ines Montani <ines@ines.io>
2017-02-02 16:22:40 +0100

    Keep infixes of punctuation + hyphens as one token (see #801)

--- spacy/language_data/punctuation.py
+++ spacy/language_data/punctuation.py
@@ -106,7 +106,7 @@ TOKENIZER_INFIXES = (
         r'(?<=[0-9])[+\-\*^](?=[0-9-])',
         r'(?<=[{al}])\.(?=[{au}])'.format(al=ALPHA_LOWER, au=ALPHA_UPPER),
         r'(?<=[{a}]),(?=[{a}])'.format(a=ALPHA),
-        r'(?<=[{a}])(?:{h})(?=[{a}])'.format(a=ALPHA, h=HYPHENS),
+        r'(?<=[{a}])[?";:=,.]*(?:{h})(?=[{a}])'.format(a=ALPHA, h=HYPHENS),
         r'(?<=[{a}"])[:<>=](?=[{a}])'.format(a=ALPHA)
     ]
 )
