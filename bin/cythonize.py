071d11cb35f276da4463978dfe6056765d106dbe
Matthew Honnibal <honnibal+gh@gmail.com>
2017-02-17 01:04:16 +1100

    Pass environment to Cythonize script. Closes #791

--- bin/cythonize.py
+++ bin/cythonize.py
@@ -55,7 +55,8 @@ def process_pyx(fromfile, tofile):
 
     try:
         try:
-            r = subprocess.call(['cython'] + flags + ['-o', tofile, fromfile])
+            r = subprocess.call(['cython'] + flags + ['-o', tofile, fromfile],
+                                shell=True, env=os.environ) # See Issue #791
             if r != 0:
                 raise Exception('Cython failed')
         except OSError:
