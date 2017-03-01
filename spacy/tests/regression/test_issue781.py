a8cfde46d33076672aa4fdbfa0f448e0bc1c593d
Juan Miguel Cejuela <i@juanmi.rocks>
2017-03-01 21:43:08 +0100

    #781 Fix test — colocalizes is lemmatized to colocaliz and colicalize

--- spacy/tests/regression/test_issue781.py
+++ spacy/tests/regression/test_issue781.py
@@ -5,6 +5,6 @@ import pytest
 
 
 # Note: "chromosomes" worked previous the bug fix
-@pytest.mark.parametrize('word,lemma', [("chromosomes", "chromosome"), ("endosomes", "endosome"), ("colocalizes", "colocalize")])
-def test_issue781(path, lemmatizer, word, lemma):
-    assert lemmatizer(word, 'noun', morphology={'number': 'plur'}) == set([lemma])
+@pytest.mark.parametrize('word,lemmas', [("chromosomes", ["chromosome"]), ("endosomes", ["endosome"]), ("colocalizes", ["colocalize", "colocaliz"])])
+def test_issue781(path, lemmatizer, word, lemmas):
+    assert lemmatizer(word, 'noun', morphology={'number': 'plur'}) == set(lemmas)
