d70386ec6e7ac524e66391d868a085cc2e8aa588
ines <ines@ines.io>
2017-03-13 12:00:38 +0100

    Update docstring in #886 regression test

--- spacy/tests/regression/test_issue886.py
+++ spacy/tests/regression/test_issue886.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.mark.parametrize('text', ["Datum:2014-06-02\nDokument:76467"])
 def test_issue886(en_tokenizer, text):
-    """Test that no extra space is added in doc.text method."""
+    """Test that token.idx matches the original text index for texts with newlines."""
     doc = en_tokenizer(text)
     for token in doc:
         assert len(token.text) == len(token.text_with_ws)
