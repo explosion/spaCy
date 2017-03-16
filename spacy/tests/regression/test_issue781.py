db51abf685e3b7d39728e6420e67e10de58664ed
Matthew Honnibal <honnibal@gmail.com>
2017-03-16 18:53:47 -0500

    Fix tests

--- spacy/tests/regression/test_issue781.py
+++ spacy/tests/regression/test_issue781.py
@@ -6,5 +6,5 @@ import pytest
 
 # Note: "chromosomes" worked previous the bug fix
 @pytest.mark.parametrize('word,lemmas', [("chromosomes", ["chromosome"]), ("endosomes", ["endosome"]), ("colocalizes", ["colocalize", "colocaliz"])])
-def test_issue781(path, lemmatizer, word, lemmas):
+def test_issue781(lemmatizer, word, lemmas):
     assert lemmatizer(word, 'noun', morphology={'number': 'plur'}) == set(lemmas)
