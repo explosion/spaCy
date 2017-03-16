2f0db1dd365c23e3c35ad0e80767cf9106b18a62
ines <ines@ines.io>
2017-03-16 09:54:40 +0100

    Use small English model as default

--- spacy/about.py
+++ spacy/about.py
@@ -13,4 +13,4 @@ __license__ = 'MIT'
 __docs__ = 'https://spacy.io/docs/usage'
 __download_url__ = 'https://github.com/explosion/spacy-models/releases/download'
 __compatibility__ = 'https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json?token=ANAt54fi5zcUtnwGhMLw2klWwcAyHkZGks5Y0nw1wA%3D%3D'
-__shortcuts__ = {'en': 'en_core_web_md', 'de': 'de_core_web_md', 'vectors': 'en_vectors_glove_md'}
+__shortcuts__ = {'en': 'en_core_web_sm', 'de': 'de_core_web_md', 'vectors': 'en_vectors_glove_md'}
