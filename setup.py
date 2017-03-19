9c17fb472f618e6e1f342374f09ae4e4030b90f4
Matthew Honnibal <honnibal+gh@gmail.com>
2017-03-19 01:40:24 +0100

    Add tag for spaCy v3.6 compatibility

--- setup.py
+++ setup.py
@@ -261,6 +261,7 @@ def setup_package():
                 'Programming Language :: Python :: 3.3',
                 'Programming Language :: Python :: 3.4',
                 'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
                 'Topic :: Scientific/Engineering'],
             cmdclass = {
                 'build_ext': build_ext_subclass},
