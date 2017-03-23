e821c484895d42be9a90743ffd8d31a0469f841b
Leif Uwe Vogelsang <leif.vogelsang@retriever.no>
2017-03-23 11:10:22 +0100

    Norwegian language basics

--- spacy/nb/stop_words.py
+++ spacy/nb/stop_words.py
@@ -1,6 +1,7 @@
 # encoding: utf8
 from __future__ import unicode_literals
  
+<<<<<<< HEAD
 
 STOP_WORDS = set("""
 alle allerede alt and andre annen annet at av
@@ -46,4 +47,42 @@ vant var ved veldig vi videre viktig vil ville viser vår være vært
 å år
 
 ønsker
+=======
+ 
+STOP_WORDS = set("""alle at av
+ 
+bare begge ble blei bli blir blitt både båe 
+ 
+da de deg dei deim deira deires dem den denne der dere deres det dette di din disse ditt du dykk dykkar då
+ 
+eg ein eit eitt eller elles en enn er et ett etter
+ 
+for fordi fra før
+ 
+ha hadde han hans har hennar henne hennes her hjå ho hoe honom hoss hossen hun hva hvem hver hvilke hvilken hvis hvor hvordan hvorfor
+ 
+i ikke ikkje ikkje ingen ingi inkje inn inni
+ 
+ja jeg
+ 
+kan kom korleis korso kun kunne kva kvar kvarhelst kven kvi kvifor
+ 
+man mange me med medan meg meget mellom men mi min mine mitt mot mykje
+ 
+ned no noe noen noka noko nokon nokor nokre nå når
+ 
+og også om opp oss over
+ 
+på
+ 
+samme seg selv si sia sidan siden sin sine sitt sjøl skal skulle slik so som som somme somt så sånn 
+ 
+til 
+ 
+um upp ut uten 
+ 
+var vart varte ved vere verte vi vil ville vore vors vort vår være være vært 
+ 
+å 
+>>>>>>> Norwegian language basics
 """.split())
