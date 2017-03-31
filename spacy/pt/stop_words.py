465b240bcb0ccfa445571830eaee923bfa2638cb
Miguel Almeida <migaxmoitax@gmail.com>
2017-03-31 13:00:29 +0100

    Review Portuguese stop words
    Mainly to review typos and add missing masculines/feminines

--- spacy/pt/stop_words.py
+++ spacy/pt/stop_words.py
@@ -3,18 +3,19 @@ from __future__ import unicode_literals
 
 
 STOP_WORDS = set("""
-à às acerca adeus agora ainda algo algumas alguns ali além ambos ano
-anos antes ao aos apenas apoio apontar após aquela aquelas aquele aqueles aqui
-aquilo area área as assim através atrás até aí
+à às acerca adeus agora ainda algo algumas alguns ali além ambas ambos ano
+anos antes ao aos apenas apoio apoia apontar após aquela aquelas aquele aqueles
+aqui aquilo área as assim através atrás até aí
 
-baixo bastante bem bom breve
+baixo bastante bem boa bom breve
 
 cada caminho catorze cedo cento certamente certeza cima cinco coisa com como
-comprido conhecido conselho contra corrente custa cá
+comprido comprida conhecida conhecido conselho contra corrente custa cá
 
-da daquela daquele dar das de debaixo demais dentro depois desde desligado
-dessa desse desta deste deve devem deverá dez dezanove dezasseis dezassete
-dezoito dia diante direita diz dizem dizer do dois dos doze duas dá dão dúvida
+da daquela daquele dar das de debaixo demais dentro depois desde desligada
+desligado dessa desse desta deste deve devem deverá dez dezanove dezasseis
+dezassete dezoito dia diante direita diz dizem dizer do dois dos doze duas dá
+dão dúvida
 
 é ela elas ele eles em embora enquanto entre então era és essa essas esse esses
 esta estado estar estará estas estava este estes esteve estive estivemos
@@ -35,34 +36,34 @@ maior maioria maiorias mais mal mas me meio menor menos meses mesmo meu meus
 mil minha minhas momento muito muitos máximo mês
 
 na nada naquela naquele nas nem nenhuma nessa nesse nesta neste no noite nome
-nos nossa nossas nosso nossos nova nove novo novos num numa nunca não nível nós
-número
+nos nossa nossas nosso nossos nova novas nove novo novos num numa nunca nuns
+não nível nós número números
 
 obra obrigada obrigado oitava oitavo oito onde ontem onze os ou outra outras
 outro outros
 
 para parece parte partir pegar pela pelas pelo pelos perto pessoas pode podem
 poder poderá podia ponto pontos por porque porquê posição possivelmente posso
-possível pouca pouco povo primeira primeiro promeiro próprio próximo puderam
-pôde põe põem
+possível pouca pouco povo primeira primeiro próprio próxima próximo puderam pôde
+põe põem
 
-qual qualquer quando quanto quarta quarto quatro que quem quer quero questão
-quieto quinta quinto quinze quê
+qual qualquer quando quanto quarta quarto quatro que quem quer querem quero
+questão quieta quieto quinta quinto quinze quê
 
 relação
 
 sabe saber se segunda segundo sei seis sem sempre ser seria sete seu seus sexta
 sexto sim sistema sob sobre sois somente somos sou sua suas são sétima sétimo
 
-tal talvez também tanto tarde te tem temos tempo tendes tenho tens tentar
+tal talvez também tanta tanto tarde te tem temos tempo tendes tenho tens tentar
 tentaram tente tentei ter terceira terceiro teu teus teve tipo tive tivemos
 tiveram tiveste tivestes toda todas todo todos trabalhar trabalho treze três tu
 tua tuas tudo tão têm
 
 último um uma umas uns usa usar
 
-vai vais valor veja vem vens ver verdade verdadeiro vez vezes viagem vindo
-vinte você vocês vos vossa vossas vosso vossos vários vão vêm vós
+vai vais valor veja vem vens ver verdade verdadeira verdadeiro vez vezes viagem
+vinda vindo vinte você vocês vos vossa vossas vosso vossos vários vão vêm vós
 
 zero
 """.split())
