e854f28304bfea7d7b80e2f52d8a514418a53553
Matthew Honnibal <honnibal+gh@gmail.com>
2017-03-31 13:26:25 +0200

    Add test for Issue #758
    Issue #758 occurs when no actions are available for a single token
    doc after merging.

---
+++ spacy/tests/regression/test_issue758.py
@@ -0,0 +1,17 @@
+from ... import load as load_spacy
+from ...attrs import LEMMA
+from ...matcher import merge_phrase
+
+import pytest
+
+
+
+
+@pytest.mark.models
+def test_issue758():
+    '''Test parser transition bug after label added.'''
+    nlp = load_spacy('en')
+    nlp.matcher.add('splash', 'my_entity', {},
+                    [[{LEMMA: 'splash'}, {LEMMA: 'on'}]],
+                    on_match=merge_phrase)
+    doc = nlp('splash On')
