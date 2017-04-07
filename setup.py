eedafd8d82cd96f097fab96ac3e09b0271128213
Matthew Honnibal <honnibal+gh@gmail.com>
2017-04-07 17:47:11 +0200

    Fix regex version pin

--- setup.py
+++ setup.py
@@ -247,7 +247,7 @@ def setup_package():
                 'ujson>=1.35',
                 'dill>=0.2,<0.3',
                 'requests>=2.13.0,<3.0.0',
-                'regex>=2.4.0,<3.0.0'],
+                'regex==2017.4.5']
             classifiers=[
                 'Development Status :: 5 - Production/Stable',
                 'Environment :: Console',
