42305bc519653c1f46bc0acb2ced8196ddc02025
ines <ines@ines.io>
2017-04-23 21:21:41 +0200

    Remove unnecessary test

--- spacy/tests/test_misc.py
+++ spacy/tests/test_misc.py
@@ -11,9 +11,3 @@ import pytest
 def test_util_ensure_path_succeeds(text):
     path = ensure_path(text)
     assert isinstance(path, Path)
-
-
-@pytest.mark.parametrize('text', [b'hello/world', True, False, None])
-def test_util_ensure_path_fails(text):
-    path = ensure_path(text)
-    assert not isinstance(path, Path)
