26e31afc188a6b265d744a5b7424c1f900bc646d
ghoward <gregory.howard@sword-group.com>
2017-04-25 17:46:01 +0200

    renamming tests

--- spacy/tests/fr/test_lemmatization.py
+++ spacy/tests/fr/test_lemmatization.py
@@ -4,15 +4,17 @@ from __future__ import unicode_literals
 
 import pytest
 
+
 @pytest.mark.xfail
-def test_lemmatizer_(fr_tokenizer):
+def test_lemmatizer_verb(fr_tokenizer):
     text = "Je suis allé au mois de janv. aux prud’hommes."
     tokens = fr_tokenizer(text)
     assert len(tokens) == 10
     assert tokens[2].lemma_ == "aller"
 
+
 @pytest.mark.xfail
-def test_tokenizer_handles_exc_in_text_2(fr_tokenizer):
+def test_tokenizer_verb_2(fr_tokenizer):
     text = "Je dois manger ce soir"
     tokens = fr_tokenizer(text)
     assert len(tokens) == 11
@@ -20,14 +22,7 @@ def test_tokenizer_handles_exc_in_text_2(fr_tokenizer):
 
 
 @pytest.mark.xfail
-def test_tokenizer_handles_exc_in_text_2(fr_tokenizer):
-    text = "Je dois manger ce soir"
-    tokens = fr_tokenizer(text)
-    assert len(tokens) == 11
-    assert tokens[1].lemma_ == "devoir"
-
-@pytest.mark.xfail
-def test_tokenizer_handles_exc_in_text_2(fr_tokenizer):
+def test_tokenizer_verb_noun(fr_tokenizer):
     # This one is tricky because notes is a NOUN and can be a VERB
     text = "Nous validerons vos notes plus tard"
     tokens = fr_tokenizer(text)
