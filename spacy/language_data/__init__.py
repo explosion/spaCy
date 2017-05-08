4930f0fa8f0021d23f68c8ae7af6866bae4c2750
ines <ines@ines.io>
2017-05-08 16:11:54 +0200

    Explicitly import TOKEN_MATCH

--- spacy/language_data/__init__.py
+++ spacy/language_data/__init__.py
@@ -1,4 +1,4 @@
 from .punctuation import *
 from .tag_map import TAG_MAP
 from .entity_rules import ENTITY_RULES
-from .tokenizer_exceptions import BASE_EXCEPTIONS
+from .tokenizer_exceptions import BASE_EXCEPTIONS, TOKEN_MATCH
