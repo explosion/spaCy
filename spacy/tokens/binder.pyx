62ecdea9f2e6735ec4f23d35324fb50eaa096407
Matthew Honnibal <honnibal+gh@gmail.com>
2017-05-09 17:21:00 +0200

    Add binder class for document serialization

---
+++ spacy/tokens/binder.pyx
@@ -0,0 +1,21 @@
+cdef class Binder:
+    def __init__(self, *docs):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __reduce__(self):
+        pass
+
+    def to_bytes(self):
+        pass
+
+    def from_bytes(cls, data):
+        pass
+
+    def to_disk(self):
+        pass
+
+    def from_disk(self, path):
+        pass
