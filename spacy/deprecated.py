36bebe716465121ca90850b2d9011f59d3946b4b
ines <ines@ines.io>
2017-05-14 01:30:29 +0200

    Update docstrings

--- spacy/deprecated.py
+++ spacy/deprecated.py
@@ -10,9 +10,9 @@ PRON_LEMMA = "-PRON-"
 
 
 def depr_model_download(lang):
-    """
-    Replace download modules within en and de with deprecation warning and
-    download default language model (using shortcut).
+    """Replace en/de download modules within, warn and ownload default models.
+
+    lang (unicode): Language shortcut, 'en' or 'de'.
     """
     prints("The spacy.%s.download command is now deprecated. Please use "
            "python -m spacy download [model name or shortcut] instead. For "
@@ -24,6 +24,12 @@ def depr_model_download(lang):
 
 
 def resolve_load_name(name, **overrides):
+    """Resolve model loading if deprecated path kwarg is specified in overrides.
+
+    name (unicode): Name of model to load.
+    **overrides: Overrides specified in spacy.load().
+    RETURNS: Model name or value of path kwarg.
+    """
     if overrides.get('path') not in (None, False, True):
         name = overrides.get('path')
         prints("To load a model from a path, you can now use the first argument. "
