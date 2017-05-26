51882c49842c873db75c1f260091349c6295af28
ines <ines@ines.io>
2017-05-26 12:37:45 +0200

    Fix formatting

--- spacy/util.py
+++ spacy/util.py
@@ -174,6 +174,7 @@ def get_async(stream, numpy_array):
         array.set(numpy_array, stream=stream)
         return array
 
+
 def itershuffle(iterable, bufsize=1000):
     """Shuffle an iterator. This works by holding `bufsize` items back
     and yielding them sometime later. Obviously, this is not unbiased --
