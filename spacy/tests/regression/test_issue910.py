96867a24aec5a1bc2378c5237612e520c51ec196
ines <ines@ines.io>
2017-06-04 22:36:40 +0200

    Fix typo

--- spacy/tests/regression/test_issue910.py
+++ spacy/tests/regression/test_issue910.py
@@ -79,7 +79,8 @@ def test_issue910(EN, train_data, additional_entity_types):
     2) There's no way to set the learning rate for the weight update, so we
         end up out-of-scale, causing it to learn too fast.
     '''
-    doc = EN(u"I am looking for a restaurant in Berlin")
+    nlp = EN
+    doc = nlp(u"I am looking for a restaurant in Berlin")
     ents_before_train = [(ent.label_, ent.text) for ent in doc.ents]
     # Fine tune the ner model
     for entity_type in additional_entity_types:
