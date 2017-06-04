9254a3dd78d7cff536b10e69ff0825880af2ba4c
ines <ines@ines.io>
2017-06-04 21:42:15 +0200

    Import and add Spanish syntax iterators

--- spacy/lang/es/__init__.py
+++ spacy/lang/es/__init__.py
@@ -5,6 +5,7 @@ from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .tag_map import TAG_MAP
 from .stop_words import STOP_WORDS
 from .lemmatizer import LOOKUP
+from .syntax_iterators import SYNTAX_ITERATORS
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
@@ -22,6 +23,7 @@ class SpanishDefaults(Language.Defaults):
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     tag_map = dict(TAG_MAP)
     stop_words = set(STOP_WORDS)
+    sytax_iterators = dict(SYNTAX_ITERATORS)
 
     @classmethod
     def create_lemmatizer(cls, nlp=None):
