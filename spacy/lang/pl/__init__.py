28d7f0a672e5286393a71595825d41745c7f80e3
Jim O'Regan <joregan@gmail.com>
2017-06-26 22:38:28 +0100

    reference

--- spacy/lang/pl/__init__.py
+++ spacy/lang/pl/__init__.py
@@ -1,6 +1,7 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
@@ -16,7 +17,7 @@ class Polish(Language):
         lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
         lex_attr_getters[LANG] = lambda text: 'pl'
 
-        tokenizer_exceptions = update_exc(BASE_EXCEPTIONS)
+        tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
         stop_words = set(STOP_WORDS)
 
 
