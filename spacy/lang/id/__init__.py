b80c35bc9afd050948afb31c91f7505ac4c355de
Jim Geovedi <jim@geovedi.com>
2017-07-23 22:54:49 +0700

    added norm_exceptions

--- spacy/lang/id/__init__.py
+++ spacy/lang/id/__init__.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from .stop_words import STOP_WORDS
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .norm_exceptions import NORM_EXCEPTIONS
 from .lex_attrs import LEX_ATTRS
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
