7aad6718bc461c963dc47aabd9e9b80181e775b5
Jim Geovedi <jim@geovedi.com>
2017-07-24 14:11:10 +0700

    enable tokenizer exceptions

--- spacy/lang/id/tokenizer_exceptions.py
+++ spacy/lang/id/tokenizer_exceptions.py
@@ -1,4 +1,11 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-TOKENIZER_EXCEPTIONS = {}
+from ._tokenizer_exceptions_list import FR_BASE_EXCEPTIONS
+
+_exc = {}
+
+for orth in FR_BASE_EXCEPTIONS + ["etc."]:
+    _exc[orth] = [{ORTH: orth}]
+
+TOKENIZER_EXCEPTIONS = dict(_exc)
