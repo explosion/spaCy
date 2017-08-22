a4633fff6fd7f5c84b41c24178c5d8b22aa09724
Matthew Honnibal <honnibal+gh@gmail.com>
2017-08-22 17:01:58 -0500

    Restore use of batch norm in model

--- spacy/_ml.py
+++ spacy/_ml.py
@@ -236,7 +236,7 @@ def Tok2Vec(width, embed_size, preprocess=None):
                 >> uniqued(embed, column=5)
                 >> drop_layer(
                     Residual(
-                        (ExtractWindow(nW=1) >> ReLu(width, width*3))
+                        (ExtractWindow(nW=1) >> BN(Maxout(width, width*3)))
                     )
                 ) ** 4, pad=4
             )
