adda08fe14479e465668bcf39dcedd238a4be20e
ines <ines@ines.io>
2017-09-26 16:39:15 +0200

    Implement like_num getter for Dutch (via #1177)

--- spacy/lang/nl/__init__.py
+++ spacy/lang/nl/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
@@ -12,6 +13,7 @@ from ...util import update_exc, add_lookups
 
 class DutchDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: 'nl'
     lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM], BASE_NORMS)
 
