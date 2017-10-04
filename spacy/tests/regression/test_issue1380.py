db05d4d5823c05208880eda92d99e7ba2de3ddd4
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-04 14:56:16 +0200

    Add test for #1380. Passes without fix?

---
+++ spacy/tests/regression/test_issue1380.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ...language import Language
+
+def test_issue1380_empty_string():
+    nlp = Language()
+    doc = nlp('')
+    assert len(doc) == 0
+
+@pytest.mark.models('en')
+def test_issue1380_en(EN):
+    doc = EN('')
+    assert len(doc) == 0
