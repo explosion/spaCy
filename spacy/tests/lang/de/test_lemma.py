453c47ca24c7e8d3cd71afc3fe2ef4b501c25e27
ines <ines@ines.io>
2017-10-11 13:27:26 +0200

    Add German lemmatizer tests

---
+++ spacy/tests/lang/de/test_lemma.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import pytest
+
+
+@pytest.mark.parametrize('string,lemma', [('Abgehängten', 'Abgehängte'),
+                                          ('engagierte', 'engagieren'),
+                                          ('schließt', 'schließen'),
+                                          ('vorgebenden', 'vorgebend')])
+def test_lemmatizer_lookup_assigns(de_tokenizer, string, lemma):
+    tokens = de_tokenizer(string)
+    assert tokens[0].lemma_ == lemma
