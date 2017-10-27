ea4a41c8fb573e3034daacdae117f39b18e82842
ines <ines@ines.io>
2017-10-27 14:39:09 +0200

    Tidy up util and helpers

--- spacy/deprecated.py
+++ spacy/deprecated.py
@@ -24,7 +24,7 @@ def depr_model_download(lang):
 
 
 def resolve_load_name(name, **overrides):
-    """Resolve model loading if deprecated path kwarg is specified in overrides.
+    """Resolve model loading if deprecated path kwarg in overrides.
 
     name (unicode): Name of model to load.
     **overrides: Overrides specified in spacy.load().
@@ -32,8 +32,9 @@ def resolve_load_name(name, **overrides):
     """
     if overrides.get('path') not in (None, False, True):
         name = overrides.get('path')
-        prints("To load a model from a path, you can now use the first argument. "
-               "The model meta is used to load the required Language class.",
-               "OLD: spacy.load('en', path='/some/path')", "NEW: spacy.load('/some/path')",
+        prints("To load a model from a path, you can now use the first "
+               "argument. The model meta is used to load the Language class.",
+               "OLD: spacy.load('en', path='/some/path')",
+               "NEW: spacy.load('/some/path')",
                title="Warning: deprecated argument 'path'")
     return name
