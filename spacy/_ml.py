807572683851262a8234ddef46eec9fa7bebda6c
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-31 19:21:17 +0100

    Restore vector usage in models

--- spacy/_ml.py
+++ spacy/_ml.py
@@ -208,11 +208,11 @@ class PrecomputableAffine(Model):
 def link_vectors_to_models(vocab):
     vectors = vocab.vectors
     ops = Model.ops
-    #for word in vocab:
-    #    if word.orth in vectors.key2row:
-    #        word.rank = vectors.key2row[word.orth]
-    #    else:
-    #        word.rank = 0
+    for word in vocab:
+        if word.orth in vectors.key2row:
+            word.rank = vectors.key2row[word.orth]
+        else:
+            word.rank = 0
     data = ops.asarray(vectors.data)
     # Set an entry here, so that vectors are accessed by StaticVectors
     # (unideal, I know)
