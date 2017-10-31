00ecfa5417e6ceff9a2ef55ad36ffe475aa2e65b
Jim O'Regan <jaoregan@tcd.ie>
2017-10-31 22:54:42 +0000

    Ó, not O

--- spacy/tests/lang/ga/test_tokenizer.py
+++ spacy/tests/lang/ga/test_tokenizer.py
@@ -5,7 +5,7 @@ import pytest
 
 
 GA_TOKEN_EXCEPTION_TESTS = [
-    ('Niall Ó Domhnaill, Rialtas na hÉireann 1977 (lch. 600).', ['Niall', 'O', 'Domhnaill', ',', 'Rialtas', 'na', 'hÉireann', '1977', '('. 'lch.', '600', ')', '.']),
+    ('Niall Ó Domhnaill, Rialtas na hÉireann 1977 (lch. 600).', ['Niall', 'Ó', 'Domhnaill', ',', 'Rialtas', 'na', 'hÉireann', '1977', '('. 'lch.', '600', ')', '.']),
     ('Daoine a bhfuil Gaeilge acu, m.sh. tusa agus mise', ['Daoine', 'a', 'bhfuil', 'Gaeilge', 'acu', ',', 'm.sh.', 'tusa', 'agus', 'mise'])
 ]
 
