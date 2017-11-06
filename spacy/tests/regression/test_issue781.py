102f7979338b948744b6af06689f928deb72f27c
Matthew Honnibal <honnibal+gh@gmail.com>
2017-11-06 17:02:17 +0100

    Fix lemma ordering in test

--- spacy/tests/regression/test_issue781.py
+++ spacy/tests/regression/test_issue781.py
@@ -6,7 +6,7 @@ import pytest
 
 # Note: "chromosomes" worked previous the bug fix
 @pytest.mark.models('en')
-@pytest.mark.parametrize('word,lemmas', [("chromosomes", ["chromosome"]), ("endosomes", ["endosome"]), ("colocalizes", ["colocalize", "colocaliz"])])
+@pytest.mark.parametrize('word,lemmas', [("chromosomes", ["chromosome"]), ("endosomes", ["endosome"]), ("colocalizes", ["colocaliz", "colocalize"])])
 def test_issue781(EN, word, lemmas):
     lemmatizer = EN.Defaults.create_lemmatizer()
     assert lemmatizer(word, 'noun', morphology={'number': 'plur'}) == lemmas
