d58d6b56641ade095ccf6d99b284890c652a3336
Shirish Kadam <shirishkadam35@yahoo.com>
2017-11-19 00:37:14 +0530

    Added Hindi numerals

--- spacy/lang/hi/lex_attrs.py
+++ spacy/lang/hi/lex_attrs.py
@@ -3,16 +3,35 @@ from __future__ import unicode_literals
 
 from ..norm_exceptions import BASE_NORMS
 from ...attrs import NORM
+from ...attrs import LIKE_NUM
 from ...util import add_lookups
 
 
 _stem_suffixes = [
-    ["ो","े","ू","ु","ी","ि","ा"],
-    ["कर","ाओ","िए","ाई","ाए","ने","नी","ना","ते","ीं","ती","ता","ाँ","ां","ों","ें"],
-    ["ाकर","ाइए","ाईं","ाया","ेगी","ेगा","ोगी","ोगे","ाने","ाना","ाते","ाती","ाता","तीं","ाओं","ाएं","ुओं","ुएं","ुआं"],
-    ["ाएगी","ाएगा","ाओगी","ाओगे","एंगी","ेंगी","एंगे","ेंगे","ूंगी","ूंगा","ातीं","नाओं","नाएं","ताओं","ताएं","ियाँ","ियों","ियां"],
-    ["ाएंगी","ाएंगे","ाऊंगी","ाऊंगा","ाइयाँ","ाइयों","ाइयां"]
+    ["ो", "े", "ू", "ु", "ी", "ि", "ा"],
+    ["कर", "ाओ", "िए", "ाई", "ाए", "ने", "नी", "ना", "ते", "ीं", "ती", "ता", "ाँ", "ां", "ों", "ें"],
+    ["ाकर", "ाइए", "ाईं", "ाया", "ेगी", "ेगा", "ोगी", "ोगे", "ाने", "ाना", "ाते", "ाती", "ाता", "तीं", "ाओं", "ाएं", "ुओं", "ुएं", "ुआं"],
+    ["ाएगी", "ाएगा", "ाओगी", "ाओगे", "एंगी", "ेंगी", "एंगे", "ेंगे", "ूंगी", "ूंगा", "ातीं", "नाओं", "नाएं", "ताओं", "ताएं", "ियाँ", "ियों", "ियां"],
+    ["ाएंगी", "ाएंगे", "ाऊंगी", "ाऊंगा", "ाइयाँ", "ाइयों", "ाइयां"]
 ]
+
+_num_words = ["शून्य", "एक", "दो", "तीन", "चार", "पांच", "छह", "सात", "आठ", "नौ", "दस", "ग्यारह", "बारह", "तेरह", "चौदह", "पंद्रह",
+              "सोलह", "सत्रह", "अठारह", "उन्नीस", "बीस", "तीस", "चालीस", "पचास", "साठ", "सत्तर", "अस्सी", "नब्बे", "सौ", "हज़ार",
+              "लाख", "करोड़", "अरब", "खरब", "नील", "पद्म", "शङ्ख", "गुलशन"]
+
+
+"""
+For Hindi numerals the prefix remains the same for every 10th digit eg. 12, 22, 32, 42 (बारह, बाईस, बतीस, बयालीस...).
+Here, the prefix ब or बा , although 89 and 99 are exception from 19 to 79.
+Note: Indian Numbering System is based on the Vedic numbering system in which numbers over 9,999 are written in 
+two-digit groups rather than the three-digit groups used in most other parts of the world. (1,00,000 = 1 Lakh)
+1 arab = 1 billion
+1 kharab = 100 billion
+1 neel = 10 trillion
+1 padma = 1 quadrillion
+1 sankh = 1 hundred quadrillion
+1 gulshan = 1 quintillion
+"""
 
 
 def norm(string):
@@ -33,6 +52,20 @@ def norm(string):
     return string
 
 
+def like_num(text):
+    text = text.replace(',', '').replace('.', '')
+    if text.isdigit():
+        return True
+    if text.count('/') == 1:
+        num, denom = text.split('/')
+        if num.isdigit() and denom.isdigit():
+            return True
+    if text in _num_words:
+        return True
+    return False
+
+
 LEX_ATTRS = {
-    NORM: norm
+    NORM: norm,
+    LIKE_NUM: like_num
 }
