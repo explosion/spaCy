2e50dbb9d72416e70c45022365e587b424806eea
ines <ines@ines.io>
2017-11-28 20:27:27 +0100

    Simplify test

--- spacy/tests/regression/test_issue1612.py
+++ spacy/tests/regression/test_issue1612.py
@@ -1,12 +1,8 @@
+# coding: utf8
 from __future__ import unicode_literals
 
-import pytest
 
-from ...lang.en import English
-
-
-def test_issue1612():
-    nlp = English()
-    doc = nlp('The black cat purrs.')
+def test_issue1612(en_tokenizer):
+    doc = en_tokenizer('The black cat purrs.')
     span = doc[1: 3]
     assert span.orth_ == span.text
