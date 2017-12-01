f2ea6d4713fe32048e26c3992202cbd431bb3203
ines <ines@ines.io>
2017-12-01 23:36:05 +0100

    Add Dutch example sentences (see #1107)

---
+++ spacy/lang/nl/examples.py
@@ -0,0 +1,18 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.nl.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+examples = [
+    "Apple overweegt om voor 1 miljard een U.K. startup te kopen",
+    "Autonome auto's verschuiven de verzekeringverantwoordelijkheid naar producenten",
+    "San Francisco overweegt robots op voetpaden te verbieden",
+    "Londen is een grote stad in het Verenigd Koninkrijk"
+]
