f1d3deffac8ad7bcc64626b25f5d82c25ee24924
ines <ines@ines.io>
2018-02-01 20:09:40 +0100

    Add Russian example sentences (see #1107)

---
+++ spacy/lang/ru/examples.py
@@ -0,0 +1,18 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.ru.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple рассматривает возможность покупки стартапа из Соединенного Королевства за $1 млрд",
+    "Автономные автомобили переносят страховую ответственность на производителя",
+    "В Сан Франциско рассматривается возможность запрета роботов-курьеров, которые перемещаются по тротуару",
+    "Лондон - большой город Соединенного Королевства"
+]
