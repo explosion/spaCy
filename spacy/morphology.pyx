95f0673fbc92dd21be8a8b8b856a8e9ccdf1730d
Jim O'Regan <jaoregan@tcd.ie>
2018-02-18 14:38:27 +0000

    fix typo/missing here too

--- spacy/morphology.pyx
+++ spacy/morphology.pyx
@@ -184,7 +184,9 @@ cdef class Morphology:
 
 IDS = {
     "Animacy_anim": Animacy_anim,
-    "Animacy_inam": Animacy_inam,
+    "Animacy_inan": Animacy_inan,
+    "Animacy_hum": Animacy_hum, # U20
+    "Animacy_nhum": Animacy_nhum,
     "Aspect_freq": Aspect_freq,
     "Aspect_imp": Aspect_imp,
     "Aspect_mod": Aspect_mod,
