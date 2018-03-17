53eb0c3674063ccbfe79c602d1200ea78a7d0eb4
Matthew Honnibal <honnibal+gh@gmail.com>
2018-03-17 03:57:58 +0100

    Remove rapidjson requirement

--- setup.py
+++ setup.py
@@ -194,7 +194,6 @@ def setup_package():
                 'plac<1.0.0,>=0.9.6',
                 'pathlib',
                 'ujson>=1.35',
-                'rapidjson==0.4.3',
                 'dill>=0.2,<0.3',
                 'requests>=2.13.0,<3.0.0',
                 'regex==2017.4.5',
