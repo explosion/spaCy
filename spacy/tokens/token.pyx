9b49a40f4e01dacb314007ab2b3f73ae93c654a8
Douglas Knox <knoxdw@gmail.com>
2018-05-03 11:40:46 -0500

    Test and fix for Issue #2219 (#2272)
    Test and fix for Issue #2219: Token.similarity() failed if single letter

--- spacy/tokens/token.pyx
+++ spacy/tokens/token.pyx
@@ -155,7 +155,7 @@ cdef class Token:
         """
         if 'similarity' in self.doc.user_token_hooks:
             return self.doc.user_token_hooks['similarity'](self)
-        if hasattr(other, '__len__') and len(other) == 1:
+        if hasattr(other, '__len__') and len(other) == 1 and hasattr(other, "__getitem__"):
             if self.c.lex.orth == getattr(other[0], 'orth', None):
                 return 1.0
         elif hasattr(other, 'orth'):
