7b09a4ca499aaf27119933d5896682713e003876
Matthew Honnibal <honnibal+gh@gmail.com>
2018-07-05 13:56:02 +0200

    Fix lemmatization

--- spacy/morphology.pyx
+++ spacy/morphology.pyx
@@ -176,7 +176,7 @@ cdef class Morphology:
         cdef list lemma_strings
         cdef unicode lemma_string
         lemma_strings = self.lemmatizer(py_string, univ_pos, morphology)
-        lemma_string = sorted(lemma_strings)[0]
+        lemma_string = lemma_strings[0]
         lemma = self.strings.add(lemma_string)
         return lemma
 
