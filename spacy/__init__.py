19f5046934cc384a4be5dda44d2ade95c3264b82
Matthew Honnibal <honnibal+gh@gmail.com>
2018-08-10 00:13:34 +0200

    Undoing warning suppression, as doesnt really work

--- spacy/__init__.py
+++ spacy/__init__.py
@@ -1,17 +1,10 @@
 # coding: utf8
 from __future__ import unicode_literals
-import warnings
-
-# This is used to suppress numpy runtime warnings, which warn about irrelevant
-# binary incompatibility. We could pin to a specific numpy version, but then
-# we risk entering dependency hell if other packages pin to different constraints.
-# Ideally we would somehow specify the warning to suppress?
-with warnings.catch_warnings(record=True) as w:
-    from .cli.info import info as cli_info
-    from .glossary import explain
-    from .about import __version__
-    from .errors import Warnings, deprecation_warning
-    from . import util
+from .cli.info import info as cli_info
+from .glossary import explain
+from .about import __version__
+from .errors import Warnings, deprecation_warning
+from . import util
 
 
 def load(name, **overrides):
