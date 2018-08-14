fe94e696d3dc5abdfb846d152ebf489518419513
Ioannis Daras <daras.giannhs@gmail.com>
2018-08-14 03:31:32 +0300

    Optimize Greek language support (#2658)

--- spacy/lang/el/__init__.py
+++ spacy/lang/el/__init__.py
@@ -7,6 +7,8 @@ from .tag_map_general import TAG_MAP
 from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
 from .lemmatizer import LEMMA_RULES, LEMMA_INDEX, LEMMA_EXC
+from .lemmatizer.lemmatizer import GreekLemmatizer
+from .syntax_iterators import SYNTAX_ITERATORS
 from .punctuation import TOKENIZER_PREFIXES, TOKENIZER_SUFFIXES, TOKENIZER_INFIXES
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from .norm_exceptions import NORM_EXCEPTIONS
@@ -20,15 +22,23 @@ class GreekDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: 'el'  # ISO code
-    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM], BASE_NORMS, NORM_EXCEPTIONS)
+    lex_attr_getters[NORM] = add_lookups(
+        Language.Defaults.lex_attr_getters[NORM], BASE_NORMS, NORM_EXCEPTIONS)
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     stop_words = STOP_WORDS
-    lemma_rules = LEMMA_RULES
-    lemma_index = LEMMA_INDEX
     tag_map = TAG_MAP
     prefixes = TOKENIZER_PREFIXES
     suffixes = TOKENIZER_SUFFIXES
     infixes = TOKENIZER_INFIXES
+    syntax_iterators = SYNTAX_ITERATORS
+
+    @classmethod
+    def create_lemmatizer(cls, nlp=None):
+        lemma_rules = LEMMA_RULES
+        lemma_index = LEMMA_INDEX
+        lemma_exc = LEMMA_EXC
+        return GreekLemmatizer(index=lemma_index, exceptions=lemma_exc,
+                               rules=lemma_rules)
 
 
 class Greek(Language):
@@ -39,4 +49,3 @@ class Greek(Language):
 
 # set default export – this allows the language class to be lazy-loaded
 __all__ = ['Greek']
-
---
+++ spacy/lang/el/lemmatizer/lemmatizer.py
@@ -0,0 +1,69 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from ....symbols import NOUN, VERB, ADJ, PUNCT
+
+'''
+Greek language lemmatizer applies the default rule based lemmatization
+procedure with some modifications for better Greek language support.
+
+The first modification is that it checks if the word for lemmatization is
+already a lemma and if yes, it just returns it.
+The second modification is about removing the base forms function which is
+not applicable for Greek language.
+'''
+
+
+class GreekLemmatizer(object):
+    @classmethod
+    def load(cls, path, index=None, exc=None, rules=None, lookup=None):
+        return cls(index, exc, rules, lookup)
+
+    def __init__(self, index=None, exceptions=None, rules=None, lookup=None):
+        self.index = index
+        self.exc = exceptions
+        self.rules = rules
+        self.lookup_table = lookup if lookup is not None else {}
+
+    def __call__(self, string, univ_pos, morphology=None):
+        if not self.rules:
+            return [self.lookup_table.get(string, string)]
+        if univ_pos in (NOUN, 'NOUN', 'noun'):
+            univ_pos = 'noun'
+        elif univ_pos in (VERB, 'VERB', 'verb'):
+            univ_pos = 'verb'
+        elif univ_pos in (ADJ, 'ADJ', 'adj'):
+            univ_pos = 'adj'
+        elif univ_pos in (PUNCT, 'PUNCT', 'punct'):
+            univ_pos = 'punct'
+        else:
+            return list(set([string.lower()]))
+        lemmas = lemmatize(string, self.index.get(univ_pos, {}),
+                           self.exc.get(univ_pos, {}),
+                           self.rules.get(univ_pos, []))
+        return lemmas
+
+
+def lemmatize(string, index, exceptions, rules):
+    string = string.lower()
+    forms = []
+    if (string in index):
+        forms.append(string)
+        return forms
+    forms.extend(exceptions.get(string, []))
+    oov_forms = []
+    if not forms:
+        for old, new in rules:
+            if string.endswith(old):
+                form = string[:len(string) - len(old)] + new
+                if not form:
+                    pass
+                elif form in index or not form.isalpha():
+                    forms.append(form)
+                else:
+                    oov_forms.append(form)
+    if not forms:
+        forms.extend(oov_forms)
+    if not forms:
+        forms.append(string)
+    return list(set(forms))
