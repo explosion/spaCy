bda9d2f32bc43d9be4b191f4e519a0288f50bed1
Sainath Adapa <adapasainath@gmail.com>
2018-09-06 18:05:00 +0200

    Basic support for Telugu language

---
+++ spacy/lang/te/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
+
+from ...language import Language
+from ...attrs import LANG
+
+
+class TeluguDefaults(Language.Defaults):
+    lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters.update(LEX_ATTRS)
+    lex_attr_getters[LANG] = lambda text: 'te'
+    stop_words = STOP_WORDS
+
+
+class Telugu(Language):
+    lang = 'te'
+    Defaults = TeluguDefaults
+
+
+__all__ = ['Telugu']
