dfef1f310de79ee721c4d1554df4539d7ec823bb
mateusztybura <tyburam@hotmail.com>
2018-09-08 18:45:46 +0200

    Added polish version of english lex_attrs

---
+++ spacy/lang/pl/lex_attrs.py
@@ -0,0 +1,32 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from ...attrs import LIKE_NUM
+
+
+_num_words = ['zero', 'jeden', 'dwa', 'trzy', 'cztery', 'pięć', 'sześć', 
+              'siedem', 'osiem', 'dziewięć', 'dziesięć', 'jedenaście', 
+              'dwanaście', 'trzynaście', 'czternaście',
+              'pietnaście', 'szesnaście', 'siedemnaście', 'osiemnaście',
+              'dziewiętnaście', 'dwadzieścia', 'trzydzieści', 'czterdzieści', 
+              'pięćdziesiąt', 'szcześćdziesiąt', 'siedemdziesiąt', 
+              'osiemdziesiąt', 'dziewięćdziesiąt', 'sto', 'tysiąc', 'milion', 
+              'miliard', 'bilion', 'trylion']
+
+
+def like_num(text):
+    text = text.replace(',', '').replace('.', '')
+    if text.isdigit():
+        return True
+    if text.count('/') == 1:
+        num, denom = text.split('/')
+        if num.isdigit() and denom.isdigit():
+            return True
+    if text.lower() in _num_words:
+        return True
+    return False
+
+
+LEX_ATTRS = {
+    LIKE_NUM: like_num
+}
