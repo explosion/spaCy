8f0ab3785dc3a2a4cafb0c405aa4dbf7a45a64a6
mauryaland <amaury@fouret.org>
2018-10-02 19:37:19 +0200

    add auxiliary verbs  pronouns

--- spacy/lang/fr/lemmatizer/__init__.py
+++ spacy/lang/fr/lemmatizer/__init__.py
@@ -10,11 +10,14 @@ from ._nouns_irreg import NOUNS_IRREG
 from ._verbs import VERBS
 from ._verbs_irreg import VERBS_IRREG
 from ._dets_irreg import DETS_IRREG
-from ._lemma_rules import ADJECTIVE_RULES, NOUN_RULES
+from ._pronouns_irreg import PRONOUNS_IRREG
+from ._auxiliary_verbs_irreg import AUXILIARY_VERBS_IRREG
+from ._lemma_rules import ADJECTIVE_RULES, NOUN_RULES, VERB_RULES
 
 
 LEMMA_INDEX = {'adj': ADJECTIVES, 'adv': ADVERBS, 'noun': NOUNS, 'verb': VERBS}
 
-LEMMA_EXC = {'adj': ADJECTIVES_IRREG, 'noun': NOUNS_IRREG, 'verb': VERBS_IRREG, 'det': DETS_IRREG}
+LEMMA_EXC = {'adj': ADJECTIVES_IRREG, 'noun': NOUNS_IRREG, 'verb': VERBS_IRREG, 
+             'det': DETS_IRREG, 'pron': PRONOUNS_IRREG, 'aux': AUXILIARY_VERBS_IRREG}
 
-LEMMA_RULES = {'adj': ADJECTIVE_RULES, 'noun': NOUN_RULES}
+LEMMA_RULES = {'adj': ADJECTIVE_RULES, 'noun': NOUN_RULES, 'verb': VERB_RULES}
