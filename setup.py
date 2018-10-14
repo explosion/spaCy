2e675d9523064e27778d45b4efe7f7e6e787bdb3
Ines Montani <ines@ines.io>
2018-10-14 16:37:38 +0200

    Update murmurhash pin

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ def setup_package():
             install_requires=[
                 'numpy>=1.15.0',
                 'msgpack-numpy<0.4.4.0'
-                'murmurhash>=0.28,<0.29',
+                'murmurhash>=0.28.0,<1.1.0',
                 'cymem>=1.30,<1.32',
                 'preshed>=1.0.0,<2.0.0',
                 'thinc>=6.10.3,<6.11.0',
