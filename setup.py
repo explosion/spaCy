fd750ec3bfb755d21519e5e83dbf68c4f2295d1b
Ines Montani <ines@ines.io>
2018-10-15 14:18:38 +0200

    Fix msgpack-numpy version pin

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ def setup_package():
             setup_requires=['wheel>=0.32.0,<0.33.0'],
             install_requires=[
                 'numpy>=1.15.0',
-                'msgpack-numpy<0.4.4.0'
+                'msgpack-numpy<0.4.4',
                 'murmurhash>=0.28.0,<1.1.0',
                 'cymem>=2.0.2,<2.1.0',
                 'preshed>=2.0.1,<2.1.0',
