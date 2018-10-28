69ec51a9ac42fc598ca028922b49dfab52c22ba7
Stanisław Giziński <s.gizinski84@gmail.com>
2018-10-28 16:39:00 +0100

    Improved stop words list

--- spacy/lang/pl/stop_words.py
+++ spacy/lang/pl/stop_words.py
@@ -1,46 +1,83 @@
 # encoding: utf8
+
 from __future__ import unicode_literals
 
-
-# Source: http://www.ranks.nl/stopwords/polish
+#sources: https://github.com/bieli/stopwords/blob/master/polish.stopwords.txt and https://github.com/stopwords-iso/stopwords-pl
 
 STOP_WORDS = set("""
-ach aj albo
+ a aby ach acz aczkolwiek aj albo ale alez
+ależ ani az aż
 
-bardzo bez bo być
+bardziej bardzo beda bede bedzie bez bo bowiem by
+byc byl byla byli bylo byly bym bynajmniej być był
+była było były będzie będą będę
 
-ci cię ciebie co czy
+cala cali caly cała cały chce choć ci cie
+ciebie cię co cokolwiek coraz cos coś czasami czasem czemu
+czy czyli często
 
-daleko dla dlaczego dlatego do dobrze dokąd dość dużo dwa dwaj dwie dwoje dziś
-dzisiaj
+daleko deda dla dlaczego dlatego do dobrze dokad dokąd
+dosc dość dr duzo dużo dwa dwaj dwie dwoje dzis
+dzisiaj dziś
 
-gdyby gdzie
+gdy gdyby gdyz gdyż gdzie gdziekolwiek gdzies gdzieś go
+godz
 
-go
+hab
 
-ich ile im inny
+i ich ii iii ile im inna inne inny
+innych inż iv ix iz iż
 
-ja ją jak jakby jaki je jeden jedna jedno jego jej jemu jeśli jest jestem
-jeżeli już
+ja jak jakas jakaś jakby jaki jakichs jakichś jakie
+jakis jakiz jakiś jakiż jakkolwiek jako jakos jakoś je jeden
+jedna jednak jednakze jednakże jedno jednym jedynie jego jej jemu
+jesli jest jestem jeszcze jezeli jeśli jeżeli juz już ją
 
-każdy kiedy kierunku kto ku
+kazdy każdy kiedy kierunku kilka kilku kims kimś kto
+ktokolwiek ktora ktore ktorego ktorej ktory ktorych ktorym ktorzy ktos
+ktoś która które którego której który których którym którzy ku
 
-lub
+lat lecz lub
 
-ma mają mam mi mną mnie moi mój moja moje może mu my
+ma mają mam mamy mało mgr mi miał miedzy
+mimo między mna mnie mną moga mogą moi moim moj
+moja moje moze mozliwe mozna może możliwe można mu musi
+my mój
 
-na nam nami nas nasi nasz nasza nasze natychmiast nią nic nich nie niego niej
-niemu nigdy nim nimi niż
+na nad nam nami nas nasi nasz nasza nasze
+naszego naszych natomiast natychmiast nawet nia nic nich nie niech
+niego niej niemu nigdy nim nimi niz nią niż no
+nowe np nr
 
-obok od około on ona one oni ono owszem
+o o.o. obok od ok około on ona one
+oni ono oraz oto owszem
 
-po pod ponieważ przed przedtem
+pan pana pani pl po pod podczas pomimo ponad
+poniewaz ponieważ powinien powinna powinni powinno poza prawie prof przeciez
+przecież przed przede przedtem przez przy
 
-są sam sama się skąd
+raz razie roku rowniez również
 
-tak taki tam ten to tobą tobie tu tutaj twoi twój twoja twoje ty
+sam sama sie się skad skąd soba sobie sobą
+sposob sposób swoje są
 
-wam wami was wasi wasz wasza wasze we więc wszystko wtedy wy
+ta tak taka taki takich takie takze także tam
+te tego tej tel temu ten teraz też to toba
+tobie tobą totez toteż totobą trzeba tu tutaj twoi twoim
+twoj twoja twoje twym twój ty tych tylko tym tys
+tzw tę
 
-żaden zawsze że
-""".split())
+u ul
+
+vi vii viii vol
+
+w wam wami was wasi wasz wasza wasze we
+według wie wiele wielu więc więcej wlasnie wszyscy wszystkich wszystkie
+wszystkim wszystko wtedy www wy właśnie wśród
+
+xi xii xiii xiv xv
+
+z za zaden zadna zadne zadnych zapewne zawsze zaś
+ze zeby zeznowu znow znowu znów zostal został zł
+
+żaden żadna żadne żadnych że żeby""".split())
