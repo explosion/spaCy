562ef2b5dd23079a373788eaf8bbe5b5208e7a75
Stanisław Giziński <s.gizinski84@gmail.com>
2018-11-04 00:50:52 +0100

    Removed some wrong stop words form list

--- spacy/lang/pl/stop_words.py
+++ spacy/lang/pl/stop_words.py
@@ -16,17 +16,15 @@ cala cali caly cała cały chce choć ci cie
 ciebie cię co cokolwiek coraz cos coś czasami czasem czemu
 czy czyli często
 
-daleko deda dla dlaczego dlatego do dobrze dokad dokąd
-dosc dość dr duzo dużo dwa dwaj dwie dwoje dzis
+daleko dla dlaczego dlatego do dobrze dokad dokąd
+dosc dość duzo dużo dwa dwaj dwie dwoje dzis
 dzisiaj dziś
 
 gdy gdyby gdyz gdyż gdzie gdziekolwiek gdzies gdzieś go
 godz
 
-hab
-
-i ich ii iii ile im inna inne inny
-innych inż iv ix iz iż
+i ich ile im inna inne inny
+innych iv ix iz iż
 
 ja jak jakas jakaś jakby jaki jakichs jakichś jakie
 jakis jakiz jakiś jakiż jakkolwiek jako jakos jakoś je jeden
@@ -37,23 +35,22 @@ kazdy każdy kiedy kierunku kilka kilku kims kimś kto
 ktokolwiek ktora ktore ktorego ktorej ktory ktorych ktorym ktorzy ktos
 ktoś która które którego której który których którym którzy ku
 
-lat lecz lub
+lecz lub
 
-ma mają mam mamy mało mgr mi miał miedzy
+ma mają mam mamy mało mi miał miedzy
 mimo między mna mnie mną moga mogą moi moim moj
 moja moje moze mozliwe mozna może możliwe można mu musi
 my mój
 
 na nad nam nami nas nasi nasz nasza nasze
 naszego naszych natomiast natychmiast nawet nia nic nich nie niech
-niego niej niemu nigdy nim nimi niz nią niż no
-nowe np nr
+niego niej niemu nigdy nim nimi niz nią niż no 
 
-o o.o. obok od ok około on ona one
+o obok od ok około on ona one
 oni ono oraz oto owszem
 
-pan pana pani pl po pod podczas pomimo ponad
-poniewaz ponieważ powinien powinna powinni powinno poza prawie prof przeciez
+pan pana pani po pod podczas pomimo ponad
+poniewaz ponieważ powinien powinna powinni powinno poza prawie przeciez
 przecież przed przede przedtem przez przy
 
 raz razie roku rowniez również
@@ -67,17 +64,17 @@ tobie tobą totez toteż totobą trzeba tu tutaj twoi twoim
 twoj twoja twoje twym twój ty tych tylko tym tys
 tzw tę
 
-u ul
+u
 
-vi vii viii vol
+vi vii viii
 
 w wam wami was wasi wasz wasza wasze we
 według wie wiele wielu więc więcej wlasnie wszyscy wszystkich wszystkie
-wszystkim wszystko wtedy www wy właśnie wśród
+wszystkim wszystko wtedy wy właśnie wśród
 
 xi xii xiii xiv xv
 
 z za zaden zadna zadne zadnych zapewne zawsze zaś
-ze zeby zeznowu znow znowu znów zostal został zł
+ze zeby znow znowu znów zostal został
 
 żaden żadna żadne żadnych że żeby""".split())
