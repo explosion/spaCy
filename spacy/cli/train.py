e0197f2e0bf6968edf0d1d262b374a7c22f970c8
Ines Montani <ines@ines.io>
2018-11-15 23:52:56 +0100

    WIP: Update spacy train

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -10,55 +10,60 @@ import json
 import shutil
 
 from ._messages import Messages
+from .._ml import create_default_optimizer
 from ..attrs import PROB, IS_OOV, CLUSTER, LANG
 from ..gold import GoldCorpus
-from ..util import prints, minibatch, minibatch_by_words
+from ..util import prints, minibatch, minibatch_by_words, write_json
 from .. import util
 from .. import about
-from .. import displacy
-from ..compat import json_dumps
+
+
+# Take dropout and batch size as generators of values -- dropout
+# starts high and decays sharply, to force the optimizer to explore.
+# Batch size starts at 1 and grows, so that we make updates quickly
+# at the beginning of training.
+dropout_rates = util.decaying(util.env_opt('dropout_from', 0.2),
+                              util.env_opt('dropout_to', 0.2),
+                              util.env_opt('dropout_decay', 0.0))
+batch_sizes = util.compounding(util.env_opt('batch_from', 1000),
+                               util.env_opt('batch_to', 1000),
+                               util.env_opt('batch_compound', 1.001))
 
 
 @plac.annotations(
     lang=("model language", "positional", None, str),
-    output_dir=("output directory to store model in", "positional", None, str),
-    train_data=("location of JSON-formatted training data", "positional",
-                None, str),
-    dev_data=("location of JSON-formatted development data (optional)",
-              "positional", None, str),
+    output_path=("output directory to store model in", "positional", None, Path),
+    train_path=("location of JSON-formatted training data", "positional", None, Path),
+    dev_path=("location of JSON-formatted development data", "positional", None, Path),
+    base_model=("name of model to update (optional)", "option", "b", str),
+    pipeline=("Comma-separated names of pipeline components to train", "option", "p", str),
+    vectors=("Model to load vectors from", "option", "v", str),
     n_iter=("number of iterations", "option", "n", int),
-    n_sents=("number of sentences", "option", "ns", int),
+    n_examples=("number of examples", "option", "ns", int),
     use_gpu=("Use GPU", "option", "g", int),
-    vectors=("Model to load vectors from", "option", "v"),
-    no_tagger=("Don't train tagger", "flag", "T", bool),
-    no_parser=("Don't train parser", "flag", "P", bool),
-    no_entities=("Don't train NER", "flag", "N", bool),
-    parser_multitasks=("Side objectives for parser CNN, e.g. dep dep,tag", "option", "pt", str),
-    noise_level=("Amount of corruption to add for data augmentation", "option", "nl", float),
-    entity_multitasks=("Side objectives for ner CNN, e.g. dep dep,tag", "option", "et", str),
-    gold_preproc=("Use gold preprocessing", "flag", "G", bool),
     version=("Model version", "option", "V", str),
-    meta_path=("Optional path to meta.json. All relevant properties will be "
-               "overwritten.", "option", "m", Path),
-    verbose=("Display more information for debug", "option", None, bool))
-def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
-          parser_multitasks='', entity_multitasks='',
-          use_gpu=-1, vectors=None, no_tagger=False, noise_level=0.0,
-          no_parser=False, no_entities=False, gold_preproc=False,
-          version="0.0.0", meta_path=None, verbose=False):
-    """
-    Train a model. Expects data in spaCy's JSON format.
-    """
+    meta_path=("Optional path to meta.json. All relevant properties will be overwritten.", "option", "m", Path),
+    parser_multitasks=("Side objectives for parser CNN, e.g. dep dep,tag", "option", "pt", str),
+    entity_multitasks=("Side objectives for ner CNN, e.g. dep dep,tag", "option", "et", str),
+    noise_level=("Amount of corruption to add for data augmentation", "option", "nl", float),
+    gold_preproc=("Use gold preprocessing", "flag", "G", bool),
+    learn_tokens=("Make parser learn gold-standard tokenization", "flag", "T", bool),
+    verbose=("Display more information for debug", "flag", "vv", bool),
+    debug=("Run data diagnostics before training", "flag", "D", bool)
+)
+def train(lang, output_path, train_path, dev_path, base_model=None,
+          pipeline='tagger,parser,ner', vectors=None, n_iter=30, n_examples=0,
+          use_gpu=-1, version="0.0.0", meta_path=None, parser_multitasks='',
+          entity_multitasks='', noise_level=0.0, gold_preproc=False,
+          learn_tokens=False, verbose=False, debug=False):
+
     util.fix_random_seed()
-    util.set_env_log(True)
-    n_sents = n_sents or None
-    output_path = util.ensure_path(output_dir)
-    train_path = util.ensure_path(train_data)
-    dev_path = util.ensure_path(dev_data)
-    meta_path = util.ensure_path(meta_path)
+    util.set_env_log(verbose)
+
+    # Make sure all files and paths exists if they are needed
     if not train_path.exists():
         prints(train_path, title=Messages.M050, exits=1)
-    if dev_path and not dev_path.exists():
+    if not dev_path.exists():
         prints(dev_path, title=Messages.M051, exits=1)
     if meta_path is not None and not meta_path.exists():
         prints(meta_path, title=Messages.M020, exits=1)
@@ -66,63 +71,59 @@ def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
     if not isinstance(meta, dict):
         prints(Messages.M053.format(meta_type=type(meta)),
                title=Messages.M052, exits=1)
-    meta.setdefault('lang', lang)
-    meta.setdefault('name', 'unnamed')
-
     if not output_path.exists():
         output_path.mkdir()
 
-    print("Counting training words (limit=%s" % n_sents)
-    corpus = GoldCorpus(train_path, dev_path, limit=n_sents)
-    n_train_words = corpus.count_train()
-    print(n_train_words)
-    pipeline = ['tagger', 'parser', 'ner']
-    if no_tagger and 'tagger' in pipeline:
-        pipeline.remove('tagger')
-    if no_parser and 'parser' in pipeline:
-        pipeline.remove('parser')
-    if no_entities and 'ner' in pipeline:
-        pipeline.remove('ner')
+    # Set up the base model and pipeline. If a base model is specified, load
+    # the model and make sure the pipeline matches the pipeline setting. If
+    # training starts from a blank model, intitalize the language class.
+    pipeline = [p.strip() for p in pipeline.split(',')]
+    print("Training pipeline: {}".format(pipeline))
+    if base_model:
+        print("Starting with base model '{}'".format(base_model))
+        nlp = util.load_model(base_model)
+        other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipeline]
+        nlp.disable_pipes(*other_pipes)
+        for pipe in pipeline:
+            if pipe not in nlp.pipe_names:
+                nlp.add_pipe(nlp.create_pipe(pipe))
+    else:
+        print("Starting with blank model '{}'".format(lang))
+        lang_cls = util.get_lang_class(lang)
+        nlp = lang_cls()
+        for pipe in pipeline:
+            nlp.add_pipe(nlp.create_pipe(pipe))
 
-    # Take dropout and batch size as generators of values -- dropout
-    # starts high and decays sharply, to force the optimizer to explore.
-    # Batch size starts at 1 and grows, so that we make updates quickly
-    # at the beginning of training.
-    dropout_rates = util.decaying(util.env_opt('dropout_from', 0.2),
-                                  util.env_opt('dropout_to', 0.2),
-                                  util.env_opt('dropout_decay', 0.0))
-    batch_sizes = util.compounding(util.env_opt('batch_from', 1000),
-                                   util.env_opt('batch_to', 1000),
-                                   util.env_opt('batch_compound', 1.001))
-    lang_class = util.get_lang_class(lang)
-    nlp = lang_class()
-    meta['pipeline'] = pipeline
-    nlp.meta.update(meta)
+    if learn_tokens:
+        nlp.add_pipe(nlp.create_pipe('merge_subtokens'))
+
     if vectors:
-        print("Load vectors model", vectors)
-        util.load_model(vectors, vocab=nlp.vocab)
-        for lex in nlp.vocab:
-            values = {}
-            for attr, func in nlp.vocab.lex_attr_getters.items():
-                # These attrs are expected to be set by data. Others should
-                # be set by calling the language functions.
-                if attr not in (CLUSTER, PROB, IS_OOV, LANG):
-                    values[lex.vocab.strings[attr]] = func(lex.orth_)
-            lex.set_attrs(**values)
-            lex.is_oov = False
-    for name in pipeline:
-        nlp.add_pipe(nlp.create_pipe(name), name=name)
-    nlp.add_pipe(nlp.create_pipe('merge_subtokens'))
-    if parser_multitasks:
-        for objective in parser_multitasks.split(','):
-            nlp.parser.add_multitask_objective(objective)
-    if entity_multitasks:
-        for objective in entity_multitasks.split(','):
-            nlp.entity.add_multitask_objective(objective)
-    optimizer = nlp.begin_training(lambda: corpus.train_tuples, device=use_gpu)
+        _load_vectors(nlp, vectors)
+
+    # Multitask objectives
+    for pipe_name, multitasks in [('parser', parser_multitasks),
+                                  ('ner', entity_multitasks)]:
+        if multitasks:
+            if not pipe_name in pipeline:
+                raise ValueError("Needs '{}' in pipeline".format(pipe_name))
+            pipe = nlp.get_pipe(pipe_name)
+            for objective in multitasks.split(','):
+                pipe.add_multitask_objective(objective)
+
+    # Prepare training corpus
+    print("Counting training words (limit={})".format(n_examples))
+    corpus = GoldCorpus(train_path, dev_path, limit=n_examples)
+    n_train_words = corpus.count_train()
+
+    if base_model:
+        # Start with an existing model, use default optimizer
+        optimizer = create_default_optimizer(Model.ops)
+    else:
+        # Start with a blank model, call begin_training
+        optimizer = nlp.begin_training(lambda: corpus.train_tuples, device=use_gpu)
     nlp._optimizer = None
 
-    print("Itn.  Dep Loss  NER Loss  UAS     NER P.  NER R.  NER F.  Tag %   Token %  CPU WPS  GPU WPS")
+    print("\nItn.  Dep Loss  NER Loss  UAS     NER P.  NER R.  NER F.  Tag %   Token %  CPU WPS  GPU WPS")
     try:
         for i in range(n_iter):
             train_docs = corpus.train_docs(nlp, noise_level=noise_level,
@@ -148,7 +149,7 @@ def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
                                 gold_preproc=gold_preproc))
                 nwords = sum(len(doc_gold[0]) for doc_gold in dev_docs)
                 start_time = timer()
-                scorer = nlp_loaded.evaluate(dev_docs, verbose)
+                scorer = nlp_loaded.evaluate(dev_docs, debug)
                 end_time = timer()
                 if use_gpu < 0:
                     gpu_wps = None
@@ -163,40 +164,50 @@ def train(lang, output_dir, train_data, dev_data, n_iter=30, n_sents=0,
                         scorer = nlp_loaded.evaluate(dev_docs)
                         end_time = timer()
                         cpu_wps = nwords/(end_time-start_time)
-                acc_loc = (output_path / ('model%d' % i) / 'accuracy.json')
-                with acc_loc.open('w') as file_:
-                    file_.write(json_dumps(scorer.scores))
-                meta_loc = output_path / ('model%d' % i) / 'meta.json'
+                acc_loc = output_path / ('model%d' % i) / 'accuracy.json'
+                write_json(acc_loc, scorer.scores)
+
+                # Update model meta.json
+                meta['lang'] = nlp.lang
+                meta['pipeline'] = nlp.pipe_names
+                meta['spacy_version'] = '>=%s' % about.__version__
                 meta['accuracy'] = scorer.scores
                 meta['speed'] = {'nwords': nwords, 'cpu': cpu_wps,
                                  'gpu': gpu_wps}
                 meta['vectors'] = {'width': nlp.vocab.vectors_length,
                                    'vectors': len(nlp.vocab.vectors),
                                    'keys': nlp.vocab.vectors.n_keys}
-                meta['lang'] = nlp.lang
-                meta['pipeline'] = pipeline
-                meta['spacy_version'] = '>=%s' % about.__version__
                 meta.setdefault('name', 'model%d' % i)
                 meta.setdefault('version', version)
+                meta_loc = output_path / ('model%d' % i) / 'meta.json'
+                write_json(meta_loc, meta)
 
-                with meta_loc.open('w') as file_:
-                    file_.write(json_dumps(meta))
-                util.set_env_log(True)
+                util.set_env_log(verbose)
+
             print_progress(i, losses, scorer.scores, cpu_wps=cpu_wps,
                            gpu_wps=gpu_wps)
     finally:
-        print("Saving model...")
+        print("\nSaving model...")
         with nlp.use_params(optimizer.averages):
             final_model_path = output_path / 'model-final'
             nlp.to_disk(final_model_path)
-    components = []
-    if not no_parser:
-        components.append('parser')
-    if not no_tagger:
-        components.append('tagger')
-    if not no_entities:
-        components.append('ner')
-    _collate_best_model(meta, output_path, components)
+            print(final_model_path)
+
+    _collate_best_model(meta, output_path, nlp.pipe_names)
+
+
+def _load_vectors(nlp, vectors):
+    print("Load vectors model", vectors)
+    util.load_model(vectors, vocab=nlp.vocab)
+    for lex in nlp.vocab:
+        values = {}
+        for attr, func in nlp.vocab.lex_attr_getters.items():
+            # These attrs are expected to be set by data. Others should
+            # be set by calling the language functions.
+            if attr not in (CLUSTER, PROB, IS_OOV, LANG):
+                values[lex.vocab.strings[attr]] = func(lex.orth_)
+        lex.set_attrs(**values)
+        lex.is_oov = False
 
 
 def _collate_best_model(meta, output_path, components):
@@ -212,8 +223,7 @@ def _collate_best_model(meta, output_path, components):
             accs = json.load(file_)
         for metric in _get_metrics(component):
             meta['accuracy'][metric] = accs[metric]
-    with (best_dest / 'meta.json').open('w') as file_:
-        file_.write(json_dumps(meta))
+    write_json(best_dest / 'meta.json', meta)
 
 
 def _find_best(experiment_dir, component):
@@ -228,6 +238,7 @@ def _find_best(experiment_dir, component):
     else:
         return None
 
+
 def _get_metrics(component):
     if component == "parser":
         return ("las", "uas", "token_acc")
@@ -236,16 +247,6 @@ def _get_metrics(component):
     elif component == "ner":
         return ("ents_f", "ents_p", "ents_r")
     return ("token_acc",)
-
-
-def _render_parses(i, to_render):
-    to_render[0].user_data['title'] = "Batch %d" % i
-    with Path('/tmp/entities.html').open('w') as file_:
-        html = displacy.render(to_render[:5], style='ent', page=True)
-        file_.write(html)
-    with Path('/tmp/parses.html').open('w') as file_:
-        html = displacy.render(to_render[:5], style='dep', page=True)
-        file_.write(html)
 
 
 def print_progress(itn, losses, dev_scores, cpu_wps=0.0, gpu_wps=0.0):
@@ -273,15 +274,3 @@ def print_progress(itn, losses, dev_scores, cpu_wps=0.0, gpu_wps=0.0):
         '{gpu_wps:.1f}',
     ))
     print(tpl.format(itn, **scores))
-
-
-def print_results(scorer):
-    results = {
-        'TOK': '%.2f' % scorer.token_acc,
-        'POS': '%.2f' % scorer.tags_acc,
-        'UAS': '%.2f' % scorer.uas,
-        'LAS': '%.2f' % scorer.las,
-        'NER P': '%.2f' % scorer.ents_p,
-        'NER R': '%.2f' % scorer.ents_r,
-        'NER F': '%.2f' % scorer.ents_f}
-    util.print_table(results, title="Results")
