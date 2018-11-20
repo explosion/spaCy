c60488961be397ad07039c23afaece9c28ef1ba6
Shawn Cicoria <github@cicoria.com>
2018-11-19 23:44:57 -0500

    fixes symbolic link on py3 and windows during setup of spacy using command python -m spacy link en_core_web_sm en closes #2948

--- spacy/compat.py
+++ spacy/compat.py
@@ -80,7 +80,7 @@ def getattr_(obj, name, *default):
 
 
 def symlink_to(orig, dest):
-    if is_python2 and is_windows:
+    if (is_python2 or is_python3) and is_windows:
         import subprocess
         subprocess.call(['mklink', '/d', path2str(orig), path2str(dest)], shell=True)
     else:
