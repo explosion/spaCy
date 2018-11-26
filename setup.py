e238bf5e4fd2dfb25a4f969f2f31d3348cd72f56
Matthew Honnibal <honnibal+gh@gmail.com>
2018-11-27 00:06:49 +0100

    Fix murmurhash version

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ def setup_package():
             scripts=["bin/spacy"],
             install_requires=[
                 "numpy>=1.15.0",
-                "murmurhash>=0.28,<0.29",
+                "murmurhash>=0.28.0,<1.1.0",
                 "cymem>=2.0.2,<2.1.0",
                 "preshed>=2.0.1,<2.1.0",
                 "thinc==7.0.0.dev0",
