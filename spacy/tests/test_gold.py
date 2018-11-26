31cd2d84b5a94e5ec2c6f54a835ac0b1a37d9353
Ines Montani <ines@ines.io>
2018-11-26 19:49:27 +0100

    Replace orths_and_spaces with words and spaces

--- spacy/tests/test_gold.py
+++ spacy/tests/test_gold.py
@@ -8,60 +8,36 @@ from .util import get_doc
 
 
 def test_gold_biluo_U(en_vocab):
-    orths_and_spaces = [
-        ("I", True),
-        ("flew", True),
-        ("to", True),
-        ("London", False),
-        (".", True),
-    ]
-    doc = Doc(en_vocab, orths_and_spaces=orths_and_spaces)
+    words = ["I", "flew", "to", "London", "."]
+    spaces = [True, True, True, False, True]
+    doc = Doc(en_vocab, words=words, spaces=spaces)
     entities = [(len("I flew to "), len("I flew to London"), "LOC")]
     tags = biluo_tags_from_offsets(doc, entities)
     assert tags == ["O", "O", "O", "U-LOC", "O"]
 
 
 def test_gold_biluo_BL(en_vocab):
-    orths_and_spaces = [
-        ("I", True),
-        ("flew", True),
-        ("to", True),
-        ("San", True),
-        ("Francisco", False),
-        (".", True),
-    ]
-    doc = Doc(en_vocab, orths_and_spaces=orths_and_spaces)
+    words = ["I", "flew", "to", "San", "Francisco", "."]
+    spaces = [True, True, True, True, False, True]
+    doc = Doc(en_vocab, words=words, spaces=spaces)
     entities = [(len("I flew to "), len("I flew to San Francisco"), "LOC")]
     tags = biluo_tags_from_offsets(doc, entities)
     assert tags == ["O", "O", "O", "B-LOC", "L-LOC", "O"]
 
 
 def test_gold_biluo_BIL(en_vocab):
-    orths_and_spaces = [
-        ("I", True),
-        ("flew", True),
-        ("to", True),
-        ("San", True),
-        ("Francisco", True),
-        ("Valley", False),
-        (".", True),
-    ]
-    doc = Doc(en_vocab, orths_and_spaces=orths_and_spaces)
+    words = ["I", "flew", "to", "San", "Francisco", "Valley", "."]
+    spaces = [True, True, True, True, True, False, True]
+    doc = Doc(en_vocab, words=words, spaces=spaces)
     entities = [(len("I flew to "), len("I flew to San Francisco Valley"), "LOC")]
     tags = biluo_tags_from_offsets(doc, entities)
     assert tags == ["O", "O", "O", "B-LOC", "I-LOC", "L-LOC", "O"]
 
 
 def test_gold_biluo_misalign(en_vocab):
-    orths_and_spaces = [
-        ("I", True),
-        ("flew", True),
-        ("to", True),
-        ("San", True),
-        ("Francisco", True),
-        ("Valley.", False),
-    ]
-    doc = Doc(en_vocab, orths_and_spaces=orths_and_spaces)
+    words = ["I", "flew", "to", "San", "Francisco", "Valley."]
+    spaces = [True, True, True, True, True, False]
+    doc = Doc(en_vocab, words=words, spaces=spaces)
     entities = [(len("I flew to "), len("I flew to San Francisco Valley"), "LOC")]
     tags = biluo_tags_from_offsets(doc, entities)
     assert tags == ["O", "O", "O", "-", "-", "-"]
