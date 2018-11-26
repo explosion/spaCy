cf227966f5c5e7a5ba763dbeb8eac387074f7048
Ines Montani <ines@ines.io>
2018-11-26 20:13:19 +0100

    💫 Refactor CLI (#2943)
    To be merged into #2932.
    
    ## Description
    - [x] refactor CLI To use [`wasabi`](https://github.com/ines/wasabi)
    - [x] use [`black`](https://github.com/ambv/black) for auto-formatting
    - [x] add `flake8` config
    - [x] move all messy UD-related scripts to `cli.ud`
    - [x] make converters function that take the opened file and return the converted data (instead of having them handle the IO)
    
    ### Types of change
    enhancement
    
    ## Checklist
    <!--- Before you submit the PR, go over this checklist and make sure you can
    tick off all the boxes. [] -> [x] -->
    - [x] I have submitted the spaCy Contributor Agreement.
    - [x] I ran the tests, and all new and existing tests passed.
    - [x] My changes don't require a change to the documentation, or if they do, I've added all required information.

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -7,12 +7,12 @@ import tqdm
 from thinc.neural._classes.model import Model
 from timeit import default_timer as timer
 import shutil
+from wasabi import Printer
 
 from ._messages import Messages
 from .._ml import create_default_optimizer
 from ..attrs import PROB, IS_OOV, CLUSTER, LANG
 from ..gold import GoldCorpus
-from ..util import prints
 from .. import util
 from .. import about
 
@@ -21,67 +21,101 @@ from .. import about
 # starts high and decays sharply, to force the optimizer to explore.
 # Batch size starts at 1 and grows, so that we make updates quickly
 # at the beginning of training.
-dropout_rates = util.decaying(util.env_opt('dropout_from', 0.2),
-                              util.env_opt('dropout_to', 0.2),
-                              util.env_opt('dropout_decay', 0.0))
-batch_sizes = util.compounding(util.env_opt('batch_from', 1000),
-                               util.env_opt('batch_to', 1000),
-                               util.env_opt('batch_compound', 1.001))
+dropout_rates = util.decaying(
+    util.env_opt("dropout_from", 0.2),
+    util.env_opt("dropout_to", 0.2),
+    util.env_opt("dropout_decay", 0.0),
+)
+batch_sizes = util.compounding(
+    util.env_opt("batch_from", 1000),
+    util.env_opt("batch_to", 1000),
+    util.env_opt("batch_compound", 1.001),
+)
 
 
 @plac.annotations(
-    lang=("model language", "positional", None, str),
-    output_path=("output directory to store model in", "positional", None, Path),
-    train_path=("location of JSON-formatted training data", "positional", None, Path),
-    dev_path=("location of JSON-formatted development data", "positional", None, Path),
-    base_model=("name of model to update (optional)", "option", "b", str),
-    pipeline=("Comma-separated names of pipeline components to train", "option", "p", str),
+    lang=("Model language", "positional", None, str),
+    output_path=("Output directory to store model in", "positional", None, Path),
+    train_path=("Location of JSON-formatted training data", "positional", None, Path),
+    dev_path=("Location of JSON-formatted development data", "positional", None, Path),
+    base_model=("Name of model to update (optional)", "option", "b", str),
+    pipeline=("Comma-separated names of pipeline components", "option", "p", str),
     vectors=("Model to load vectors from", "option", "v", str),
-    n_iter=("number of iterations", "option", "n", int),
-    n_examples=("number of examples", "option", "ns", int),
+    n_iter=("Number of iterations", "option", "n", int),
+    n_examples=("Number of examples", "option", "ns", int),
     use_gpu=("Use GPU", "option", "g", int),
     version=("Model version", "option", "V", str),
-    meta_path=("Optional path to meta.json. All relevant properties will be overwritten.", "option", "m", Path),
-    parser_multitasks=("Side objectives for parser CNN, e.g. dep dep,tag", "option", "pt", str),
-    entity_multitasks=("Side objectives for ner CNN, e.g. dep dep,tag", "option", "et", str),
-    noise_level=("Amount of corruption to add for data augmentation", "option", "nl", float),
+    meta_path=("Optional path to meta.json to use as base.", "option", "m", Path),
+    parser_multitasks=(
+        "Side objectives for parser CNN, e.g. dep dep,tag",
+        "option",
+        "pt",
+        str,
+    ),
+    entity_multitasks=(
+        "Side objectives for NER CNN, e.g. dep dep,tag",
+        "option",
+        "et",
+        str,
+    ),
+    noise_level=("Amount of corruption for data augmentation", "option", "nl", float),
     gold_preproc=("Use gold preprocessing", "flag", "G", bool),
     learn_tokens=("Make parser learn gold-standard tokenization", "flag", "T", bool),
     verbose=("Display more information for debug", "flag", "vv", bool),
-    debug=("Run data diagnostics before training", "flag", "D", bool)
+    debug=("Run data diagnostics before training", "flag", "D", bool),
 )
-def train(lang, output_path, train_path, dev_path, base_model=None,
-          pipeline='tagger,parser,ner', vectors=None, n_iter=30, n_examples=0,
-          use_gpu=-1, version="0.0.0", meta_path=None, parser_multitasks='',
-          entity_multitasks='', noise_level=0.0, gold_preproc=False,
-          learn_tokens=False, verbose=False, debug=False):
-    """Train or update a spaCy model."""
+def train(
+    lang,
+    output_path,
+    train_path,
+    dev_path,
+    base_model=None,
+    pipeline="tagger,parser,ner",
+    vectors=None,
+    n_iter=30,
+    n_examples=0,
+    use_gpu=-1,
+    version="0.0.0",
+    meta_path=None,
+    parser_multitasks="",
+    entity_multitasks="",
+    noise_level=0.0,
+    gold_preproc=False,
+    learn_tokens=False,
+    verbose=False,
+    debug=False,
+):
+    """
+    Train or update a spaCy model. Requires data to be formatted in spaCy's
+    JSON format. To convert data from other formats, use the `spacy convert`
+    command.
+    """
+    msg = Printer()
     util.fix_random_seed()
     util.set_env_log(verbose)
 
     # Make sure all files and paths exists if they are needed
     if not train_path.exists():
-        prints(train_path, title=Messages.M050, exits=1)
+        msg.fail(Messages.M050, train_path, exits=1)
     if not dev_path.exists():
-        prints(dev_path, title=Messages.M051, exits=1)
+        msg.fail(Messages.M051, dev_path, exits=1)
     if meta_path is not None and not meta_path.exists():
-        prints(meta_path, title=Messages.M020, exits=1)
+        msg.fail(Messages.M020, meta_path, exits=1)
     meta = util.read_json(meta_path) if meta_path else {}
     if not isinstance(meta, dict):
-        prints(Messages.M053.format(meta_type=type(meta)),
-               title=Messages.M052, exits=1)
+        msg.fail(Messages.M052, Messages.M053.format(meta_type=type(meta)), exits=1)
     if output_path.exists() and [p for p in output_path.iterdir() if p.is_dir()]:
-        raise ValueError(Messages.M062)
+        msg.fail(Messages.M062, Messages.M065)
     if not output_path.exists():
         output_path.mkdir()
 
     # Set up the base model and pipeline. If a base model is specified, load
     # the model and make sure the pipeline matches the pipeline setting. If
     # training starts from a blank model, intitalize the language class.
-    pipeline = [p.strip() for p in pipeline.split(',')]
-    print(Messages.M055.format(pipeline=pipeline))
+    pipeline = [p.strip() for p in pipeline.split(",")]
+    msg.text(Messages.M055.format(pipeline=pipeline))
     if base_model:
-        print(Messages.M056.format(model=base_model))
+        msg.text(Messages.M056.format(model=base_model))
         nlp = util.load_model(base_model)
         other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipeline]
         nlp.disable_pipes(*other_pipes)
@@ -89,31 +123,31 @@ def train(lang, output_path, train_path, dev_path, base_model=None,
             if pipe not in nlp.pipe_names:
                 nlp.add_pipe(nlp.create_pipe(pipe))
     else:
-        print(Messages.M057.format(model=lang))
+        msg.text(Messages.M057.format(model=lang))
         lang_cls = util.get_lang_class(lang)
         nlp = lang_cls()
         for pipe in pipeline:
             nlp.add_pipe(nlp.create_pipe(pipe))
 
     if learn_tokens:
-        nlp.add_pipe(nlp.create_pipe('merge_subtokens'))
+        nlp.add_pipe(nlp.create_pipe("merge_subtokens"))
 
     if vectors:
-        print(Messages.M058.format(model=vectors))
+        msg.text(Messages.M058.format(model=vectors))
         _load_vectors(nlp, vectors)
 
     # Multitask objectives
-    for pipe_name, multitasks in [('parser', parser_multitasks),
-                                  ('ner', entity_multitasks)]:
+    multitask_options = [("parser", parser_multitasks), ("ner", entity_multitasks)]
+    for pipe_name, multitasks in multitask_options:
         if multitasks:
-            if not pipe_name in pipeline:
-                raise ValueError(Messages.M059.format(pipe=pipe_name))
+            if pipe_name not in pipeline:
+                msg.fail(Messages.M059.format(pipe=pipe_name))
             pipe = nlp.get_pipe(pipe_name)
-            for objective in multitasks.split(','):
+            for objective in multitasks.split(","):
                 pipe.add_multitask_objective(objective)
 
     # Prepare training corpus
-    print(Messages.M060.format(limit=n_examples))
+    msg.text(Messages.M060.format(limit=n_examples))
     corpus = GoldCorpus(train_path, dev_path, limit=n_examples)
     n_train_words = corpus.count_train()
 
@@ -125,11 +159,14 @@ def train(lang, output_path, train_path, dev_path, base_model=None,
         optimizer = nlp.begin_training(lambda: corpus.train_tuples, device=use_gpu)
     nlp._optimizer = None
 
-    print("\nItn.  Dep Loss  NER Loss  UAS     NER P.  NER R.  NER F.  Tag %   Token %  CPU WPS  GPU WPS")
+    print(
+        "\nItn.  Dep Loss  NER Loss  UAS     NER P.  NER R.  NER F.  Tag %   Token %  CPU WPS  GPU WPS"
+    )
     try:
         for i in range(n_iter):
-            train_docs = corpus.train_docs(nlp, noise_level=noise_level,
-                                           gold_preproc=gold_preproc, max_length=0)
+            train_docs = corpus.train_docs(
+                nlp, noise_level=noise_level, gold_preproc=gold_preproc, max_length=0
+            )
             words_seen = 0
             with tqdm.tqdm(total=n_train_words, leave=False) as pbar:
                 losses = {}
@@ -137,63 +174,67 @@ def train(lang, output_path, train_path, dev_path, base_model=None,
                     if not batch:
                         continue
                     docs, golds = zip(*batch)
-                    nlp.update(docs, golds, sgd=optimizer,
-                               drop=next(dropout_rates), losses=losses)
+                    nlp.update(
+                        docs,
+                        golds,
+                        sgd=optimizer,
+                        drop=next(dropout_rates),
+                        losses=losses,
+                    )
                     pbar.update(sum(len(doc) for doc in docs))
                     words_seen += sum(len(doc) for doc in docs)
             with nlp.use_params(optimizer.averages):
                 util.set_env_log(False)
-                epoch_model_path = output_path / ('model%d' % i)
+                epoch_model_path = output_path / ("model%d" % i)
                 nlp.to_disk(epoch_model_path)
                 nlp_loaded = util.load_model_from_path(epoch_model_path)
-                dev_docs = list(corpus.dev_docs(
-                                nlp_loaded,
-                                gold_preproc=gold_preproc))
+                dev_docs = list(corpus.dev_docs(nlp_loaded, gold_preproc=gold_preproc))
                 nwords = sum(len(doc_gold[0]) for doc_gold in dev_docs)
                 start_time = timer()
                 scorer = nlp_loaded.evaluate(dev_docs, debug)
                 end_time = timer()
                 if use_gpu < 0:
                     gpu_wps = None
-                    cpu_wps = nwords/(end_time-start_time)
+                    cpu_wps = nwords / (end_time - start_time)
                 else:
-                    gpu_wps = nwords/(end_time-start_time)
-                    with Model.use_device('cpu'):
+                    gpu_wps = nwords / (end_time - start_time)
+                    with Model.use_device("cpu"):
                         nlp_loaded = util.load_model_from_path(epoch_model_path)
-                        dev_docs = list(corpus.dev_docs(
-                                        nlp_loaded, gold_preproc=gold_preproc))
+                        dev_docs = list(
+                            corpus.dev_docs(nlp_loaded, gold_preproc=gold_preproc)
+                        )
                         start_time = timer()
                         scorer = nlp_loaded.evaluate(dev_docs)
                         end_time = timer()
-                        cpu_wps = nwords/(end_time-start_time)
-                acc_loc = output_path / ('model%d' % i) / 'accuracy.json'
+                        cpu_wps = nwords / (end_time - start_time)
+                acc_loc = output_path / ("model%d" % i) / "accuracy.json"
                 util.write_json(acc_loc, scorer.scores)
 
                 # Update model meta.json
-                meta['lang'] = nlp.lang
-                meta['pipeline'] = nlp.pipe_names
-                meta['spacy_version'] = '>=%s' % about.__version__
-                meta['accuracy'] = scorer.scores
-                meta['speed'] = {'nwords': nwords, 'cpu': cpu_wps,
-                                 'gpu': gpu_wps}
-                meta['vectors'] = {'width': nlp.vocab.vectors_length,
-                                   'vectors': len(nlp.vocab.vectors),
-                                   'keys': nlp.vocab.vectors.n_keys}
-                meta.setdefault('name', 'model%d' % i)
-                meta.setdefault('version', version)
-                meta_loc = output_path / ('model%d' % i) / 'meta.json'
+                meta["lang"] = nlp.lang
+                meta["pipeline"] = nlp.pipe_names
+                meta["spacy_version"] = ">=%s" % about.__version__
+                meta["accuracy"] = scorer.scores
+                meta["speed"] = {"nwords": nwords, "cpu": cpu_wps, "gpu": gpu_wps}
+                meta["vectors"] = {
+                    "width": nlp.vocab.vectors_length,
+                    "vectors": len(nlp.vocab.vectors),
+                    "keys": nlp.vocab.vectors.n_keys,
+                }
+                meta.setdefault("name", "model%d" % i)
+                meta.setdefault("version", version)
+                meta_loc = output_path / ("model%d" % i) / "meta.json"
                 util.write_json(meta_loc, meta)
 
                 util.set_env_log(verbose)
 
-            print_progress(i, losses, scorer.scores, cpu_wps=cpu_wps,
-                           gpu_wps=gpu_wps)
+            print_progress(i, losses, scorer.scores, cpu_wps=cpu_wps, gpu_wps=gpu_wps)
     finally:
-        print(Messages.M061)
-        with nlp.use_params(optimizer.averages):
-            final_model_path = output_path / 'model-final'
-            nlp.to_disk(final_model_path)
-            print(util.path2str(final_model_path))
+        with msg.loading(Messages.M061):
+            with nlp.use_params(optimizer.averages):
+                final_model_path = output_path / "model-final"
+                nlp.to_disk(final_model_path)
+        msg.good(Messages.M066, util.path2str(final_model_path))
 
     _collate_best_model(meta, output_path, nlp.pipe_names)
 
@@ -210,34 +251,34 @@ def _load_vectors(nlp, vectors):
         lex.set_attrs(**values)
         lex.is_oov = False
 
-        
+
 def _load_pretrained_tok2vec(nlp, loc):
     """Load pre-trained weights for the 'token-to-vector' part of the component
     models, which is typically a CNN. See 'spacy pretrain'. Experimental.
     """
-    with loc.open('rb') as file_:
+    with loc.open("rb") as file_:
         weights_data = file_.read()
     loaded = []
     for name, component in nlp.pipeline:
-        if hasattr(component, 'model') and hasattr(component.model, 'tok2vec'):
+        if hasattr(component, "model") and hasattr(component.model, "tok2vec"):
             component.tok2vec.from_bytes(weights_data)
             loaded.append(name)
     return loaded
-  
+
 
 def _collate_best_model(meta, output_path, components):
     bests = {}
     for component in components:
         bests[component] = _find_best(output_path, component)
-    best_dest = output_path / 'model-best'
-    shutil.copytree(output_path / 'model-final', best_dest)
+    best_dest = output_path / "model-best"
+    shutil.copytree(output_path / "model-final", best_dest)
     for component, best_component_src in bests.items():
         shutil.rmtree(best_dest / component)
         shutil.copytree(best_component_src / component, best_dest / component)
-        accs = util.read_json(best_component_src / 'accuracy.json')
+        accs = util.read_json(best_component_src / "accuracy.json")
         for metric in _get_metrics(component):
-            meta['accuracy'][metric] = accs[metric]
-    util.write_json(best_dest / 'meta.json', meta)
+            meta["accuracy"][metric] = accs[metric]
+    util.write_json(best_dest / "meta.json", meta)
 
 
 def _find_best(experiment_dir, component):
@@ -265,26 +306,38 @@ def _get_metrics(component):
 
 def print_progress(itn, losses, dev_scores, cpu_wps=0.0, gpu_wps=0.0):
     scores = {}
-    for col in ['dep_loss', 'tag_loss', 'uas', 'tags_acc', 'token_acc',
-                'ents_p', 'ents_r', 'ents_f', 'cpu_wps', 'gpu_wps']:
+    for col in [
+        "dep_loss",
+        "tag_loss",
+        "uas",
+        "tags_acc",
+        "token_acc",
+        "ents_p",
+        "ents_r",
+        "ents_f",
+        "cpu_wps",
+        "gpu_wps",
+    ]:
         scores[col] = 0.0
-    scores['dep_loss'] = losses.get('parser', 0.0)
-    scores['ner_loss'] = losses.get('ner', 0.0)
-    scores['tag_loss'] = losses.get('tagger', 0.0)
+    scores["dep_loss"] = losses.get("parser", 0.0)
+    scores["ner_loss"] = losses.get("ner", 0.0)
+    scores["tag_loss"] = losses.get("tagger", 0.0)
     scores.update(dev_scores)
-    scores['cpu_wps'] = cpu_wps
-    scores['gpu_wps'] = gpu_wps or 0.0
-    tpl = ''.join((
-        '{:<6d}',
-        '{dep_loss:<10.3f}',
-        '{ner_loss:<10.3f}',
-        '{uas:<8.3f}',
-        '{ents_p:<8.3f}',
-        '{ents_r:<8.3f}',
-        '{ents_f:<8.3f}',
-        '{tags_acc:<8.3f}',
-        '{token_acc:<9.3f}',
-        '{cpu_wps:<9.1f}',
-        '{gpu_wps:.1f}',
-    ))
+    scores["cpu_wps"] = cpu_wps
+    scores["gpu_wps"] = gpu_wps or 0.0
+    tpl = "".join(
+        (
+            "{:<6d}",
+            "{dep_loss:<10.3f}",
+            "{ner_loss:<10.3f}",
+            "{uas:<8.3f}",
+            "{ents_p:<8.3f}",
+            "{ents_r:<8.3f}",
+            "{ents_f:<8.3f}",
+            "{tags_acc:<8.3f}",
+            "{token_acc:<9.3f}",
+            "{cpu_wps:<9.1f}",
+            "{gpu_wps:.1f}",
+        )
+    )
     print(tpl.format(itn, **scores))
---
+++ spacy/cli/ud/__init__.py
@@ -0,0 +1,2 @@
+from .conll17_ud_eval import main as ud_evaluate  # noqa: F401
+from .ud_train import main as ud_train  # noqa: F401
