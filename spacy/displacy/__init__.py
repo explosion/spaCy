c706d3ba8e7840e7c760f571eb1992c17755d1a7
Ines Montani <ines@ines.io>
2018-11-29 18:15:19 +0100

    Auto-format spacy/displacy

--- spacy/displacy/__init__.py
+++ spacy/displacy/__init__.py
@@ -12,8 +12,15 @@ _html = {}
 IS_JUPYTER = is_in_jupyter()
 
 
-def render(docs, style='dep', page=False, minify=False, jupyter=IS_JUPYTER,
-           options={}, manual=False):
+def render(
+    docs,
+    style="dep",
+    page=False,
+    minify=False,
+    jupyter=IS_JUPYTER,
+    options={},
+    manual=False,
+):
     """Render displaCy visualisation.
 
     docs (list or Doc): Document(s) to visualise.
@@ -25,8 +32,10 @@ def render(docs, style='dep', page=False, minify=False, jupyter=IS_JUPYTER,
     manual (bool): Don't parse `Doc` and instead expect a dict/list of dicts.
     RETURNS (unicode): Rendered HTML markup.
     """
-    factories = {'dep': (DependencyRenderer, parse_deps),
-                 'ent': (EntityRenderer, parse_ents)}
+    factories = {
+        "dep": (DependencyRenderer, parse_deps),
+        "ent": (EntityRenderer, parse_ents),
+    }
     if style not in factories:
         raise ValueError(Errors.E087.format(style=style))
     if isinstance(docs, (Doc, Span, dict)):
@@ -37,16 +46,18 @@ def render(docs, style='dep', page=False, minify=False, jupyter=IS_JUPYTER,
     renderer, converter = factories[style]
     renderer = renderer(options=options)
     parsed = [converter(doc, options) for doc in docs] if not manual else docs
-    _html['parsed'] = renderer.render(parsed, page=page, minify=minify).strip()
-    html = _html['parsed']
+    _html["parsed"] = renderer.render(parsed, page=page, minify=minify).strip()
+    html = _html["parsed"]
     if jupyter:  # return HTML rendered by IPython display()
         from IPython.core.display import display, HTML
+
         return display(HTML(html))
     return html
 
 
-def serve(docs, style='dep', page=True, minify=False, options={}, manual=False,
-          port=5000):
+def serve(
+    docs, style="dep", page=True, minify=False, options={}, manual=False, port=5000
+):
     """Serve displaCy visualisation.
 
     docs (list or Doc): Document(s) to visualise.
@@ -58,11 +69,13 @@ def serve(docs, style='dep', page=True, minify=False, options={}, manual=False,
     port (int): Port to serve visualisation.
     """
     from wsgiref import simple_server
-    render(docs, style=style, page=page, minify=minify, options=options,
-           manual=manual)
-    httpd = simple_server.make_server('0.0.0.0', port, app)
-    prints("Using the '{}' visualizer".format(style),
-           title="Serving on port {}...".format(port))
+
+    render(docs, style=style, page=page, minify=minify, options=options, manual=manual)
+    httpd = simple_server.make_server("0.0.0.0", port, app)
+    prints(
+        "Using the '{}' visualizer".format(style),
+        title="Serving on port {}...".format(port),
+    )
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -72,11 +85,10 @@ def serve(docs, style='dep', page=True, minify=False, options={}, manual=False,
 
 
 def app(environ, start_response):
-    # headers and status need to be bytes in Python 2, see #1227
-    headers = [(b_to_str(b'Content-type'),
-                b_to_str(b'text/html; charset=utf-8'))]
-    start_response(b_to_str(b'200 OK'), headers)
-    res = _html['parsed'].encode(encoding='utf-8')
+    # Headers and status need to be bytes in Python 2, see #1227
+    headers = [(b_to_str(b"Content-type"), b_to_str(b"text/html; charset=utf-8"))]
+    start_response(b_to_str(b"200 OK"), headers)
+    res = _html["parsed"].encode(encoding="utf-8")
     return [res]
 
 
@@ -89,11 +101,10 @@ def parse_deps(orig_doc, options={}):
     doc = Doc(orig_doc.vocab).from_bytes(orig_doc.to_bytes())
     if not doc.is_parsed:
         user_warning(Warnings.W005)
-    if options.get('collapse_phrases', False):
+    if options.get("collapse_phrases", False):
         for np in list(doc.noun_chunks):
-            np.merge(tag=np.root.tag_, lemma=np.root.lemma_,
-                    ent_type=np.root.ent_type_)
-    if options.get('collapse_punct', True):
+            np.merge(tag=np.root.tag_, lemma=np.root.lemma_, ent_type=np.root.ent_type_)
+    if options.get("collapse_punct", True):
         spans = []
         for word in doc[:-1]:
             if word.is_punct or not word.nbor(1).is_punct:
@@ -103,23 +114,31 @@ def parse_deps(orig_doc, options={}):
             while end < len(doc) and doc[end].is_punct:
                 end += 1
             span = doc[start:end]
-            spans.append((span.start_char, span.end_char, word.tag_,
-                          word.lemma_, word.ent_type_))
+            spans.append(
+                (span.start_char, span.end_char, word.tag_, word.lemma_, word.ent_type_)
+            )
         for start, end, tag, lemma, ent_type in spans:
             doc.merge(start, end, tag=tag, lemma=lemma, ent_type=ent_type)
-    if options.get('fine_grained'):
-        words = [{'text': w.text, 'tag': w.tag_} for w in doc]
+    if options.get("fine_grained"):
+        words = [{"text": w.text, "tag": w.tag_} for w in doc]
     else:
-        words = [{'text': w.text, 'tag': w.pos_} for w in doc]
+        words = [{"text": w.text, "tag": w.pos_} for w in doc]
     arcs = []
     for word in doc:
         if word.i < word.head.i:
-            arcs.append({'start': word.i, 'end': word.head.i,
-                         'label': word.dep_, 'dir': 'left'})
+            arcs.append(
+                {"start": word.i, "end": word.head.i, "label": word.dep_, "dir": "left"}
+            )
         elif word.i > word.head.i:
-            arcs.append({'start': word.head.i, 'end': word.i,
-                         'label': word.dep_, 'dir': 'right'})
-    return {'words': words, 'arcs': arcs}
+            arcs.append(
+                {
+                    "start": word.head.i,
+                    "end": word.i,
+                    "label": word.dep_,
+                    "dir": "right",
+                }
+            )
+    return {"words": words, "arcs": arcs}
 
 
 def parse_ents(doc, options={}):
@@ -128,10 +147,11 @@ def parse_ents(doc, options={}):
     doc (Doc): Document do parse.
     RETURNS (dict): Generated entities keyed by text (original text) and ents.
     """
-    ents = [{'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_}
-            for ent in doc.ents]
+    ents = [
+        {"start": ent.start_char, "end": ent.end_char, "label": ent.label_}
+        for ent in doc.ents
+    ]
     if not ents:
         user_warning(Warnings.W006)
-    title = (doc.user_data.get('title', None)
-             if hasattr(doc, 'user_data') else None)
-    return {'text': doc.text, 'ents': ents, 'title': title}
+    title = doc.user_data.get("title", None) if hasattr(doc, "user_data") else None
+    return {"text": doc.text, "ents": ents, "title": title}
