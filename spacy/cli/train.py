e62526e013837d0cca7ad082d9c28112427a9e04
Ines Montani <ines@ines.io>
2018-11-30 18:07:29 +0100

    Auto-format remaining code

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -136,15 +136,19 @@ def train(
     # starts high and decays sharply, to force the optimizer to explore.
     # Batch size starts at 1 and grows, so that we make updates quickly
     # at the beginning of training.
-    dropout_rates = util.decaying(util.env_opt('dropout_from', 0.1),
-                                  util.env_opt('dropout_to', 0.1),
-                                  util.env_opt('dropout_decay', 0.0))
-    batch_sizes = util.compounding(util.env_opt('batch_from', 750),
-                                   util.env_opt('batch_to', 750),
-                                   util.env_opt('batch_compound', 1.001))
+    dropout_rates = util.decaying(
+        util.env_opt("dropout_from", 0.1),
+        util.env_opt("dropout_to", 0.1),
+        util.env_opt("dropout_decay", 0.0),
+    )
+    batch_sizes = util.compounding(
+        util.env_opt("batch_from", 750),
+        util.env_opt("batch_to", 750),
+        util.env_opt("batch_compound", 1.001),
+    )
     lang_class = util.get_lang_class(lang)
     nlp = lang_class()
-    meta['pipeline'] = pipeline
+    meta["pipeline"] = pipeline
     nlp.meta.update(meta)
     if vectors:
         msg.text(Messages.M058.format(model=vectors))
