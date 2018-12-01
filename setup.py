61abb1ef70932a32184914ae5399d22469c449c1
Matthew Honnibal <honnibal+gh@gmail.com>
2018-12-01 02:36:41 +0100

    Remove msgpack dependency, to try to fix #2995

--- setup.py
+++ setup.py
@@ -188,11 +188,10 @@ def setup_package():
             setup_requires=['wheel>=0.32.0,<0.33.0'],
             install_requires=[
                 'numpy>=1.15.0',
-                'msgpack-numpy<0.4.4',
                 'murmurhash>=0.28.0,<1.1.0',
                 'cymem>=2.0.2,<2.1.0',
                 'preshed>=2.0.1,<2.1.0',
-                'thinc>=6.12.0,<6.13.0',
+                'thinc>=6.12.1,<6.13.0',
                 'plac<1.0.0,>=0.9.6',
                 'ujson>=1.35',
                 'dill>=0.2,<0.3',
