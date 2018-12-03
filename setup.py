ee4733b48cd43300f55c038954f0d16d11569954
Ines Montani <ines@ines.io>
2018-12-03 02:10:37 +0100

    Update srsly version pin

--- setup.py
+++ setup.py
@@ -208,7 +208,7 @@ def setup_package():
                 "requests>=2.13.0,<3.0.0",
                 "jsonschema>=2.6.0,<3.0.0",
                 "wasabi>=0.0.8,<1.1.0",
-                "srsly>=0.0.4,<1.1.0",
+                "srsly>=0.0.5,<1.1.0",
                 'pathlib==1.0.1; python_version < "3.4"',
             ],
             setup_requires=["wheel"],
