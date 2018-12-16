c6ad557cea57977f898c8a5388de4bf122311bdc
Sofie <svlandeg@users.noreply.github.com>
2018-12-16 18:04:55 +0100

    French regular expressions instead of extensive exceptions list (on develop) (#3046) (resolves #2679)
    * merge changes of PR 3023 into develop branch instead of master
    
    * further deletions from exception list according to PR 3023

--- spacy/tests/lang/fr/test_exceptions.py
+++ spacy/tests/lang/fr/test_exceptions.py
@@ -5,7 +5,15 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "text", ["aujourd'hui", "Aujourd'hui", "prud'hommes", "prud’hommal"]
+    "text", ["aujourd'hui", "Aujourd'hui", "prud'hommes", "prud’hommal",
+             "audio-numérique", "Audio-numérique",
+             "entr'amis", "entr'abat", "rentr'ouvertes", "grand'hamien",
+             "Châteauneuf-la-Forêt", "Château-Guibert",
+             "11-septembre", "11-Septembre", "refox-trottâmes",
+             "K-POP", "K-Pop", "K-pop", "z'yeutes",
+             "black-outeront", "états-unienne",
+             "courtes-pattes", "court-pattes",
+             "saut-de-ski", "Écourt-Saint-Quentin", "Bout-de-l'Îlien", "pet-en-l'air"]
 )
 def test_fr_tokenizer_infix_exceptions(fr_tokenizer, text):
     tokens = fr_tokenizer(text)
