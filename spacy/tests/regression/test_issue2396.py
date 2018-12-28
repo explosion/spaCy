8fafe6bb492fe646e02695a2e72de84353d092f2
Álvaro Abella Bascarán <alvaroabascar@gmail.com>
2018-12-28 06:36:15 +0100

    tests Span.get_lca_matrix() as well as Doc.get_lca_matrix()

--- spacy/tests/regression/test_issue2396.py
+++ spacy/tests/regression/test_issue2396.py
@@ -1,18 +1,27 @@
 # coding: utf-8
 from __future__ import unicode_literals
+
+from ..util import get_doc
+
 import pytest
+import numpy
 
-import numpy as np
+@pytest.mark.parametrize('sentence,matrix', [
+    (
+        'She created a test for spacy',
+        numpy.array([
+            [0, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 2, 3, 3, 3],
+            [1, 1, 3, 3, 3, 3],
+            [1, 1, 3, 3, 4, 4],
+            [1, 1, 3, 3, 4, 5]], dtype=numpy.int32)
+    )
+    ])
+def test_issue2396(EN, sentence, matrix):
+    doc = EN(sentence)
+    span = doc[:]
+    assert (doc.get_lca_matrix() == matrix).all()
+    assert (span.get_lca_matrix() == matrix).all()
 
-def test_issue2396(EN):
-    doc = EN('He created a test for spacy')
-    right_lca_matrix = np.array([
-        [0, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1],
-        [1, 1, 2, 3, 3, 3],
-        [1, 1, 3, 3, 3, 3],
-        [1, 1, 3, 3, 4, 4],
-        [1, 1, 3, 3, 4, 5]
-    ], dtype=np.int32)
-    lca_matrix = doc.get_lca_matrix()
-    assert (lca_matrix == right_lca_matrix).all()
+
