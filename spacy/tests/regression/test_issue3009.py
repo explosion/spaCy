14400f7e1ab762b58208dd2af5aa0457a1c9db6f
Matthew Honnibal <honnibal+gh@gmail.com>
2018-12-29 14:50:48 +0100

    Update matcher ? quantifier test

--- spacy/tests/regression/test_issue3009.py
+++ spacy/tests/regression/test_issue3009.py
@@ -1,6 +1,7 @@
 import pytest
-import spacy
 from spacy.matcher import Matcher
+from spacy.tokens import Doc
+
 
 PATTERNS = [
     ("1", [[{"LEMMA": "have"}, {"LOWER": "to"}, {"LOWER": "do"}, {"POS": "ADP"}]]),
@@ -48,8 +49,15 @@ def matcher(en_tokenizer):
 
 
 @pytest.mark.parametrize("pattern", PATTERNS)
-def test_issue3009_patterns_individually(doc, matcher, pattern):
+def test_issue3009(doc, matcher, pattern):
     """Test problem with matcher quantifiers"""
     matcher.add(pattern[0], None, *pattern[1])
     matches = matcher(doc)
     assert matches
+
+def test_issue2464(matcher):
+    """Test problem with successive ?. This is the same bug, so putting it here."""
+    doc = Doc(matcher.vocab, words=['a', 'b'])
+    matcher.add('4', None, [{'OP': '?'}, {'OP': '?'}])
+    matches = matcher(doc)
+    assert len(matches) == 3
