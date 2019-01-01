cfe0c0a55db10009a0e74e1df7ad1765d6297ad8
Álvaro Abella Bascarán <alvaroabascar@gmail.com>
2019-01-01 23:06:18 +0100

    parametrize test for doc.get_lca_matrix, add case of doc with two sentences

--- spacy/tests/doc/test_doc_api.py
+++ spacy/tests/doc/test_doc_api.py
@@ -278,10 +278,30 @@ def test_doc_api_similarity_match():
     assert doc.similarity(doc2) == 0.0
 
 
-def test_lowest_common_ancestor(en_tokenizer):
-    tokens = en_tokenizer('the lazy dog slept')
-    doc = get_doc(tokens.vocab, [t.text for t in tokens], heads=[2, 1, 1, 0])
+@pytest.mark.parametrize('sentence,heads,lca_matrix', [
+    ('the lazy dog slept',
+     [2, 1, 1, 0],
+     numpy.array([[0, 2, 2, 3],
+                  [2, 1, 2, 3],
+                  [2, 2, 2, 3],
+                  [3, 3, 3, 3]])),
+    ('The lazy dog slept. The quick fox jumped',
+     [2, 1, 1, 0, -1, 2, 1, 1, 0],
+     numpy.array([[0, 2, 2, 3, 3, -1, -1, -1, -1],
+                  [2, 1, 2, 3, 3, -1, -1, -1, -1],
+                  [2, 2, 2, 3, 3, -1, -1, -1, -1],
+                  [3, 3, 3, 3, 3, -1, -1, -1, -1],
+                  [3, 3, 3, 3, 4, -1, -1, -1, -1],
+                  [-1, -1, -1, -1, -1, 5, 7, 7, 8],
+                  [-1, -1, -1, -1, -1, 7, 6, 7, 8],
+                  [-1, -1, -1, -1, -1, 7, 7, 7, 8],
+                  [-1, -1, -1, -1, -1, 8, 8, 8, 8]]))
+])
+def test_lowest_common_ancestor(en_tokenizer, sentence, heads, lca_matrix):
+    tokens = en_tokenizer(sentence)
+    doc = get_doc(tokens.vocab, [t.text for t in tokens], heads=heads)
     lca = doc.get_lca_matrix()
+    assert (lca == lca_matrix).all()
     assert(lca[1, 1] == 1)
     assert(lca[0, 1] == 2)
     assert(lca[1, 2] == 2)
