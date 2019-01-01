4412d24eaf04d5f72f4f326846ca2f1f0aca6d28
Álvaro Abella Bascarán <alvaroabascar@gmail.com>
2019-01-01 16:52:02 +0100

    solve bug for doc[i:].get_lca_matrix() where i > 0

--- spacy/tokens/doc.pyx
+++ spacy/tokens/doc.pyx
@@ -1053,19 +1053,19 @@ cdef int [:,:] _get_lca_matrix(Doc doc, int start, int end):
     n_tokens= end - start
     lca_matrix = numpy.empty((n_tokens, n_tokens), dtype=numpy.int32)
 
-    for j in range(start, end):
-        token_j = doc[j]
+    for j in range(n_tokens):
+        token_j = doc[start + j]
         # the common ancestor of token and itself is itself:
         lca_matrix[j, j] = j
-        for k in range(j + 1, end):
-            lca = _get_tokens_lca(token_j, doc[k])
+        for k in range(j + 1, n_tokens):
+            lca = _get_tokens_lca(token_j, doc[start + k])
             # if lca is outside of span, we set it to -1
             if not start <= lca < end:
                 lca_matrix[j, k] = -1
                 lca_matrix[k, j] = -1
             else:
-                lca_matrix[j, k] = lca
-                lca_matrix[k, j] = lca
+                lca_matrix[j, k] = lca - start
+                lca_matrix[k, j] = lca - start
 
     return lca_matrix
 
