1ea4df459dbd96842b337240059be5dd1ff44ef1
Ines Montani <ines@ines.io>
2019-02-07 09:42:25 +0100

    💫 Break up large matcher.pyx (#3236)
    * Break up large matcher.pyx
    
    * Remove unused function

---
+++ spacy/matcher/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from .matcher import Matcher
+from .phrasematcher import PhraseMatcher
+from .dependencymatcher import DependencyTreeMatcher
