c49ac559180f1e7319af95bb19e762478c2a8d8d
Ines Montani <ines@ines.io>
2019-02-17 19:30:44 +0100

    Update usage of useInView

--- website/src/components/section.js
+++ website/src/components/section.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { useInView } from 'react-intersection-observer'
@@ -8,9 +8,8 @@ import classes from '../styles/section.module.sass'
 
 const Section = ({ id, className, ...props }) => {
     const sectionClassNames = classNames(classes.root, className)
-    const ref = useRef()
     const relId = id && id.startsWith('section-') ? id.slice(8) : id
-    const inView = useInView(ref, { threshold: 0 })
+    const [ref, inView] = useInView({ threshold: 0 })
 
     useEffect(() => {
         if (inView && relId) {
