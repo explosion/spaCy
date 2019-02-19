5dacd73d40d0817678db0e640e375d44c8d68a10
svlandeg <sofie.vanlandeghem@gmail.com>
2019-02-19 12:42:44 +0100

    Fix issue #2926 by allowing numbers right before infix /

--- spacy/lang/punctuation.py
+++ spacy/lang/punctuation.py
@@ -41,8 +41,9 @@ _infixes = (
         r"(?<=[0-9])[+\-\*^](?=[0-9-])",
         r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES),
         r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+
         r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
-        r"(?<=[{a}])[:<>=/](?=[{a}])".format(a=ALPHA),
+        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
     ]
 )
 
