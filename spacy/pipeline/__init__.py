3908911da477a85073eb8ac607bc9ef7f60ccd7e
Matthew Honnibal <honnibal+gh@gmail.com>
2019-03-08 17:04:14 +0100

    Fix import

--- spacy/pipeline/__init__.py
+++ spacy/pipeline/__init__.py
@@ -1,8 +1,9 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from .pipes import Tagger, DependencyParser, EntityRecognizer, Morphologizer
+from .pipes import Tagger, DependencyParser, EntityRecognizer
 from .pipes import TextCategorizer, Tensorizer, Pipe
+from .morphologizer import Morphologizer
 from .entityruler import EntityRuler
 from .hooks import SentenceSegmenter, SimilarityHook
 from .functions import merge_entities, merge_noun_chunks, merge_subtokens
