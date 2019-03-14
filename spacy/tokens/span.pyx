ab442338c8c4ddd7dfbc15348f999b74f4928090
svlandeg <sofie.vanlandeghem@gmail.com>
2019-03-14 16:11:00 +0100

    label in span not writable anymore

--- spacy/tokens/span.pyx
+++ spacy/tokens/span.pyx
@@ -653,7 +653,7 @@ cdef class Span:
             return self.doc.vocab.strings[self.label]
 
         def __set__(self, unicode label_):
-            self.label = self.doc.vocab.strings.add(label_)
+            raise NotImplementedError(TempErrors.T007.format(attr="label_"))
 
 
 cdef int _count_words_to_root(const TokenC* token, int sent_length) except -1:
