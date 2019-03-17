aaa1951b6628caeb30391d52f5a29646ab4c6f91
Ines Montani <ines@ines.io>
2019-03-17 19:39:33 +0100

    Use production docsearch [ci skip]

--- website/src/templates/index.js
+++ website/src/templates/index.js
@@ -191,7 +191,6 @@ export const pageQuery = graphql`
                 docSearch {
                     apiKey
                     indexName
-                    appId
                 }
             }
         }
