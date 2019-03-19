f0decf98f19f13ceb87dfeb78955f6241fecb69e
svlandeg <sofie.vanlandeghem@gmail.com>
2019-03-19 21:43:48 +0100

    check and unit test in case prior probs exceed 1

--- spacy/tests/pipeline/test_el.py
+++ spacy/tests/pipeline/test_el.py
@@ -1,14 +1,16 @@
+# coding: utf-8
 import pytest
 
 from spacy.kb import KnowledgeBase
 
 
 def test_kb_valid_entities():
+    """Test the valid construction of a KB with 3 entities and one alias"""
     mykb = KnowledgeBase()
 
     # adding entities
-    mykb.add_entity(entity_id="Q1", prob=0.5)
-    mykb.add_entity(entity_id="Q2", prob=0.5)
+    mykb.add_entity(entity_id="Q1", prob=0.9)
+    mykb.add_entity(entity_id="Q2", prob=0.2)
     mykb.add_entity(entity_id="Q3", prob=0.5)
 
     # adding aliases
@@ -16,14 +18,29 @@ def test_kb_valid_entities():
 
 
 def test_kb_invalid_entities():
+    """Test the invalid construction of a KB with an alias linked to a non-existing entity"""
     mykb = KnowledgeBase()
 
     # adding entities
-    mykb.add_entity(entity_id="Q1", prob=0.5)
-    mykb.add_entity(entity_id="Q2", prob=0.5)
+    mykb.add_entity(entity_id="Q1", prob=0.9)
+    mykb.add_entity(entity_id="Q2", prob=0.2)
     mykb.add_entity(entity_id="Q3", prob=0.5)
 
     # adding aliases - should fail because one of the given IDs is not valid
     with pytest.raises(ValueError):
         mykb.add_alias(alias="douglassss", entities=["Q2", "Q342"], probabilities=[0.8, 0.2])
+
+
+def test_kb_invalid_probabilities():
+    """Test the invalid construction of a KB with wrong prior probabilities"""
+    mykb = KnowledgeBase()
+
+    # adding entities
+    mykb.add_entity(entity_id="Q1", prob=0.9)
+    mykb.add_entity(entity_id="Q2", prob=0.2)
+    mykb.add_entity(entity_id="Q3", prob=0.5)
+
+    # adding aliases - should fail because the sum of the probabilities exceeds 1
+    with pytest.raises(ValueError):
+        mykb.add_alias(alias="douglassss", entities=["Q2", "Q3"], probabilities=[0.8, 0.4])
 
