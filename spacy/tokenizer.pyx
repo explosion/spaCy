e65b5bb9a027af0c290c41a90b89728e0e77f9ae
Matthew Honnibal <honnibal+gh@gmail.com>
2019-03-22 13:42:47 +0100

    Fix tokenizer on Python2.7 (#3460)
    spaCy v2.1 switched to the built-in re module, where v2.0 had been using
    the third-party regex library. When the tokenizer was deserialized on
    Python2.7, the `re.compile()` function was called with expressions that
    featured escaped unicode codepoints that were not in Python2.7's unicode
    database.
    
    Problems occurred when we had a range between two of these unknown
    codepoints, like this:
    
    ```
        '[\\uAA77-\\uAA79]'
    ```
    
    On Python2.7, the unknown codepoints are not unescaped correctly,
    resulting in arbitrary out-of-range characters being matched by the
    expression.
    
    This problem does not occur if we instead have a range between two
    unicode literals, rather than the escape sequences. To fix the bug, we
    therefore add a new compat function that unescapes unicode sequences
    using the `ast.literal_eval()` function. Care is taken to ensure we
    do not also escape non-unicode sequences.
    
    Closes #3356.
    
    - [x] I have submitted the spaCy Contributor Agreement.
    - [x] I ran the tests, and all new and existing tests passed.
    - [x] My changes don't require a change to the documentation, or if they do, I've added all required information.

--- spacy/tokenizer.pyx
+++ spacy/tokenizer.pyx
@@ -14,6 +14,7 @@ import re
 
 from .tokens.doc cimport Doc
 from .strings cimport hash_string
+from .compat import unescape_unicode
 
 from .errors import Errors, Warnings, deprecation_warning
 from . import util
@@ -428,6 +429,9 @@ cdef class Tokenizer:
         ))
         exclude = util.get_serialization_exclude(deserializers, exclude, kwargs)
         msg = util.from_bytes(bytes_data, deserializers, exclude)
+        for key in ["prefix_search", "suffix_search", "infix_finditer"]:
+            if key in data:
+                data[key] = unescape_unicode(data[key])
         if data.get("prefix_search"):
             self.prefix_search = re.compile(data["prefix_search"]).search
         if data.get("suffix_search"):
