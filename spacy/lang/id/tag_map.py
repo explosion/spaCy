3cc897102fb6059698f9a619d619556f30b4ba70
jeannefukumaru <fukumaruuu@gmail.com>
2019-04-01 00:00:08 +0800

    added tag_map for indonesian

---
+++ spacy/lang/id/tag_map.py
@@ -0,0 +1,31 @@
+# coding: utf8
+
+'''
+POS explanations for indonesian available from https://www.aclweb.org/anthology/Y12-1014
+'''
+
+TAG_MAP = {
+    "NSD" : {POS: NOUN}, 
+    "Z–" : {POS: PUNCT},
+    "VSA" : {POS: VERB}, 
+    "CC-" : {POS: NUM}, 
+    "R–" : {POS: ADP}, 
+    "D–" : {POS: ADV},
+    "ASP": {POS: ADJ}, 
+    "S–" : {POS: SCONJ}, 
+    "VSP" : {POS: VERB}, 
+    "H–" : {POS: CCONJ}, 
+    "F–" : {POS: X}, 
+    "B–" : {POS: DET}, 
+    "CO-" : {POS: NUM},
+    "G–" : {POS: ADV}, 
+    "PS3" : {POS: PRON}, 
+    "W–" : {POS: ADV}, 
+    "O–" : {POS: AUX}, 
+    "PP1" : {POS: PRON}, 
+    "ASS" : {POS: ADJ}, 
+    "PS1" : {POS: PRON}, 
+    "APP" : {POS: ADJ}, 
+    "CD-" : {POS: NUM}, 
+    "VPA" : {POS: VERB}, 
+    "VPP" : {POS: VERB}}
