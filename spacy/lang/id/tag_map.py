8d6b5446328f4785d75ec99dedd2a0d238bfaeb5
Ines Montani <ines@ines.io>
2019-04-01 11:45:43 +0200

    Auto-format

--- spacy/lang/id/tag_map.py
+++ spacy/lang/id/tag_map.py
@@ -1,37 +1,34 @@
 # coding: utf8
-
 from __future__ import unicode_literals
 
-from ...symbols import POS, PUNCT, SYM, ADJ, CCONJ, NUM, DET, ADV, ADP, X, VERB 
-from ...symbols import NOUN, PROPN, PART, INTJ, SPACE, PRON, AUX, SCONJ 
+from ...symbols import POS, PUNCT, SYM, ADJ, CCONJ, NUM, DET, ADV, ADP, X, VERB
+from ...symbols import NOUN, PROPN, PART, INTJ, SPACE, PRON, AUX, SCONJ
 
 
 # POS explanations for indonesian available from https://www.aclweb.org/anthology/Y12-1014
-
-
 TAG_MAP = {
-    "NSD" : {POS: NOUN}, 
-    "Z–" : {POS: PUNCT},
-    "VSA" : {POS: VERB}, 
-    "CC-" : {POS: NUM}, 
-    "R–" : {POS: ADP}, 
-    "D–" : {POS: ADV},
-    "ASP": {POS: ADJ}, 
-    "S–" : {POS: SCONJ}, 
-    "VSP" : {POS: VERB}, 
-    "H–" : {POS: CCONJ}, 
-    "F–" : {POS: X}, 
-    "B–" : {POS: DET}, 
-    "CO-" : {POS: NUM},
-    "G–" : {POS: ADV}, 
-    "PS3" : {POS: PRON}, 
-    "W–" : {POS: ADV}, 
-    "O–" : {POS: AUX}, 
-    "PP1" : {POS: PRON}, 
-    "ASS" : {POS: ADJ}, 
-    "PS1" : {POS: PRON}, 
-    "APP" : {POS: ADJ}, 
-    "CD-" : {POS: NUM}, 
-    "VPA" : {POS: VERB}, 
-    "VPP" : {POS: VERB},
+    "NSD": {POS: NOUN},
+    "Z–": {POS: PUNCT},
+    "VSA": {POS: VERB},
+    "CC-": {POS: NUM},
+    "R–": {POS: ADP},
+    "D–": {POS: ADV},
+    "ASP": {POS: ADJ},
+    "S–": {POS: SCONJ},
+    "VSP": {POS: VERB},
+    "H–": {POS: CCONJ},
+    "F–": {POS: X},
+    "B–": {POS: DET},
+    "CO-": {POS: NUM},
+    "G–": {POS: ADV},
+    "PS3": {POS: PRON},
+    "W–": {POS: ADV},
+    "O–": {POS: AUX},
+    "PP1": {POS: PRON},
+    "ASS": {POS: ADJ},
+    "PS1": {POS: PRON},
+    "APP": {POS: ADJ},
+    "CD-": {POS: NUM},
+    "VPA": {POS: VERB},
+    "VPP": {POS: VERB},
 }
