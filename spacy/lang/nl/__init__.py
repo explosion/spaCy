951825532c6380b88c29db31d027f87f3f45990f
Yves Peirsman <yves.peirsman@gmail.com>
2019-04-03 14:13:26 +0200

    Improved Dutch language resources and Dutch lemmatization (#3409)
    * Improved Dutch language resources and Dutch lemmatization
    
    * Fix conftest
    
    * Update punctuation.py
    
    * Auto-format
    
    * Format and fix tests
    
    * Remove unused test file
    
    * Re-add deleted test
    
    * removed redundant infix regex pattern for ','; note: brackets + simple hyphen remains
    
    * Cleaner lemmatization files

--- spacy/lang/nl/__init__.py
+++ spacy/lang/nl/__init__.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
 from .tag_map import TAG_MAP
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .punctuation import TOKENIZER_INFIXES, TOKENIZER_SUFFIXES
+
+from .lemmatizer import LOOKUP, LEMMA_EXC, LEMMA_INDEX, RULES
+from .lemmatizer.lemmatizer import DutchLemmatizer
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
@@ -13,20 +18,33 @@ from ...util import update_exc, add_lookups
 
 
 class DutchDefaults(Language.Defaults):
+
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters.update(LEX_ATTRS)
-    lex_attr_getters[LANG] = lambda text: "nl"
-    lex_attr_getters[NORM] = add_lookups(
-        Language.Defaults.lex_attr_getters[NORM], BASE_NORMS
-    )
-    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS)
+    lex_attr_getters[LANG] = lambda text: 'nl'
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM],
+                                         BASE_NORMS)
+    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     stop_words = STOP_WORDS
     tag_map = TAG_MAP
+    infixes = TOKENIZER_INFIXES
+    suffixes = TOKENIZER_SUFFIXES
+
+    @classmethod
+    def create_lemmatizer(cls, nlp=None):
+        rules = RULES
+        lemma_index = LEMMA_INDEX
+        lemma_exc = LEMMA_EXC
+        lemma_lookup = LOOKUP
+        return DutchLemmatizer(index=lemma_index,
+                               exceptions=lemma_exc,
+                               lookup=lemma_lookup,
+                               rules=rules)
 
 
 class Dutch(Language):
-    lang = "nl"
+    lang = 'nl'
     Defaults = DutchDefaults
 
 
-__all__ = ["Dutch"]
+__all__ = ['Dutch']
---
+++ spacy/lang/nl/lemmatizer/_adpositions_irreg.py
@@ -0,0 +1,12 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+ADPOSITIONS_IRREG = {
+    "'t": ('te',),
+    'me': ('mee',),
+    'meer': ('mee',),
+    'on': ('om',),
+    'ten': ('te',),
+    'ter': ('te',)
+}
