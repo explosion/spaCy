3621b07e41bfafdae88b706f4e26f3565d72b2bf
pksiazek <piotr.ksiazek96@gmail.com>
2019-04-09 15:19:30 +0200

    Trie lemmatizer

--- spacy/lang/pl/lemmatizer/lemmatizer.py
+++ spacy/lang/pl/lemmatizer/lemmatizer.py
@@ -2,6 +2,7 @@
 import re
 
 from ....symbols import NOUN, VERB, ADJ, PUNCT, ADV, PART
+from .trie import Trie, trie_from_rules
 
 
 class PolishLemmatizer(object):
@@ -14,6 +15,9 @@ class PolishLemmatizer(object):
         self.exc = exceptions
         self.rules = rules
         self.lookup_table = lookup if lookup is not None else {}
+        self.tries = {}
+        for univ_pos in ['noun', 'verb', 'adj', 'adv', 'part']:
+            self.tries[univ_pos] = trie_from_rules(self.rules.get(univ_pos, []) + self.rules.get('other', {}))
 
     def __call__(self, string, univ_pos, morphology=None):
         if univ_pos in (NOUN, 'NOUN', 'noun'):
@@ -33,16 +37,38 @@ class PolishLemmatizer(object):
         
         exceptions = self.exc.get(univ_pos, {}).copy()
         exceptions.update(self.exc.get('other', {}))
-        lemmas = lemmatize(
-            string,
-            self.index.get(univ_pos, {}) | self.index.get('other', {}),
-            exceptions,
-            self.rules.get(univ_pos, []) + self.rules.get('other', {})
-        )
+        lemmas = lemmatize_trie(string,
+                                self.index.get(univ_pos, {}) | self.index.get('other', {}),
+                                exceptions,
+                                self.tries[univ_pos])
+        # lemmas = lemmatize(
+        #     string,
+        #     self.index.get(univ_pos, {}) | self.index.get('other', {}),
+        #     exceptions,
+        #     self.rules.get(univ_pos, []) + self.rules.get('other', {})
+        # )
         return lemmas
 
     def lookup(self, string):
         return string.lower()
+
+def lemmatize_trie(string, index, exceptions, trie):
+    forms = []
+    oov_forms = []
+    matches = trie.get_rules(string)
+    for suf_start, matched_rules in matches:
+        for rule in matched_rules:
+            form = re.sub(rule[0], rule[1], string)
+            if form in index or not form.isalpha():
+                forms.append(form)
+            else:
+                oov_forms.append(form)
+
+    if not forms:
+        forms.extend(oov_forms)
+    if not forms:
+        forms.append(string)
+    return list(set(forms))
 
 
 def lemmatize(string, index, exceptions, rules):
---
+++ spacy/lang/pl/lemmatizer/trie.py
@@ -0,0 +1,72 @@
+import re
+import time
+import sre_yield
+import string
+from ...char_classes import LATIN_LOWER
+
+# ALPHABET_SIZE = ord('z') - ord('a') + 1 + len(polish_special_chars)
+ALPHABET_SIZE = len(LATIN_LOWER)
+charset = LATIN_LOWER
+
+idx_map = {}
+for i, char in enumerate(LATIN_LOWER):
+    idx_map[char] = i
+
+def reverse(string):
+    return "".join(reversed(string))
+
+def get_idx(c):
+    return idx_map[c]
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word, rule):
+        self.root.insert(word, rule)
+
+    def get_rules(self, word):
+        return self.root.get_rules(word)
+
+
+class Node:
+    def __init__(self):
+        self.is_end = False
+        self.children = ALPHABET_SIZE*[None]
+        self.rules = []
+
+    def insert(self, word, rule):
+        if word == "":
+            self.is_end = True
+            self.rules += [rule]
+        else:
+            child = self.children[get_idx(word[0])]
+            if child is None:
+                child = Node()
+                self.children[get_idx(word[0])] = child
+            child.insert(word[1:], rule)
+
+    def get_rules(self, word):
+        res = []
+        if self.is_end:
+            res += [self.rules]
+        if word == "":
+            return res
+        head = word[-1]
+        tail = word[:-1]
+        if self.children[get_idx(head)] is None:
+            return res
+        return res + self.children[get_idx(head)].get_rules(tail)
+
+def trie_from_rules(rules):
+    # rules are expected to be in format:
+    # [(suffix, (regex, replacement))]
+    trie = Trie()
+    for suf, regexp, rule in rules:
+        trie.insert(reverse(suf), (regexp, rule))
+
+    return trie
+
+
+
+
