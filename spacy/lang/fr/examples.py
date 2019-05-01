8952004dfcb2eb43c5c8bbf7c1b74df2fcd98281
BreakBB <33514570+BreakBB@users.noreply.github.com>
2019-05-01 12:01:35 +0200

    Update French example sents and add two German stop words (#3662)
    * Update french example sentences
    
    * Add 'anderem' and 'ihren' to German stop words

--- spacy/lang/fr/examples.py
+++ spacy/lang/fr/examples.py
@@ -11,9 +11,9 @@ Example sentences to test spaCy and its language models.
 
 
 sentences = [
-    "Apple cherche a acheter une startup anglaise pour 1 milliard de dollard",
-    "Les voitures autonomes voient leur assurances décalées vers les constructeurs",
-    "San Francisco envisage d'interdire les robots coursiers",
+    "Apple cherche à acheter une startup anglaise pour 1 milliard de dollars",
+    "Les voitures autonomes déplacent la responsabilité de l'assurance vers les constructeurs",
+    "San Francisco envisage d'interdire les robots coursiers sur les trottoirs",
     "Londres est une grande ville du Royaume-Uni",
     "L’Italie choisit ArcelorMittal pour reprendre la plus grande aciérie d’Europe",
     "Apple lance HomePod parce qu'il se sent menacé par l'Echo d'Amazon",
