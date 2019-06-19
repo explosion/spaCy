9abdaaa2757150cdeca15295b2135655573970db
Rokas Ramanauskas <rokasramas@gmail.com>
2019-06-19 12:08:49 +0300

    test: add native examples to basic tests

--- spacy/tests/lang/lt/test_basic.py
+++ spacy/tests/lang/lt/test_basic.py
@@ -2,17 +2,10 @@ from __future__ import unicode_literals
 
 import pytest
 
-from spacy.lang.lt import LithuanianLanguage
 
-util.set_lang_class('lt', LithuanianLanguage)
-lang_class = util.get_lang_class('lt')
-nlp = lang_class()
-
-# Change to Lithuanian
 @pytest.mark.parametrize('text,length', [
-    ("The U.S. Army likes Shock and Awe.", 8),
-    ("U.N. regulations are not a part of their concern.", 10),
-    ("“Isn't it?”", 6)])
-def test_en_tokenizer_handles_punct_abbrev(en_tokenizer, text, length):
-    tokens = en_tokenizer(text)
+    ("177R Parodų rūmai–Ozo g. nuo vasario 18 d. bus skelbiamas interneto tinklalapyje.", 15),
+    ("ISM universiteto doc. dr. Ieva Augutytė-Kvedaravičienė pastebi, kad tyrimais nustatyti elgesio pokyčiai.", 16)])
+def test_lt_tokenizer_handles_punct_abbrev(lt_tokenizer, text, length):
+    tokens = lt_tokenizer(text)
     assert len(tokens) == length
