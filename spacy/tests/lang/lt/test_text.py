91d8054ccdf92a9a5ad9bc9b191f5d664abe7087
Ines Montani <ines@ines.io>
2019-07-11 12:16:43 +0200

    Fix test

--- spacy/tests/lang/lt/test_text.py
+++ spacy/tests/lang/lt/test_text.py
@@ -5,10 +5,8 @@ import pytest
 
 
 def test_lt_tokenizer_handles_long_text(lt_tokenizer):
-    text = """Tokios sausros kriterijus atitinka pirmadienį atlikti skaičiavimai, palyginus faktinį ir žemiausią
-vidutinį daugiametį vandens lygį. Nustatyta, kad iš 48 šalies vandens matavimo stočių 28-iose stotyse vandens lygis
-yra žemesnis arba lygus žemiausiam vidutiniam daugiamečiam šiltojo laikotarpio vandens lygiui."""
-    tokens = lt_tokenizer(text.replace("\n", ""))
+    text = """Tokios sausros kriterijus atitinka pirmadienį atlikti skaičiavimai, palyginus faktinį ir žemiausią vidutinį daugiametį vandens lygį. Nustatyta, kad iš 48 šalies vandens matavimo stočių 28-iose stotyse vandens lygis yra žemesnis arba lygus žemiausiam vidutiniam daugiamečiam šiltojo laikotarpio vandens lygiui."""
+    tokens = lt_tokenizer(text)
     assert len(tokens) == 42
 
 
