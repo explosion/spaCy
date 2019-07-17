54c29ebafb1b0bb5a9b69d811d7057776cd93c2b
Ines Montani <ines@ines.io>
2019-07-17 12:34:13 +0200

    Auto-format [ci skip]

--- spacy/tests/regression/test_issue3611.py
+++ spacy/tests/regression/test_issue3611.py
@@ -1,7 +1,6 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-import pytest
 import spacy
 from spacy.util import minibatch, compounding
 
@@ -9,27 +8,25 @@ from spacy.util import minibatch, compounding
 def test_issue3611():
     """ Test whether adding n-grams in the textcat works even when n > token length of some docs """
     unique_classes = ["offensive", "inoffensive"]
-    x_train = ["This is an offensive text",
-               "This is the second offensive text",
-               "inoff"]
+    x_train = [
+        "This is an offensive text",
+        "This is the second offensive text",
+        "inoff",
+    ]
     y_train = ["offensive", "offensive", "inoffensive"]
 
     # preparing the data
     pos_cats = list()
     for train_instance in y_train:
         pos_cats.append({label: label == train_instance for label in unique_classes})
-    train_data = list(zip(x_train, [{'cats': cats} for cats in pos_cats]))
+    train_data = list(zip(x_train, [{"cats": cats} for cats in pos_cats]))
 
     # set up the spacy model with a text categorizer component
-    nlp = spacy.blank('en')
+    nlp = spacy.blank("en")
 
     textcat = nlp.create_pipe(
         "textcat",
-        config={
-            "exclusive_classes": True,
-            "architecture": "bow",
-            "ngram_size": 2
-        }
+        config={"exclusive_classes": True, "architecture": "bow", "ngram_size": 2},
     )
 
     for label in unique_classes:
@@ -37,7 +34,7 @@ def test_issue3611():
     nlp.add_pipe(textcat, last=True)
 
     # training the network
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'textcat']
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "textcat"]
     with nlp.disable_pipes(*other_pipes):
         optimizer = nlp.begin_training()
         for i in range(3):
@@ -46,6 +43,10 @@ def test_issue3611():
 
             for batch in batches:
                 texts, annotations = zip(*batch)
-                nlp.update(docs=texts, golds=annotations, sgd=optimizer, drop=0.1, losses=losses)
-
-
+                nlp.update(
+                    docs=texts,
+                    golds=annotations,
+                    sgd=optimizer,
+                    drop=0.1,
+                    losses=losses,
+                )
