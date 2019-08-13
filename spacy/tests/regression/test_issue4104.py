2bebc1647fe32ac394830cce4138d9db4ea80e11
AJ Rader <ARader@dmcinsurance.com>
2019-08-13 11:26:39 -0400

    eliminated parameterization and sorted dictionary dependnency in issue 4104 test

--- spacy/tests/regression/test_issue4104.py
+++ spacy/tests/regression/test_issue4104.py
@@ -1,13 +1,14 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-import pytest
 from ..util import get_doc
 
-@pytest.mark.parametrize('text', ['dry spun spun-dry'])
-
-def test_issue4104(en_tokenizer, en_vocab, text):
-    """Test that English lookup lemmatization of spun & dry are correct"""
+def test_issue4104(en_vocab):
+    """Test that English lookup lemmatization of spun & dry are correct
+    expected mapping = {'dry': 'dry', 'spun': 'spin', 'spun-dry': 'spin-dry'}
+	"""
+    text = 'dry spun spun-dry'
     doc = get_doc(en_vocab, [t for t in text.split(" ")])
-    expected = {'dry': 'dry', 'spun': 'spin', 'spun-dry': 'spin-dry'}
-    assert [token.lemma_ for token in doc] == list(expected.values())
+    # using a simple list to preserve order
+    expected = ['dry', 'spin', 'spin-dry']
+    assert [token.lemma_ for token in doc] == expected
