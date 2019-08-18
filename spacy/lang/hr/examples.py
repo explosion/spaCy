68ea390da368475f84f66aa6e1ae8aa6fce45203
Ivan Šarić <ivansari@gmail.com>
2019-08-18 20:30:34 +0200

    adds examples.py for croatian language

---
+++ spacy/lang/hr/examples.py
@@ -0,0 +1,18 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.hr.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+sentences = [
+    "Ovo je rečenica.",
+    "Kako se popravlja auto?",
+    "Zagreb je udaljen od Ljubljane svega 150 km.",
+    "Nećete vjerovati što se dogodilo na ovogodišnjem festivalu!",
+    "Budućnost Apple je upitna nakon dugotrajnog pada vrijednosti dionica firme.",
+    "Trgovina oružjem predstavlja prijetnju za globalni mir.",
+]
