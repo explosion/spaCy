5105497e3ea6dfbc226dce9bb9f88d29ef30fc45
Adriane Boyd <adrianeboyd@gmail.com>
2019-08-29 10:11:08 +0200

    Add more token attributes to token pattern schema
    Add token attributes with tests to token pattern schema.

--- spacy/tests/matcher/test_matcher_api.py
+++ spacy/tests/matcher/test_matcher_api.py
@@ -380,3 +380,33 @@ def test_attr_pipeline_checks(en_vocab):
     matcher(doc1)
     matcher(doc2)
     matcher(doc3)
+
+
+@pytest.mark.parametrize(
+    "pattern,text",
+    [
+        ([{"IS_ALPHA": True}], "a"),
+        ([{"IS_ASCII": True}], "a"),
+        ([{"IS_DIGIT": True}], "1"),
+        ([{"IS_LOWER": True}], "a"),
+        ([{"IS_UPPER": True}], "A"),
+        ([{"IS_TITLE": True}], "Aaaa"),
+        ([{"IS_PUNCT": True}], "."),
+        ([{"IS_SPACE": True}], "\n"),
+        ([{"IS_BRACKET": True}], "["),
+        ([{"IS_QUOTE": True}], "\""),
+        ([{"IS_LEFT_PUNCT": True}], "``"),
+        ([{"IS_RIGHT_PUNCT": True}], "''"),
+        ([{"IS_STOP": True}], "the"),
+        ([{"LIKE_NUM": True}], "1"),
+        ([{"LIKE_URL": True}], "http://example.com"),
+        ([{"LIKE_EMAIL": True}], "mail@example.com"),
+    ],
+)
+def test_matcher_schema_token_attributes(en_vocab, pattern, text):
+    matcher = Matcher(en_vocab)
+    doc = Doc(en_vocab, words=text.split(' '))
+    matcher.add("Rule", None, pattern)
+    assert len(matcher) == 1
+    matches = matcher(doc)
+    assert len(matches) == 1
