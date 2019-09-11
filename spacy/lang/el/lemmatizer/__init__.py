8d2fd5d7f2f91bf554be8be5bb307afa61db1486
Ines Montani <ines@ines.io>
2019-09-11 13:55:52 +0200

    Make recently added Greek method compatible

--- spacy/lang/el/lemmatizer/__init__.py
+++ spacy/lang/el/lemmatizer/__init__.py
@@ -46,9 +46,9 @@ class GreekLemmatizer(object):
         )
         return lemmas
 
-    def lookup(self, string):
-        if string in self.lookup_table:
-            return self.lookup_table[string]
+    def lookup(self, orth, string):
+        if orth in self.lookup_table:
+            return self.lookup_table[orth]
         return string
 
 
