699f97838022cde063638477cbe43b1668bde72a
Adriane Boyd <adrianeboyd@gmail.com>
2019-09-14 19:50:54 +0200

    Add textcat score to early stopping check

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -458,6 +458,8 @@ def _score_for_model(meta):
         mean_acc.append((acc["uas"] + acc["las"]) / 2)
     if "ner" in pipes:
         mean_acc.append((acc["ents_p"] + acc["ents_r"] + acc["ents_f"]) / 3)
+    if "textcat" in pipes:
+        mean_acc.append(acc["textcat_score"])
     return sum(mean_acc) / len(mean_acc)
 
 
