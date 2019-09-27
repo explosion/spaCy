c1e01c0120f375a354de76b5c2cb69e363d42af8
svlandeg <sofie.vanlandeghem@gmail.com>
2019-09-27 17:13:56 +0200

    fix for first bug in 4042

--- spacy/tests/regression/test_issue4042.py
+++ spacy/tests/regression/test_issue4042.py
@@ -1,7 +1,9 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from spacy.pipeline import EntityRecognizer
+import spacy
+
+from spacy.pipeline import EntityRecognizer, EntityRuler
 
 from spacy.lang.en import English
 from spacy.tests.util import make_tempdir
@@ -10,7 +12,42 @@ from spacy.util import ensure_path
 
 
 def test_issue4042():
-    """Test that serialization of an NER works fine when new labels were added."""
+    """Test that serialization of an EntityRuler before NER works fine."""
+    nlp = English()
+
+    # add ner pipe
+    ner = nlp.create_pipe("ner")
+    ner.add_label("SOME_LABEL")
+    nlp.add_pipe(ner)
+    nlp.begin_training()
+
+    # Add entity ruler
+    ruler = EntityRuler(nlp)
+    patterns = [
+        {"label": "MY_ORG", "pattern": "Apple"},
+        {"label": "MY_GPE", "pattern": [{"lower": "san"}, {"lower": "francisco"}]},
+    ]
+    ruler.add_patterns(patterns)
+    nlp.add_pipe(ruler, before="ner")  # works fine with "after"
+    doc1 = nlp("What do you think about Apple ?")
+    assert doc1.ents[0].label_ == "MY_ORG"
+
+    with make_tempdir() as d:
+        output_dir = ensure_path(d)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.to_disk(output_dir)
+
+        nlp2 = spacy.load(output_dir)
+        doc2 = nlp2("What do you think about Apple ?")
+        assert doc2.ents[0].label_ == "MY_ORG"
+
+
+def test_issue4042_bug2():
+    """
+    Test that serialization of an NER works fine when new labels were added.
+    This is the second bug of two bugs underlying the issue 4042.
+    """
     nlp1 = English()
     vocab = nlp1.vocab
 
