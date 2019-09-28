0df2a599b7bb3ab79e1ed89185ea15ed90f6d2ca
Matthew Honnibal <honnibal+gh@gmail.com>
2019-09-28 21:26:05 +0200

    Set version to v2.2.0.dev13

--- spacy/about.py
+++ spacy/about.py
@@ -1,6 +1,6 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "2.2.0.dev12"
+__version__ = "2.2.0.dev13"
 __summary__ = "Industrial-strength Natural Language Processing (NLP) in Python"
 __uri__ = "https://spacy.io"
 __author__ = "Explosion"
