b5c775dd42276530b48a1670095fdb476d123e47
Matthew Honnibal <honnibal+gh@gmail.com>
2019-09-30 12:47:08 +0200

    Set version to v2.2.0

--- spacy/about.py
+++ spacy/about.py
@@ -1,12 +1,12 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "2.2.0.dev15"
+__version__ = "2.2.0"
 __summary__ = "Industrial-strength Natural Language Processing (NLP) in Python"
 __uri__ = "https://spacy.io"
 __author__ = "Explosion"
 __email__ = "contact@explosion.ai"
 __license__ = "MIT"
-__release__ = False
+__release__ = True
 
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
