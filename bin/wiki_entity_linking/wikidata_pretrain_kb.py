ca89cca9fe5dc8bc4388bf4ddfffbfc0a3b1292e
svlandeg <sofie.vanlandeghem@gmail.com>
2019-10-08 21:09:54 +0200

    reading KB is unnecessary at the end of the pipeline

--- bin/wiki_entity_linking/wikidata_pretrain_kb.py
+++ bin/wiki_entity_linking/wikidata_pretrain_kb.py
@@ -165,14 +165,10 @@ def main(
             entity_vector_length=entity_vector_length,
         )
         kb.dump(kb_path)
+        logger.info("kb entities: {}".format(kb.get_size_entities()))
+        logger.info("kb aliases: {}".format(kb.get_size_aliases()))
         nlp.to_disk(output_dir / KB_MODEL_DIR)
-    else:
-        logger.info("STEP 6: Reading the KB from {}".format(kb_path))
-        # The KB is not used at this point but will be verified to work correctly + log stats
-        kb = read_kb(nlp, kb_path)
 
-    logger.info("kb entities: {}".format(kb.get_size_entities()))
-    logger.info("kb aliases: {}".format(kb.get_size_aliases()))
     logger.info("Done!")
 
 
