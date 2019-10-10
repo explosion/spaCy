9a3edff2cbde4e0c09349ecdd278ef894c288e43
Peter Gilles <PeterGilles@users.noreply.github.com>
2019-10-10 17:06:23 +0200

    Update norm_exceptions.py

--- spacy/lang/lb/norm_exceptions.py
+++ spacy/lang/lb/norm_exceptions.py
@@ -1,6 +1,9 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+# TODO
+# norm execptions: find a possibility to deal with the zillions of spelling variants (vläicht = vlaicht, vleicht, viläicht, viläischt, etc. etc.)
+
 # here one could include the most common spelling mistakes
 
 _exc = {
