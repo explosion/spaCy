f399c343dc2f31ec08d344797c07bf56cb78e9fa
Ines Montani <ines@ines.io>
2019-10-27 11:59:29 +0100

    Add unicode declarations and don't skip test on Python 2

--- spacy/tests/regression/test_issue4529.py
+++ spacy/tests/regression/test_issue4529.py
@@ -1,12 +1,13 @@
+# coding: utf8
+from __future__ import unicode_literals
+
 import pytest
-from spacy.compat import is_python2
 from spacy.gold import GoldParse
 
 
 @pytest.mark.parametrize(
     "text,words", [("A'B C", ["A", "'", "B", "C"]), ("A-B", ["A-B"])]
 )
-@pytest.mark.skipif(is_python2, reason="I don't want to spend time on python2 support")
 def test_gold_misaligned(en_tokenizer, text, words):
     doc = en_tokenizer(text)
     GoldParse(doc, words=words)
