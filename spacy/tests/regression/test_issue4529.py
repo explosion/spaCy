6ed1e33cc8d0648a670e446685de7806fa853597
Yohei Tamura <tamuhey@gmail.com>
2019-10-27 17:55:59 +0900

    skip test in python2

--- spacy/tests/regression/test_issue4529.py
+++ spacy/tests/regression/test_issue4529.py
@@ -1,10 +1,12 @@
 import pytest
+from spacy.compat import is_python2
 from spacy.gold import GoldParse
 
 
 @pytest.mark.parametrize(
     "text,words", [("A'B C", ["A", "'", "B", "C"]), ("A-B", ["A-B"])]
 )
+@pytest.mark.skipif(is_python2, reason="I don't want to spend time on python2 support")
 def test_gold_misaligned(en_tokenizer, text, words):
     doc = en_tokenizer(text)
     GoldParse(doc, words=words)
