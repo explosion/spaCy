427c319005000e6d7c021643c9b21dac629f66b6
Ines Montani <ines@ines.io>
2019-10-28 13:27:13 +0100

    Fix typo

--- spacy/cli/pretrain.py
+++ spacy/cli/pretrain.py
@@ -167,7 +167,7 @@ def pretrain(
             pretrained_vectors=pretrained_vectors,
             bilstm_depth=bilstm_depth,  # Requires PyTorch. Experimental.
             subword_features=not use_chars,  # Set to False for Chinese etc
-            cnn_maxout_pieces=cnn_piece,  # If set to 1, use Mish activation.
+            cnn_maxout_pieces=cnn_pieces,  # If set to 1, use Mish activation.
         ),
     )
     # Load in pretrained weights
