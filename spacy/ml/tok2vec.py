5e9849b60f42c17d321873af31b14f83b24b4364
Ines Montani <ines@ines.io>
2019-10-30 19:27:18 +0100

    Auto-format [ci skip]

--- spacy/ml/tok2vec.py
+++ spacy/ml/tok2vec.py
@@ -45,12 +45,15 @@ def MultiHashEmbed(config):
 
     norm = HashEmbed(width, rows, column=cols.index("NORM"), name="embed_norm")
     if config["use_subwords"]:
-        prefix = HashEmbed(width, rows // 2,
-            column=cols.index("PREFIX"), name="embed_prefix")
-        suffix = HashEmbed(width, rows // 2,
-            column=cols.index("SUFFIX"), name="embed_suffix")
-        shape = HashEmbed(width, rows // 2,
-            column=cols.index("SHAPE"), name="embed_shape")
+        prefix = HashEmbed(
+            width, rows // 2, column=cols.index("PREFIX"), name="embed_prefix"
+        )
+        suffix = HashEmbed(
+            width, rows // 2, column=cols.index("SUFFIX"), name="embed_suffix"
+        )
+        shape = HashEmbed(
+            width, rows // 2, column=cols.index("SHAPE"), name="embed_shape"
+        )
     if config.get("@pretrained_vectors"):
         glove = make_layer(config["@pretrained_vectors"])
     mix = make_layer(config["@mix"])
@@ -60,20 +63,16 @@ def MultiHashEmbed(config):
             mix._layers[0].nI = width * 5
             layer = uniqued(
                 (glove | norm | prefix | suffix | shape) >> mix,
-                column=cols.index("ORTH")
+                column=cols.index("ORTH"),
             )
         elif config["use_subwords"]:
             mix._layers[0].nI = width * 4
             layer = uniqued(
-                (norm | prefix | suffix | shape) >> mix,
-                column=cols.index("ORTH")
+                (norm | prefix | suffix | shape) >> mix, column=cols.index("ORTH")
             )
         elif config["@pretrained_vectors"]:
             mix._layers[0].nI = width * 2
-            embed = uniqued(
-                (glove | norm) >> mix,
-                column=cols.index("ORTH"),
-            )
+            embed = uniqued((glove | norm) >> mix, column=cols.index("ORTH"),)
         else:
             embed = norm
     layer.cfg = config
