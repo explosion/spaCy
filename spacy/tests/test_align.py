4061fc40e87f4438b0e5a83863a04238fe647833
Adriane Boyd <adrianeboyd@gmail.com>
2019-10-31 11:12:15 +0100

    Add more tests for align() and multi_align()
    * Test that `align()` and `multi_align()` are symmetric
    
    * Test that `align()` handles whitespace in leading / intermediate /
    trailing positions

--- spacy/tests/test_align.py
+++ spacy/tests/test_align.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import pytest
+import numpy as np
 from spacy._align import align, multi_align
 
 
@@ -75,5 +76,81 @@ def test_align_many_to_one():
     assert i2j_multi[5] == 3
     assert i2j_multi[6] == 3
 
-    assert j2i_multi[0] == 1
-    assert j2i_multi[1] == 3
+    assert j2i_multi == {}
+
+
+def test_align_one_to_many():
+    # should be the same as many_to_one except i/j swapped
+    words1 = ["ab", "bc", "e", "fg", "h"]
+    words2 = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(j2i) == [-1, -1, -1, -1, 2, -1, -1, 4]
+    lengths1 = [len(w) for w in words1]
+    lengths2 = [len(w) for w in words2]
+    i2j_multi, j2i_multi = multi_align(i2j, j2i, lengths1, lengths2)
+
+    assert i2j_multi == {}
+
+    assert j2i_multi[0] == 0
+    assert j2i_multi[1] == 0
+    assert j2i_multi[2] == 1
+    assert j2i_multi[3] == 1
+    assert j2i_multi[3] == 1
+    assert j2i_multi[5] == 3
+    assert j2i_multi[6] == 3
+
+
+def test_align_whitespace():
+    words1 = [" ", "a", "b"]
+    words2 = ["a", "b"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(i2j) == [-1, 0, 1]
+    assert list(j2i) == [1, 2]
+
+    words1 = ["a", " ", "b"]
+    words2 = ["a", "b"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(i2j) == [0, -1, 1]
+    assert list(j2i) == [0, 2]
+
+    words1 = ["a", "b"]
+    words2 = ["a", " ", "b"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(i2j) == [0, 2]
+    assert list(j2i) == [0, -1, 1]
+
+    words1 = ["a", "b", " "]
+    words2 = ["a", "b"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(i2j) == [0, 1, -1]
+    assert list(j2i) == [0, 1]
+
+    words1 = ["\n", "a", "\n\n", "b"]
+    words2 = ["a", "b"]
+    cost, i2j, j2i, matrix = align(words1, words2)
+    assert list(i2j) == [-1, 0, -1, 1]
+    assert list(j2i) == [1, 3]
+
+
+def test_align_is_symmetric():
+    words1 = ["a", " ", "b"]
+    words2 = ["a", "B", "\n"]
+    cost_a, i2j_a, j2i_a, matrix_a = align(words1, words2)
+    cost_b, i2j_b, j2i_b, matrix_b = align(words2, words1)
+    assert cost_a == cost_b
+    assert np.array_equal(i2j_a, j2i_b)
+    assert np.array_equal(j2i_a, i2j_b)
+    assert np.array_equal(matrix_a, np.transpose(matrix_b))
+
+
+def test_multi_align_is_symmetric():
+    words1 = ["a", "b", "cd"]
+    words2 = ["ab", "c", "d"]
+    cost_a, i2j_a, j2i_a, matrix_a = align(words1, words2)
+    cost_b, i2j_b, j2i_b, matrix_b = align(words2, words1)
+    lengths1 = [len(w) for w in words1]
+    lengths2 = [len(w) for w in words2]
+    i2j_multi_a, j2i_multi_a = multi_align(i2j_a, j2i_a, lengths1, lengths2)
+    i2j_multi_b, j2i_multi_b = multi_align(i2j_b, j2i_b, lengths2, lengths1)
+    assert np.array_equal(i2j_multi_a, j2i_multi_b)
+    assert np.array_equal(j2i_multi_a, i2j_multi_b)
