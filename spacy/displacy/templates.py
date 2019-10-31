df4c9ae3dca9788a565f67e7e3f0099f89f459d1
Ines Montani <ines@ines.io>
2019-10-31 15:10:25 +0100

    Fix formatting [ci skip]

--- spacy/displacy/templates.py
+++ spacy/displacy/templates.py
@@ -51,7 +51,7 @@ TPL_ENT = """
 """
 
 TPL_ENT_RTL = """
-<mark class="entity" style="background: {bg}; padding: 0.45em 0.6em; margin: 0 0.25em; line-height: 1; border-radius: 0.35em;">
+<mark class="entity" style="background: {bg}; padding: 0.45em 0.6em; margin: 0 0.25em; line-height: 1; border-radius: 0.35em">
     {text}
     <span style="font-size: 0.8em; font-weight: bold; line-height: 1; border-radius: 0.35em; text-transform: uppercase; vertical-align: middle; margin-right: 0.5rem">{label}</span>
 </mark>
