a58e94c4ce2e6e823881a969bf064f1b9a1d38e8
Matthew Honnibal <honnibal+gh@gmail.com>
2019-11-12 16:42:27 +0100

    Set version to v2.2.3.dev0

--- spacy/about.py
+++ spacy/about.py
@@ -1,7 +1,7 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "2.2.2"
-__release__ = True
+__version__ = "2.2.3.dev0"
+__release__ = False
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
 __shortcuts__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/shortcuts-v2.json"
