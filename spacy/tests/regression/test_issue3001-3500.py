2cd25a083bb053e71b1384122c6a83f5ab69e72e
Ines Montani <ines@ines.io>
2019-11-15 16:41:31 +0100

    Un-xfail passing tests

--- spacy/tests/regression/test_issue3001-3500.py
+++ spacy/tests/regression/test_issue3001-3500.py
@@ -177,7 +177,6 @@ def test_issue3328(en_vocab):
     assert matched_texts == ["Hello", "how", "you", "doing"]
 
 
-@pytest.mark.xfail
 def test_issue3331(en_vocab):
     """Test that duplicate patterns for different rules result in multiple
     matches, one per rule.
