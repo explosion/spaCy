a7ee4b6f17b5b66fc5a43d4ba94167ff9bcc16a6
Christoph Purschke <christoph@purschke.info>
2019-12-01 23:08:21 +0100

    new tests & tokenization fixes (#4734)
    - added some tests for tokenization issues
    - fixed some issues with tokenization of words with hyphen infix
    - rewrote the "tokenizer_exceptions.py" file (stemming from the German version)

--- spacy/lang/lb/punctuation.py
+++ spacy/lang/lb/punctuation.py
@@ -1,16 +1,23 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from ..punctuation import TOKENIZER_INFIXES
-from ..char_classes import ALPHA
-
+from ..char_classes import LIST_ELLIPSES, LIST_ICONS
+from ..char_classes import CONCAT_QUOTES, ALPHA, ALPHA_LOWER, ALPHA_UPPER
 
 ELISION = " ' ’ ".strip().replace(" ", "")
-HYPHENS = r"- – — ‐ ‑".strip().replace(" ", "")
 
-
-_infixes = TOKENIZER_INFIXES + [
-    r"(?<=[{a}][{el}])(?=[{a}])".format(a=ALPHA, el=ELISION)
-]
+_infixes = (
+    LIST_ELLIPSES
+    + LIST_ICONS
+    + [
+        r"(?<=[{a}][{el}])(?=[{a}])".format(a=ALPHA, el=ELISION),
+        r"(?<=[{al}])\.(?=[{au}])".format(al=ALPHA_LOWER, au=ALPHA_UPPER),
+        r"(?<=[{a}])[,!?](?=[{a}])".format(a=ALPHA),
+        r"(?<=[{a}])[:<>=](?=[{a}])".format(a=ALPHA),
+        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+        r"(?<=[{a}])--(?=[{a}])".format(a=ALPHA),
+        r"(?<=[0-9])-(?=[0-9])",
+    ]
+)
 
 TOKENIZER_INFIXES = _infixes
--- spacy/lang/lb/tokenizer_exceptions.py
+++ spacy/lang/lb/tokenizer_exceptions.py
@@ -10,7 +10,9 @@ _exc = {}
 
 # translate / delete what is not necessary
 for exc_data in [
-    {ORTH: "wgl.", LEMMA: "wann ech gelift", NORM: "wann ech gelieft"},
+    {ORTH: "'t", LEMMA: "et", NORM: "et"},
+    {ORTH: "'T", LEMMA: "et", NORM: "et"},
+    {ORTH: "wgl.", LEMMA: "wannechgelift", NORM: "wannechgelift"},
     {ORTH: "M.", LEMMA: "Monsieur", NORM: "Monsieur"},
     {ORTH: "Mme.", LEMMA: "Madame", NORM: "Madame"},
     {ORTH: "Dr.", LEMMA: "Dokter", NORM: "Dokter"},
@@ -18,7 +20,7 @@ for exc_data in [
     {ORTH: "asw.", LEMMA: "an sou weider", NORM: "an sou weider"},
     {ORTH: "etc.", LEMMA: "et cetera", NORM: "et cetera"},
     {ORTH: "bzw.", LEMMA: "bezéiungsweis", NORM: "bezéiungsweis"},
-    {ORTH: "Jan.", LEMMA: "Januar", NORM: "Januar"},
+    {ORTH: "Jan.", LEMMA: "Januar", NORM: "Januar"}
 ]:
     _exc[exc_data[ORTH]] = [exc_data]
 
