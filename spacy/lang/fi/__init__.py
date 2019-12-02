19b79ed32202a37eb6cff2178146d20da68e6f5f
Antti Ajanki <antti.ajanki@iki.fi>
2019-12-02 19:16:23 +0200

    A new file for Finnish tokenizer rules instead of including the Danish ones

--- spacy/lang/fi/__init__.py
+++ spacy/lang/fi/__init__.py
@@ -4,14 +4,13 @@ from __future__ import unicode_literals
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
+from .punctuation import TOKENIZER_INFIXES, TOKENIZER_SUFFIXES
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
 from ...language import Language
 from ...attrs import LANG, NORM
 from ...util import update_exc, add_lookups
-
-from ..da.punctuation import TOKENIZER_INFIXES, TOKENIZER_SUFFIXES
 
 
 class FinnishDefaults(Language.Defaults):
