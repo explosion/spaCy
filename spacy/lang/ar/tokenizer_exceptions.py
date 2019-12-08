6198e3881153a7308e48cae7c22ffdecd535a1af
Abdullatif <abal2051@colorado.edu>
2019-12-08 12:23:03 -0700

    fix incorrect label

--- spacy/lang/ar/tokenizer_exceptions.py
+++ spacy/lang/ar/tokenizer_exceptions.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from __future__ import unicode_literals
 
-from ...symbols import ORTH, LEMMA
+from ...symbols import ORTH, LEMMA, NORM
 
 
 _exc = {}
@@ -9,39 +9,39 @@ _exc = {}
 
 # Time
 for exc_data in [
-    {LEMMA: "قبل الميلاد", ORTH: "ق.م"},
-    {LEMMA: "بعد الميلاد", ORTH: "ب. م"},
-    {LEMMA: "هجري", ORTH: ".هـ"},
-    {LEMMA: "توفي", ORTH: ".ت"},
+    {NORM: "قبل الميلاد", ORTH: ".ق.م"},
+    {NORM: "بعد الميلاد", ORTH: ".ب.م"},
+    {NORM: "هجري", ORTH: "هـ"},
+    {NORM: "توفي", ORTH: ".ت"},
 ]:
     _exc[exc_data[ORTH]] = [exc_data]
 
 # Scientific abv.
 for exc_data in [
-    {LEMMA: "صلى الله عليه وسلم", ORTH: "صلعم"},
-    {LEMMA: "الشارح", ORTH: "الشـ"},
-    {LEMMA: "الظاهر", ORTH: "الظـ"},
-    {LEMMA: "أيضًا", ORTH: "أيضـ"},
-    {LEMMA: "إلى آخره", ORTH: "إلخ"},
-    {LEMMA: "انتهى", ORTH: "اهـ"},
-    {LEMMA: "حدّثنا", ORTH: "ثنا"},
-    {LEMMA: "أخبرنا", ORTH: "نا"},
-    {LEMMA: "مصدر سابق", ORTH: "م. س"},
-    {LEMMA: "مصدر نفسه", ORTH: "م. ن"},
+    {NORM: "صلى الله عليه وسلم", ORTH: "صلعم"},
+    {NORM: "الشارح", ORTH: "الشـ"},
+    {NORM: "الظاهر", ORTH: "الظـ"},
+    {NORM: "أيضًا", ORTH: "أيضـ"},
+    {NORM: "إلى آخره", ORTH: "إلخ"},
+    {NORM: "انتهى", ORTH: "اهـ"},
+    {NORM: "حدّثنا", ORTH: "ثنا"},
+    {NORM: "أخبرنا", ORTH: "نا"},
+    {NORM: "مصدر سابق", ORTH: "م. س"},
+    {NORM: "مصدر نفسه", ORTH: "م. ن"},
 ]:
     _exc[exc_data[ORTH]] = [exc_data]
 
 # Titles
 for exc_data in [
-    {LEMMA: "دكتور", ORTH: "د."},
-    {LEMMA: "أستاذ دكتور", ORTH: "أ.د"},
-    {LEMMA: "أستاذ", ORTH: "أ."},
-    {LEMMA: "بروفيسور", ORTH: "ب."},
-    {LEMMA: "مهندس", ORTH: ".م"},
+    {NORM: "دكتور", ORTH: "د."},
+    {NORM: "أستاذ دكتور", ORTH: "أ.د"},
+    {NORM: "أستاذ", ORTH: "أ."},
+    {NORM: "بروفيسور", ORTH: "ب."},
+    {NORM: "مهندس", ORTH: ".م"},
 ]:
     _exc[exc_data[ORTH]] = [exc_data]
 
-for exc_data in [{LEMMA: "تلفون", ORTH: "ت."}, {LEMMA: "صندوق بريد", ORTH: "ص.ب"}]:
+for exc_data in [{NORM: "تلفون", ORTH: "ت."}, {NORM: "صندوق بريد", ORTH: "ص.ب"}]:
     _exc[exc_data[ORTH]] = [exc_data]
 
 TOKENIZER_EXCEPTIONS = _exc
