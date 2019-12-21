cb4145adc7fb9ed7652d0dcc83e0e6770e679235
Ines Montani <ines@ines.io>
2019-12-21 19:04:17 +0100

    Tidy up and auto-format

--- spacy/tests/pipeline/test_tagger.py
+++ spacy/tests/pipeline/test_tagger.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 import pytest
 from spacy.language import Language
-from spacy.pipeline import Tagger
 
 
 def test_label_types():
--- spacy/tests/regression/test_issue4674.py
+++ spacy/tests/regression/test_issue4674.py
@@ -15,7 +15,9 @@ def test_issue4674():
 
     vector1 = [0.9, 1.1, 1.01]
     vector2 = [1.8, 2.25, 2.01]
-    kb.set_entities(entity_list=["Q1", "Q1"], freq_list=[32, 111], vector_list=[vector1, vector2])
+    kb.set_entities(
+        entity_list=["Q1", "Q1"], freq_list=[32, 111], vector_list=[vector1, vector2]
+    )
 
     assert kb.get_size_entities() == 1
 
@@ -31,4 +33,3 @@ def test_issue4674():
         kb2.load_bulk(str(file_path))
 
     assert kb2.get_size_entities() == 1
-
