736feb40fe77b46865577690e2481cea93a7692d
Matthew Honnibal <honnibal+gh@gmail.com>
2020-01-06 00:05:20 +0100

    Temporarily add ml.component_models module

---
+++ spacy/ml/component_models.py
@@ -0,0 +1,28 @@
+def build_text_classifier(*args, **kwargs):
+    raise NotImplementedError
+
+
+def build_simple_cnn_text_classifier(*args, **kwargs):
+    raise NotImplementedError
+
+
+def build_bow_text_classifier(*args, **kwargs):
+    raise NotImplementedError
+
+
+def build_nel_encoder(*args, **kwargs):
+    raise NotImplementedError
+
+
+def masked_language_model(*args, **kwargs):
+    raise NotImplementedError
+
+
+def Tok2Vec(*args, **kwargs):
+    raise NotImplementedError
+
+get_cossim_loss = None
+zero_init = None
+PrecomputableAffine = None
+flatten = None
+PrecomputableAffine = None
