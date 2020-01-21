bc08c53f1470f8e9638054b7eabb12ef9147ed90
Adriane Boyd <adrianeboyd@gmail.com>
2020-01-21 17:37:11 +0100

    Export MorphAnalysis under spacy.tokens

--- spacy/tokens/__init__.py
+++ spacy/tokens/__init__.py
@@ -2,5 +2,6 @@ from .doc import Doc
 from .token import Token
 from .span import Span
 from ._serialize import DocBin
+from .morphanalysis import MorphAnalysis
 
-__all__ = ["Doc", "Token", "Span", "DocBin"]
+__all__ = ["Doc", "Token", "Span", "DocBin", "MorphAnalysis"]
