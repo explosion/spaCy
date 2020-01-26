01278900eb470ad46147fca9710fbfe845b67581
Filip Bednárik <drndos@drndos.sk>
2020-01-26 21:06:59 +0100

    Add SNK Tags

--- spacy/lang/sk/__init__.py
+++ spacy/lang/sk/__init__.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from .stop_words import STOP_WORDS
+from .tag_map import TAG_MAP
+
 from ...language import Language
 from ...attrs import LANG
 
@@ -9,6 +11,7 @@ from ...attrs import LANG
 class SlovakDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: "sk"
+    tag_map = TAG_MAP
     stop_words = STOP_WORDS
 
 
