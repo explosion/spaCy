d4f4060bf33f9f2f028bd9728bdd99b9a2ddc387
Filip Bednárik <drndos@users.noreply.github.com>
2020-02-03 13:03:59 +0100

    Add Slovak language tools implementation (#4943)
    * Add correct stopwords for Slovak language
    
    * Add SNK Tags
    
    * Disable formatting lint for TAGS
    
    * Add example sentences for Slovak language
    
    * Add slovak numerals in base form
    
    * Add lex_attrs to sk init
    
    * Add contributor agreement

--- spacy/lang/sk/__init__.py
+++ spacy/lang/sk/__init__.py
@@ -2,13 +2,18 @@
 from __future__ import unicode_literals
 
 from .stop_words import STOP_WORDS
+from .tag_map import TAG_MAP
+from .lex_attrs import LEX_ATTRS
+
 from ...language import Language
 from ...attrs import LANG
 
 
 class SlovakDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: "sk"
+    tag_map = TAG_MAP
     stop_words = STOP_WORDS
 
 
