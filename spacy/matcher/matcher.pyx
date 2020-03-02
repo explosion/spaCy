6381bbe4da18ac42ab0b7239c49a790c43095c52
Adriane Boyd <adrianeboyd@gmail.com>
2020-03-02 14:44:49 +0100

    Normalize IS_SENT_START to SENT_START for Matcher

--- spacy/matcher/matcher.pyx
+++ spacy/matcher/matcher.pyx
@@ -670,6 +670,8 @@ def _get_attr_values(spec, string_store):
                 continue
             if attr == "TEXT":
                 attr = "ORTH"
+            if attr == "IS_SENT_START":
+                attr = "SENT_START"
             if attr not in TOKEN_PATTERN_SCHEMA["items"]["properties"]:
                 raise ValueError(Errors.E152.format(attr=attr))
             attr = IDS.get(attr)
