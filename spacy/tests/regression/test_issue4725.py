955c202810a7ad38eb1ee401ecb7372ea75ceee6
svlandeg <sofie.vanlandeghem@gmail.com>
2020-03-02 21:29:45 +0100

    fix test

--- spacy/tests/regression/test_issue4725.py
+++ spacy/tests/regression/test_issue4725.py
@@ -19,7 +19,6 @@ def test_issue4725():
     ner = nlp.create_pipe("ner")
     nlp.add_pipe(ner)
     nlp.begin_training()
-    multiprocessing.set_start_method('spawn')
     docs = ["Kurt is in London."] * 10
     for _ in nlp.pipe(docs, batch_size=2, n_process=2):
         pass
