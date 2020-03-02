4d129a2cacc44434b79a302881cd29527996a6cf
svlandeg <sofie.vanlandeghem@gmail.com>
2020-03-02 19:42:56 +0100

    add unit test

---
+++ spacy/tests/regression/test_issue4725.py
@@ -0,0 +1,26 @@
+import numpy
+
+import multiprocessing
+
+from spacy.lang.en import English
+from spacy.vocab import Vocab
+
+
+def test_issue4725():
+    # ensures that this runs correctly and doesn't hang or crash because of the global vectors
+    vocab = Vocab(vectors_name="test_vocab_add_vector")
+    data = numpy.ndarray((5, 3), dtype="f")
+    data[0] = 1.0
+    data[1] = 2.0
+    vocab.set_vector("cat", data[0])
+    vocab.set_vector("dog", data[1])
+
+    nlp = English(vocab=vocab)
+    ner = nlp.create_pipe("ner")
+    nlp.add_pipe(ner)
+    nlp.begin_training()
+    multiprocessing.set_start_method('spawn')
+    docs = ["Kurt is in London."] * 10
+    for _ in nlp.pipe(docs, batch_size=2, n_process=2):
+        pass
+
