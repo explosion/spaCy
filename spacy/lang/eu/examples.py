03376c9d9bea0dd850bd2612521843f6c8f580ba
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-03-04 11:58:56 +0500

    Basque language added and tested.

---
+++ spacy/lang/eu/examples.py
@@ -0,0 +1,16 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.eu.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "",
+    ""
+]
