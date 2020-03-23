59d56ea18f499558acbebfb5d1b17212873dffda
Adriane Boyd <adrianeboyd@gmail.com>
2020-03-23 17:25:13 +0100

    Omit per_type scores from model-best calculations
    The addition of per_type scores to the included metrics (#4911) causes
    errors when they're compared while determining the best model, so omit
    them for this `max()` comparison.

--- spacy/cli/train.py
+++ spacy/cli/train.py
@@ -661,6 +661,8 @@ def _find_best(experiment_dir, component):
         if epoch_model.is_dir() and epoch_model.parts[-1] != "model-final":
             accs = srsly.read_json(epoch_model / "accuracy.json")
             scores = [accs.get(metric, 0.0) for metric in _get_metrics(component)]
+            # remove per_type dicts from score list for max() comparison
+            scores = [score for score in scores if isinstance(score, float)]
             accuracies.append((scores, epoch_model))
     if accuracies:
         return max(accuracies)[1]
