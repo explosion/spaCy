721746c6ddcecfdf8de47bbbcc02d4ff87f7ec51
Adriane Boyd <adrianeboyd@gmail.com>
2020-03-23 20:41:17 +0100

    Restore test of evaluate on an empty doc

--- spacy/tests/regression/test_issue4924.py
+++ spacy/tests/regression/test_issue4924.py
@@ -5,5 +5,4 @@ from spacy.language import Language
 def test_evaluate():
     nlp = Language()
     docs_golds = [("", {})]
-    with pytest.raises(ValueError):
-        nlp.evaluate(docs_golds)
+    nlp.evaluate(docs_golds)
