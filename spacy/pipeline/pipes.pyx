cc97da74c0dbaea7984915de0b205c80f6cb3a4b
svlandeg <sofie.vanlandeghem@gmail.com>
2020-03-25 11:52:37 +0100

    add in link_vectors_to_models which got deleted

--- spacy/pipeline/pipes.pyx
+++ spacy/pipeline/pipes.pyx
@@ -1134,6 +1134,7 @@ class TextCategorizer(Pipe):
         docs = [Doc(Vocab(), words=["hello"])]
         truths, _ = self._examples_to_truth(examples)
         self.set_output(len(self.labels))
+        link_vectors_to_models(self.vocab)
         self.model.initialize(X=docs, Y=truths)
         if sgd is None:
             sgd = self.create_optimizer()
