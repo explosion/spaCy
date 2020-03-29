ce0e5380684fd593e2839ad1d954e1218224246c
adrianeboyd <adrianeboyd@gmail.com>
2020-03-29 13:57:00 +0200

    Check whether doc is instantiated in Example.get_gold_parses() (#5167)
    * Check whether doc is instantiated
    
    When creating docs to pair with gold parses, modify test to check
    whether a doc is unset rather than whether it contains tokens.
    
    * Restore test of evaluate on an empty doc
    
    * Set a minimal gold.orig for the scorer
    
    Without a minimal gold.orig the scorer can't evaluate empty docs. This
    is the v3 equivalent of #4925.

--- spacy/tests/regression/test_issue4924.py
+++ spacy/tests/regression/test_issue4924.py
@@ -5,5 +5,4 @@ from spacy.language import Language
 def test_issue4924():
     nlp = Language()
     docs_golds = [("", {})]
-    with pytest.raises(ValueError):
-        nlp.evaluate(docs_golds)
+    nlp.evaluate(docs_golds)
