67000068304b9a125ec792f32bed8491767dbed1
Leander Fiedler <lfiedler@users.noreply.github.com>
2020-04-12 09:34:54 +0200

    issue5230 attempted fix of pytest segfault for python3.5

--- spacy/kb.pyx
+++ spacy/kb.pyx
@@ -491,10 +491,10 @@ cdef class Writer:
 
 cdef class Reader:
     def __init__(self, object loc):
-        assert path.exists(loc)
-        assert not path.isdir(loc)
         if isinstance(loc, Path):
             loc = bytes(loc)
+        assert path.exists(loc)
+        assert not path.isdir(loc)
         cdef bytes bytes_loc = loc.encode('utf8') if type(loc) == unicode else loc
         self._fp = fopen(<char*>bytes_loc, 'rb')
         if not self._fp:
