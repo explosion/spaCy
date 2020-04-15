1ca32d8f9c800eb36e912dc1fa7b173edf7f2c3c
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-04-15 13:51:33 +0200

    Matcher support for Span as well as Doc (#5113)
    * Matcher support for Span, as well as Doc #5056
    
    * Removes an import unused
    
    * Signed contributors agreement
    
    * Code optimization and better test
    
    * Add error message for bad Matcher call argument
    
    * Fix merging

--- spacy/tests/matcher/test_matcher_api.py
+++ spacy/tests/matcher/test_matcher_api.py
@@ -6,7 +6,6 @@ import re
 from mock import Mock
 from spacy.matcher import Matcher, DependencyMatcher
 from spacy.tokens import Doc, Token
-
 from ..doc.test_underscore import clean_underscore  # noqa: F401
 
 
@@ -470,3 +469,13 @@ def test_matcher_callback(en_vocab):
     doc = Doc(en_vocab, words=["This", "is", "a", "test", "."])
     matches = matcher(doc)
     mock.assert_called_once_with(matcher, doc, 0, matches)
+
+
+def test_matcher_span(matcher):
+    text = "JavaScript is good but Java is better"
+    doc = Doc(matcher.vocab, words=text.split())
+    span_js = doc[:3]
+    span_java = doc[4:]
+    assert len(matcher(doc)) == 2
+    assert len(matcher(span_js)) == 1
+    assert len(matcher(span_java)) == 1
