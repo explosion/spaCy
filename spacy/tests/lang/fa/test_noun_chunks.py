84593a6b815a07edbf7085c1eebcc8b06fabac7e
adrianeboyd <adrianeboyd@gmail.com>
2020-05-14 12:07:00 +0200

    Use fa_tokenizer in test

--- spacy/tests/lang/fa/test_noun_chunks.py
+++ spacy/tests/lang/fa/test_noun_chunks.py
@@ -2,24 +2,16 @@
 from __future__ import unicode_literals
 
 import pytest
-from spacy.language import Language
-from spacy.vocab import Vocab
-from spacy.tokens import Doc
 
 
-@pytest.fixture
-def nlp():
-    return Language()
-
-
-def test_noun_chunks_is_parsed_fa():
+def test_noun_chunks_is_parsed_fa(fa_tokenizer):
     """Test that noun_chunks raises Value Error for 'fa' language if Doc is not parsed.
     To check this test, we're constructing a Doc
     with a new Vocab here and forcing is_parsed to 'False'
     to make sure the noun chunks don't run.
     """
 
-    doc = Doc(Vocab(), words=["چطور ", "هستید"])
+    doc = fa_tokenizer("این یک جمله نمونه می باشد.")
     doc.is_parsed = False
     with pytest.raises(ValueError):
         list(doc.noun_chunks)
