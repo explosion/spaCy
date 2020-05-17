90e73609813587fd80af8bd99bf3c7fa5473ed02
Matthw Honnibal <honnibal+gh@gmail.com>
2020-05-17 14:36:15 +0200

    Add extra dropout layer in tagger

--- spacy/ml/models/tagger.py
+++ spacy/ml/models/tagger.py
@@ -1,4 +1,5 @@
-from thinc.api import zero_init, with_array, Softmax, chain, Model
+from thinc.api import zero_init, with_array, Softmax, chain, Model, Dropout
+from thinc.api import glorot_uniform_init
 
 from ...util import registry
 
@@ -7,9 +8,9 @@ from ...util import registry
 def build_tagger_model(tok2vec, nO=None) -> Model:
     token_vector_width = tok2vec.get_dim("nO")
     # TODO: glorot_uniform_init seems to work a bit better than zero_init here?!
-    output_layer = Softmax(nO, nI=token_vector_width, init_W=zero_init)
+    output_layer = Softmax(nO, nI=token_vector_width, init_W=glorot_uniform_init)
     softmax = with_array(output_layer)
-    model = chain(tok2vec, softmax)
+    model = chain(tok2vec, Dropout(0.1), softmax)
     model.set_ref("tok2vec", tok2vec)
     model.set_ref("softmax", softmax)
     model.set_ref("output_layer", output_layer)
