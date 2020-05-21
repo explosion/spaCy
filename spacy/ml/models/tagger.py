d507ac28d8db197e8eac6b8c420ef3502af0a006
Matthw Honnibal <honnibal+gh@gmail.com>
2020-05-21 20:46:10 +0200

    Fix shape inference

--- spacy/ml/models/tagger.py
+++ spacy/ml/models/tagger.py
@@ -7,7 +7,8 @@ from ...util import registry
 @registry.architectures.register("spacy.Tagger.v1")
 def build_tagger_model(tok2vec, nO=None) -> Model:
     # TODO: glorot_uniform_init seems to work a bit better than zero_init here?!
-    output_layer = Softmax(nO, init_W=zero_init)
+    t2v_width = tok2vec.get_dim("nO") if tok2vec.has_dim("nO") else None
+    output_layer = Softmax(nO, t2v_width, init_W=zero_init)
     softmax = with_array(output_layer)
     model = chain(tok2vec, softmax)
     model.set_ref("tok2vec", tok2vec)
