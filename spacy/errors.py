ffe0451d0972ec209556dc7aad356deca1cbe0a7
svlandeg <sofie.vanlandeghem@gmail.com>
2020-06-03 14:45:00 +0200

    pretrain from config

--- spacy/errors.py
+++ spacy/errors.py
@@ -441,8 +441,6 @@ class Errors(object):
             "should be of equal length.")
     E141 = ("Entity vectors should be of length {required} instead of the "
             "provided {found}.")
-    E142 = ("Unsupported loss_function '{loss_func}'. Use either 'L2' or "
-            "'cosine'.")
     E143 = ("Labels for component '{name}' not initialized. Did you forget to "
             "call add_label()?")
     E144 = ("Could not find parameter `{param}` when building the entity "
