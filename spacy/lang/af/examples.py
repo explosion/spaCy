c162e8cb85784ac41c317f1ae23f8d8f1dee2e33
Andrew Joseph <andrew@aj-media.net>
2020-06-04 10:54:51 -0600

    added afrikaans language examples

---
+++ spacy/lang/af/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple wil die Britse onderneming vir $ 1 miljard koop",
+    "Outonome motors verskuif die versekeringsaanspreeklikheid teenoor vervaardigers",
+    "San Francisco oorweeg dit om afleweringsrobotte op sypaadjies te verbied",
+    "Londen is 'n groot stad in die Verenigde Koninkryk.",
+    "Waar is jy?",
+    "Wie is die president van Frankryk?",
+    "Wat is die hoofstad van die Verenigde State?",
+    "Wanneer is Barack Obama gebore?",
+]
