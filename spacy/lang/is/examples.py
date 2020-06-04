711c36ca3a425fc0de6cff4bbbad94e48df5100a
Andrew Joseph <andrew@aj-media.net>
2020-06-04 10:51:28 -0600

    added icelandic language examples

---
+++ spacy/lang/is/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple er að skoða að kaupa bandarískt gangsetning fyrir einn milljarð dala",
+    "Sjálfstæðir bílar færa ábyrgðartryggingu gagnvart framleiðendum",
+    "San Francisco íhugar að banna afhendingu vélmenni á gangstéttum",
+    "London er stór borg í Bretlandi.",
+    "Hvar ertu?",
+    "Hver er forseti Frakklands?",
+    "Hver er höfuðborg Bandaríkjanna?",
+    "Hvenær fæddist Barack Obama?",
+]
