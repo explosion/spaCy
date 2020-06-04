6bd2066042195a6c0a90b0a225968bbe3af67f0f
Andrew Joseph <andrew@aj-media.net>
2020-06-04 10:48:12 -0600

    added irish language examples

---
+++ spacy/lang/ga/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Tá Apple ag féachaint le tosaithe U.K. a cheannach ar $ 1 billiún",
+    "Athraíonn gluaisteáin uathrialacha dliteanas árachais i dtreo déantúsóirí",
+    "Measann San Francisco robots seachadta taobhlíne a thoirmeasc",
+    "Is cathair mhór í Londain sa Ríocht Aontaithe.",
+    "Cá bhfuil tú?",
+    "Cé hé uachtarán na Fraince?",
+    "Cad é príomhchathair na Stát Aontaithe?",
+    "Cathain a rugadh Barack Obama?",
+]
