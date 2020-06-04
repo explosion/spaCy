4fa1774c1ec544a5e03a7716dda6b91bc1853d75
Andrew Joseph <andrew@aj-media.net>
2020-06-04 11:01:40 -0600

    added slovenian language examples

---
+++ spacy/lang/sl/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple išče nakup ameriškega zagona za milijardo dolarjev",
+    "Avtonomni avtomobili zavarovalno odgovornost preusmerijo na proizvajalce",
+    "San Francisco razmišlja o prepovedi robov za dostavo pločnikov",
+    "London je veliko mesto v Veliki Britaniji.",
+    "Kje si?",
+    "Kdo je predsednik Francije?",
+    "Kakšno je glavno mesto ZDA?",
+    "Kdaj se je rodil Barack Obama?",
+]
