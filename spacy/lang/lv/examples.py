69833a8bd5b066f1cb4ae732e7402fcbde0d85ee
Andrew Joseph <andrew@aj-media.net>
2020-06-04 10:58:52 -0600

    added latvian language examples

---
+++ spacy/lang/lv/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple vēlas iegādāties Apvienotās Karalistes sākumcenu par USD 1 miljardu",
+    "Autonomās automašīnas nodod apdrošināšanas atbildību ražotājiem",
+    "Sanfrancisko apsver iespēju aizliegt ietvju piegādes robotus",
+    "Londona ir liela Apvienotās Karalistes pilsēta.",
+    "Kur tu esi?",
+    "Kas ir Francijas prezidents?",
+    "Kāda ir Amerikas Savienoto Valstu galvaspilsēta?",
+    "Kad dzimis Baraks Obama?",
+]
