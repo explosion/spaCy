9dbd9af38c15f1c53b2759d0e10024deb1a43c71
Andrew Joseph <andrew@aj-media.net>
2020-06-04 10:43:16 -0600

    added vietnamese and czech

---
+++ spacy/lang/cs/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple hledá nákup U.K. za 1 miliardu dolarů",
+    "Autonomní auta přesouvají pojištění na výrobce",
+    "San Francisco zvažuje zákaz robotů pro chodce",
+    "Londýn je velké město ve Velké Británii.",
+    "Kde jsi?",
+    "Kdo je prezidentem Francie?",
+    "Jaké je hlavní město Spojených států?",
+    "Kdy se narodil Barack Obama?",
+]
---
+++ spacy/lang/vi/examples.py
@@ -0,0 +1,22 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.en.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Apple đang xem xét việc mua công ty khởi nghiệp ở Vương quốc Anh với giá 1 tỷ đô la",
+    "Xe tự hành thay đổi trách nhiệm bảo hiểm đối với các nhà sản xuất",
+    "San Francisco xem xét việc cấm robot giao hàng vỉa hè",
+    "London là một thành phố lớn ở Vương quốc Anh.",
+    "Bạn ở đâu?",
+    "Tổng thống Pháp là ai?",
+    "Thủ đô của Hoa Kỳ là gì?",
+    "Barack Obama được sinh ra khi nào?",
+]
