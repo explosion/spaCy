c95494739c8b54b694a6523f4ea9064e6cc58a38
Matthew Honnibal <honnibal+gh@gmail.com>
2020-06-15 18:11:10 +0200

    Fix import

--- spacy/gold/__init__.py
+++ spacy/gold/__init__.py
@@ -1,7 +1,5 @@
 from .corpus import GoldCorpus
-from ..syntax.gold_parse import GoldParse
 from .example import Example
-from .annotation import TokenAnnotation, DocAnnotation
 from .align import align
 
 from .iob_utils import iob_to_biluo, biluo_to_iob
