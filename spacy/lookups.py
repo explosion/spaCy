d2c54987edc6fabcbf2113b2479ca578cf29ea71
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2020-06-18 18:07:33 -0700

    Reduce memory usage of Lookup's BloomFilter

--- spacy/lookups.py
+++ spacy/lookups.py
@@ -120,7 +120,7 @@ class Lookups(object):
         """
         self._tables = OrderedDict()
         for key, value in srsly.msgpack_loads(bytes_data).items():
-            self._tables[key] = Table(key)
+            self._tables[key] = Table(key, value)
             self._tables[key].update(value)
         return self
 
@@ -192,7 +192,7 @@ class Table(OrderedDict):
         self.name = name
         # Assume a default size of 1M items
         self.default_size = 1e6
-        size = len(data) if data and len(data) > 0 else self.default_size
+        size = max(len(data), 1) if data is not None else self.default_size
         self.bloom = BloomFilter.from_error_rate(size)
         if data:
             self.update(data)
