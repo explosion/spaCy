dc069e90b39ef3ca0604e950780a84482386973f
svlandeg <sofie.vanlandeghem@gmail.com>
2020-06-20 21:13:11 +0200

    fix token.morph_ for v.3 (cf PR #5517)

--- spacy/tokens/morphanalysis.pyx
+++ spacy/tokens/morphanalysis.pyx
@@ -73,3 +73,10 @@ cdef class MorphAnalysis:
         """Produce a dict representation.
         """
         return self.vocab.morphology.feats_to_dict(self.to_json())
+
+    def __str__(self):
+        return self.to_json()
+
+    def __repr__(self):
+        return self.to_json()
+
