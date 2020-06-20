95df02875827d9abd93664e164bdb4bed5468a73
Matthew Honnibal <honnibal+gh@gmail.com>
2020-06-20 03:50:23 +0200

    Update converters

--- spacy/gold/converters/__init__.py
+++ spacy/gold/converters/__init__.py
@@ -1,4 +1,6 @@
-from .conllu2docs import conllu2docs  # noqa: F401
 from .iob2docs import iob2docs # noqa: F401
 from .conll_ner2docs import conll_ner2docs  # noqa: F401
 from .jsonl2docs import ner_jsonl2docs  # noqa: F401
+
+# TODO: Update this one
+#from .conllu2docs import conllu2docs  # noqa: F401
