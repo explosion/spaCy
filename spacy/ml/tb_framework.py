6f7f2131cbde62bf1e3bf6ce4201dc2925fc002b
Matthew Honnibal <honnibal+gh@gmail.com>
2020-06-23 13:26:38 +0200

    Fix flake8

--- spacy/ml/tb_framework.py
+++ spacy/ml/tb_framework.py
@@ -38,8 +38,8 @@ def forward(model, X, is_train):
 
 
 def init(model, X=None, Y=None):
-    tok2vec = model.get_ref("tok2vec").initialize(X=X)
-    lower = model.get_ref("lower").initialize()
+    model.get_ref("tok2vec").initialize(X=X)
+    model.get_ref("lower").initialize()
     if model.attrs["has_upper"]:
         statevecs = model.ops.alloc2f(2, lower.get_dim("nO"))
         model.get_ref("upper").initialize(X=statevecs)
