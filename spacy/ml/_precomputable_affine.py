d1fd3438c31a3be94c111cdedd1a3c3a92c66b05
Matthw Honnibal <honnibal+gh@gmail.com>
2020-07-07 01:38:15 +0200

    Add dropout to parser hidden layer

--- spacy/ml/_precomputable_affine.py
+++ spacy/ml/_precomputable_affine.py
@@ -1,13 +1,14 @@
 from thinc.api import Model, normal_init
 
 
-def PrecomputableAffine(nO, nI, nF, nP):
+def PrecomputableAffine(nO, nI, nF, nP, dropout=0.1):
     model = Model(
         "precomputable_affine",
         forward,
         init=init,
         dims={"nO": nO, "nI": nI, "nF": nF, "nP": nP},
         params={"W": None, "b": None, "pad": None},
+        attrs={"dropout_rate": dropout}
     )
     return model
 
