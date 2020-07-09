dfb2c8054869b2203e565904ae44f6e38dbe5372
Adriane Boyd <adrianeboyd@gmail.com>
2020-07-09 15:25:35 +0200

    Fix Chinese serialization test to use char default

--- spacy/tests/lang/zh/test_serialize.py
+++ spacy/tests/lang/zh/test_serialize.py
@@ -3,16 +3,16 @@ from spacy.lang.zh import Chinese
 from ...util import make_tempdir
 
 
-def zh_tokenizer_serialize(zh_tokenizer_jieba):
-    tokenizer_bytes = zh_tokenizer_jieba.to_bytes()
-    nlp = Chinese(meta={"tokenizer": {"config": {"segmenter": "jieba"}}})
+def zh_tokenizer_serialize(zh_tokenizer):
+    tokenizer_bytes = zh_tokenizer.to_bytes()
+    nlp = Chinese()
     nlp.tokenizer.from_bytes(tokenizer_bytes)
     assert tokenizer_bytes == nlp.tokenizer.to_bytes()
 
     with make_tempdir() as d:
         file_path = d / "tokenizer"
-        zh_tokenizer_jieba.to_disk(file_path)
-        nlp = Chinese(meta={"tokenizer": {"config": {"segmenter": "jieba"}}})
+        zh_tokenizer.to_disk(file_path)
+        nlp = Chinese()
         nlp.tokenizer.from_disk(file_path)
         assert tokenizer_bytes == nlp.tokenizer.to_bytes()
 
