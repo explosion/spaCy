a013377d41237ee2eb39c970a13a7dfb740b0f22
Ines Montani <ines@ines.io>
2020-07-15 13:43:02 +0200

    Add test for config -> nlp with multiple instances

--- spacy/tests/serialize/test_serialize_config.py
+++ spacy/tests/serialize/test_serialize_config.py
@@ -97,6 +97,25 @@ def test_create_nlp_from_config():
         util.load_model_from_config(Config(bad_cfg))
 
 
+def test_create_nlp_from_config_multiple_instances():
+    """Test that the nlp object is created correctly for a config with multiple
+    instances of the same component."""
+    config = Config().from_str(nlp_config_string)
+    config["nlp"]["pipeline"] = {
+        "t2v": config["nlp"]["pipeline"]["tok2vec"],
+        "tagger1": config["nlp"]["pipeline"]["tagger"],
+        "tagger2": config["nlp"]["pipeline"]["tagger"],
+    }
+    nlp = util.load_model_from_config(config)
+    assert nlp.pipe_names == ["t2v", "tagger1", "tagger2"]
+    assert nlp.get_pipe_meta("t2v").factory == "tok2vec"
+    assert nlp.get_pipe_meta("tagger1").factory == "tagger"
+    assert nlp.get_pipe_meta("tagger2").factory == "tagger"
+    pipeline_config = nlp.config["nlp"]["pipeline"]
+    assert len(pipeline_config) == 3
+    assert list(pipeline_config.keys()) == ["t2v", "tagger1", "tagger2"]
+
+
 def test_serialize_nlp():
     """ Create a custom nlp pipeline from config and ensure it serializes it correctly """
     nlp_config = Config().from_str(nlp_config_string)
