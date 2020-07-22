bf24f7f67242d0b0786e29f81483824d0f6ecfc7
Adriane Boyd <adrianeboyd@gmail.com>
2020-07-22 16:02:51 +0200

    Update invalid tag maps (#5796)
    * Remove copy of (old?) PTB tag map for: bn, eu
    * Remove unsupported features from: hy, pl, ro, ru

--- spacy/lang/bn/__init__.py
+++ spacy/lang/bn/__init__.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .punctuation import TOKENIZER_PREFIXES, TOKENIZER_SUFFIXES, TOKENIZER_INFIXES
-from .tag_map import TAG_MAP
 from .stop_words import STOP_WORDS
 
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
@@ -16,7 +15,6 @@ class BengaliDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: "bn"
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
-    tag_map = TAG_MAP
     stop_words = STOP_WORDS
     prefixes = TOKENIZER_PREFIXES
     suffixes = TOKENIZER_SUFFIXES
