5cdf7e3c0367c95e58a6537852d5780f8bdb9730
svlandeg <sofie.vanlandeghem@gmail.com>
2020-07-30 00:07:22 +0200

    Revert "formatting"
    This reverts commit a85a46d546387a45758ef9959b24e104e0f2136e, reversing
    changes made to 12f43b7a16de924d9322a32d39756ef0ffaa6c24.

--- spacy/pipeline/textcat.py
+++ spacy/pipeline/textcat.py
@@ -238,6 +238,7 @@ class TextCategorizer(Pipe):
 
         DOCS: https://spacy.io/api/textcategorizer#rehearse
         """
+
         if losses is not None:
             losses.setdefault(self.name, 0.0)
         if self._rehearsal_model is None:
