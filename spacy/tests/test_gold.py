270f459d6f1008257e4f40e629ddf8c27f9543b0
svlandeg <sofie.vanlandeghem@gmail.com>
2020-08-03 19:25:58 +0200

    additional test using example.alignment

--- spacy/tests/test_gold.py
+++ spacy/tests/test_gold.py
@@ -687,6 +687,21 @@ def test_alignment_complex():
     assert list(align.y2x.dataXd) == [0, 0, 0, 1, 2, 3, 4, 5]
 
 
+def test_alignment_complex_example(en_vocab):
+    other_tokens = ["i listened to", "obama", "'", "s", "podcasts", "."]
+    spacy_tokens = ["i", "listened", "to", "obama", "'s", "podcasts."]
+    predicted = Doc(en_vocab, words=other_tokens, spaces=[True, False, False, True, False, False])
+    reference = Doc(en_vocab, words=spacy_tokens, spaces=[True, True, True, False, True, False])
+    assert predicted.text == "i listened to obama's podcasts."
+    assert reference.text == "i listened to obama's podcasts."
+    example = Example(predicted, reference)
+    align = example.alignment
+    assert list(align.x2y.lengths) == [3, 1, 1, 1, 1, 1]
+    assert list(align.x2y.dataXd) == [0, 1, 2, 3, 4, 4, 5, 5]
+    assert list(align.y2x.lengths) == [1, 1, 1, 1, 2, 2]
+    assert list(align.y2x.dataXd) == [0, 0, 0, 1, 2, 3, 4, 5]
+
+
 def test_alignment_different_texts():
     other_tokens = ["she", "listened", "to", "obama", "'s", "podcasts", "."]
     spacy_tokens = ["i", "listened", "to", "obama", "'s", "podcasts", "."]
