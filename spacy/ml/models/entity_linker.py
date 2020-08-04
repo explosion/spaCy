82347110f5ce5905b5acbde851c4b712b79130c1
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2020-08-04 14:34:09 +0200

    Default empty KB in EL component (#5872)
    * EL field documentation
    
    * documentation consistent with docs
    
    * default empty KB, initialize vocab separately
    
    * formatting
    
    * add test for changing the default entity vector length
    
    * update comment

--- spacy/ml/models/entity_linker.py
+++ spacy/ml/models/entity_linker.py
@@ -27,6 +27,13 @@ def build_nel_encoder(tok2vec: Model, nO: Optional[int] = None) -> Model:
 @registry.assets.register("spacy.KBFromFile.v1")
 def load_kb(vocab_path: str, kb_path: str) -> KnowledgeBase:
     vocab = Vocab().from_disk(vocab_path)
-    kb = KnowledgeBase(vocab=vocab)
+    kb = KnowledgeBase(entity_vector_length=1)
+    kb.initialize(vocab)
     kb.load_bulk(kb_path)
+    return kb
+
+
+@registry.assets.register("spacy.EmptyKB.v1")
+def empty_kb(entity_vector_length: int) -> KnowledgeBase:
+    kb = KnowledgeBase(entity_vector_length=entity_vector_length)
     return kb
