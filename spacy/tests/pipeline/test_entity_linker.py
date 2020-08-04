76cef6611d521ae90dd70c2bd56c79c8426f5f09
svlandeg <sofie.vanlandeghem@gmail.com>
2020-08-04 11:48:12 +0200

    add test for changing the default entity vector length

--- spacy/tests/pipeline/test_entity_linker.py
+++ spacy/tests/pipeline/test_entity_linker.py
@@ -117,6 +117,16 @@ def test_kb_default(nlp):
     assert len(entity_linker.kb) == 0
     assert entity_linker.kb.get_size_entities() == 0
     assert entity_linker.kb.get_size_aliases() == 0
+    assert entity_linker.kb.entity_vector_length == 64    # default value from pipeline.entity_linker
+
+
+def test_kb_custom_length(nlp):
+    """Test that the default (empty) KB is loaded when not providing a config"""
+    entity_linker = nlp.add_pipe("entity_linker", config={"kb": {"entity_vector_length": 35}})
+    assert len(entity_linker.kb) == 0
+    assert entity_linker.kb.get_size_entities() == 0
+    assert entity_linker.kb.get_size_aliases() == 0
+    assert entity_linker.kb.entity_vector_length == 35
 
 
 def test_kb_undefined(nlp):
