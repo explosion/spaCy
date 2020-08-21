74e5dbc3181419f41893f0902009afd65cf52b80
idoshr <idoshraga@gmail.com>
2020-08-22 01:58:18 +0300

    support human number 1K/1M....

--- spacy/lang/lex_attrs.py
+++ spacy/lang/lex_attrs.py
@@ -23,6 +23,7 @@ _tlds = set(
     "ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw".split("|")
 )
 
+_short_human_number = re.compile(r'([1-9]|[1-9][0-9]|[1-9][1-9][0-9])(K|M|G)$')
 
 def is_punct(text):
     for char in text:
@@ -45,7 +46,7 @@ def like_num(text):
     text = text.replace(",", "").replace(".", "")
     if text.isdigit():
         return True
-    if re.match(r'([1-9]|[1-9][0-9]|[1-9][1-9][0-9])(K|M|G)$', text):  # 1K - 999K / 1M - 999M / 1G - 999G
+    if _short_human_number.match(text):  # 1K - 999K / 1M - 999M / 1G - 999G
         return True
     if text.count("/") == 1:
         num, denom = text.split("/")
