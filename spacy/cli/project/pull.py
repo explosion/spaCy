3e4b86e54fbcbb25822a0935ef6501d75e98a2a6
Matthew Honnibal <honnibal+gh@gmail.com>
2020-08-22 18:30:40 +0200

    Improve printing

--- spacy/cli/project/pull.py
+++ spacy/cli/project/pull.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from wasabi import msg
 from .remote_storage import RemoteStorage
 from .remote_storage import get_command_hash
 from .._util import project_cli, Arg
@@ -17,10 +18,11 @@ def project_pull_cli(
     A storage can be anything that the smart-open library can upload to, e.g.
     gcs, aws, ssh, local directories etc
     """
-    project_pull(project_dir, remote)
+    for url, output_path in project_pull(project_dir, remote):
+        msg.good(f"Pulled {output_path} from {url}")
 
 
-def project_pull(project_dir: Path, remote: str):
+def project_pull(project_dir: Path, remote: str, *, verbose: bool=False):
     config = substitute_project_variables(load_project_config(project_dir))
     if remote in config.get("remotes", {}):
         remote = config["remotes"][remote]
@@ -31,4 +33,4 @@ def project_pull(project_dir: Path, remote: str):
         for output_path in cmd.get("outputs", []):
             url = storage.pull(output_path, command_hash=cmd_hash)
             if url is not None:
-                print(f"Pulled {output_path} from {url}")
+                yield url, output_path
