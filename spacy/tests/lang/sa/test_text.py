65c6d10b3bd71055a50f76b67f6ae0f88255ba1d
Shashank Shekhar <er.shashank10@gmail.com>
2020-08-24 23:26:35 +0530

    Added tests for lexical attribute like_num

--- spacy/tests/lang/sa/test_text.py
+++ spacy/tests/lang/sa/test_text.py
@@ -20,3 +20,26 @@ def test_sa_tokenizer_handles_long_text(sa_tokenizer):
 def test_sa_tokenizer_handles_cnts(sa_tokenizer, text, length):
     tokens = sa_tokenizer(text)
     assert len(tokens) == length
+
+
+@pytest.mark.parametrize(
+    "text,match",
+    [
+        ("10", True),
+        ("1", True),
+        ("10.000", True),
+        ("1000", True),
+        ("999,0", True),
+        ("एकः ", True),
+        ("दश", True),
+        ("पञ्चदश", True),
+        ("चत्वारिंशत् ", True),
+        ("कूपे", False),
+        (",", False),
+        ("1/2", True),
+    ],
+)
+def test_lex_attrs_like_number(sa_tokenizer, text, match):
+    tokens = sa_tokenizer(text)
+    assert len(tokens) == 1
+    assert tokens[0].like_num == match
