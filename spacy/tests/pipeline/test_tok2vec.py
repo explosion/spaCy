c45ddcc3e0d25992f31a52720d34420158194e8b
svlandeg <sofie.vanlandeghem@gmail.com>
2020-08-26 14:37:22 +0200

    unit test for current behaviour

--- spacy/tests/pipeline/test_tok2vec.py
+++ spacy/tests/pipeline/test_tok2vec.py
@@ -3,10 +3,17 @@ import pytest
 from spacy.ml.models.tok2vec import build_Tok2Vec_model
 from spacy.ml.models.tok2vec import MultiHashEmbed, CharacterEmbed
 from spacy.ml.models.tok2vec import MishWindowEncoder, MaxoutWindowEncoder
+from spacy.pipeline.tok2vec import Tok2Vec, Tok2VecListener
 from spacy.vocab import Vocab
 from spacy.tokens import Doc
+from spacy.gold import Example
+from spacy import util
 
-from .util import get_batch
+from ..util import get_batch
+
+from thinc.api import Config
+
+from numpy.testing import assert_equal
 
 
 def test_empty_doc():
@@ -41,7 +48,7 @@ def test_tok2vec_batch_sizes(batch_size, width, embed_size):
             also_use_static_vectors=False,
             also_embed_subwords=True,
         ),
-        MaxoutWindowEncoder(width=width, depth=4, window_size=1, maxout_pieces=3,),
+        MaxoutWindowEncoder(width=width, depth=4, window_size=1, maxout_pieces=3),
     )
     tok2vec.initialize()
     vectors, backprop = tok2vec.begin_update(batch)
@@ -74,3 +81,75 @@ def test_tok2vec_configs(width, embed_arch, embed_config, encode_arch, encode_co
     assert len(vectors) == len(docs)
     assert vectors[0].shape == (len(docs[0]), width)
     backprop(vectors)
+
+
+def test_tok2vec_listener():
+    cfg_string = """
+    [nlp]
+    lang = "en"
+    pipeline = ["tok2vec","tagger"]
+
+    [components]
+    
+    [components.tagger]
+    factory = "tagger"
+    
+    [components.tagger.model]
+    @architectures = "spacy.Tagger.v1"
+    nO = null
+
+    [components.tagger.model.tok2vec]
+    @architectures = "spacy.Tok2VecListener.v1"
+    width = ${components.tok2vec.model.encode.width}
+
+    [components.tok2vec]
+    factory = "tok2vec"
+
+    [components.tok2vec.model]
+    @architectures = "spacy.Tok2Vec.v1"
+
+    [components.tok2vec.model.embed]
+    @architectures = "spacy.MultiHashEmbed.v1"
+    width = ${components.tok2vec.model.encode.width}
+    rows = 2000
+    also_embed_subwords = true
+    also_use_static_vectors = false
+
+    [components.tok2vec.model.encode]
+    @architectures = "spacy.MaxoutWindowEncoder.v1"
+    width = 96
+    depth = 4
+    window_size = 1
+    maxout_pieces = 3
+    """
+
+    TRAIN_DATA = [
+        ("I like green eggs", {"tags": ["N", "V", "J", "N"]}),
+        ("Eat blue ham", {"tags": ["V", "J", "N"]}),
+    ]
+
+    orig_config = Config().from_str(cfg_string)
+    nlp, config = util.load_model_from_config(orig_config, auto_fill=True, validate=True)
+    tagger = nlp.get_pipe("tagger")
+    tok2vec = nlp.get_pipe("tok2vec")
+    tok2vec_listener = tagger.model.get_ref("tok2vec")
+    assert isinstance(tok2vec, Tok2Vec)
+    assert isinstance(tok2vec_listener, Tok2VecListener)
+    train_examples = []
+    for t in TRAIN_DATA:
+        train_examples.append(Example.from_dict(nlp.make_doc(t[0]), t[1]))
+        for tag in t[1]["tags"]:
+            tagger.add_label(tag)
+
+    # Check that the Tok2Vec component finds it listeners
+    assert tok2vec.listeners == []
+    optimizer = nlp.begin_training()
+    assert tok2vec.listeners == [tok2vec_listener]
+
+    for i in range(5):
+        losses = {}
+        nlp.update(train_examples, sgd=optimizer, losses=losses)
+
+    doc = nlp("Testing this document")
+    doc_tensor = tok2vec_listener.predict([doc])[0]
+    assert_equal(doc.tensor, doc_tensor)
