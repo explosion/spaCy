c7bd631b5f260d60dabe0ad65b609dcf8ddc298a
Adriane Boyd <adrianeboyd@gmail.com>
2020-09-13 14:05:36 +0200

    Fix token.idx for special cases with affixes (#6035)

--- spacy/tests/tokenizer/test_tokenizer.py
+++ spacy/tests/tokenizer/test_tokenizer.py
@@ -155,3 +155,11 @@ def test_tokenizer_special_cases_with_period(tokenizer):
     tokenizer.add_special_case("_SPECIAL_", [{"orth": "_SPECIAL_"}])
     doc = tokenizer(text)
     assert [token.text for token in doc] == ["_SPECIAL_", "."]
+
+
+def test_tokenizer_special_cases_idx(tokenizer):
+    text = "the _ID'X_"
+    tokenizer.add_special_case("_ID'X_", [{"orth": "_ID"}, {"orth": "'X_"}])
+    doc = tokenizer(text)
+    assert doc[1].idx == 4
+    assert doc[2].idx == 7
