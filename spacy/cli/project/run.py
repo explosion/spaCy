b2302c0a1ce7bacafdde22039cbd8da9782a3f27
Ines Montani <ines@ines.io>
2020-09-20 17:44:51 +0200

    Improve error for missing dependency

--- spacy/cli/project/run.py
+++ spacy/cli/project/run.py
@@ -59,8 +59,9 @@ def project_run(
         for dep in cmd.get("deps", []):
             if not (project_dir / dep).exists():
                 err = f"Missing dependency specified by command '{subcommand}': {dep}"
+                err_help = "Maybe you forgot to run the 'project assets' command?"
                 err_kwargs = {"exits": 1} if not dry else {}
-                msg.fail(err, **err_kwargs)
+                msg.fail(err, err_help, **err_kwargs)
         with working_dir(project_dir) as current_dir:
             rerun = check_rerun(current_dir, cmd)
             if not rerun and not force:
