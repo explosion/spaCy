2cad55169b217f557af7422cfbe7e83f3cfeee73
Duygu <duygu.altinok12@gmail.com>
2020-09-23 11:46:54 +0200

    feat: added popular postpositions

--- spacy/lang/tr/morph_rules.py
+++ spacy/lang/tr/morph_rules.py
@@ -3,25 +3,39 @@ from __future__ import unicode_literals
 
 from ...symbols import LEMMA, PRON_LEMMA
 
+_postpositions = [
+        "geçe",
+        "gibi",
+        "göre",
+        "ilişkin",
+        "kadar",
+        "kala",
+        "karşın",
+        "nazaran"
+        "rağmen",
+        "üzere"
+        ]
+
 _subordinating_conjunctions = [
         "eğer",
         "madem",
         "mademki",
-        "şayet",
+        "şayet"
         ]
 
 _coordinating_conjunctions = [
         "ama",
-        "hem"
+        "hem",
         "fakat",
         "ila",
         "lakin",
         "ve",
         "veya",
-        "veyahut",
+        "veyahut"
         ]
 
 MORPH_RULES = {
+        "ADP": {word: {"POS": "ADP"} for word in _postpositions},
         "SCONJ": {word: {"POS": "SCONJ"} for word in _subordinating_conjunctions},
         "CCONJ": {word: {"POS": "CCONJ"} for word in _coordinating_conjunctions},
         "PRON": {
