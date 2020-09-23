3370cd600c70d89f9f24de0b43726d127b9b2fba
svlandeg <sofie.vanlandeghem@gmail.com>
2020-09-24 01:03:09 +0200

    formatting

--- spacy/tests/pipeline/test_entity_linker.py
+++ spacy/tests/pipeline/test_entity_linker.py
@@ -161,6 +161,7 @@ def test_kb_serialize(nlp):
             # can not read from an unknown file
             mykb.from_disk(d / "unknown" / "kb")
 
+
 def test_candidate_generation(nlp):
     """Test correct candidate generation"""
     mykb = KnowledgeBase(nlp.vocab, entity_vector_length=1)
@@ -258,7 +259,9 @@ def test_vocab_serialization(nlp):
     q3_hash = mykb.add_entity(entity="Q3", freq=5, entity_vector=[3])
 
     # adding aliases
-    douglas_hash = mykb.add_alias(alias="douglas", entities=["Q2", "Q3"], probabilities=[0.4, 0.1])
+    douglas_hash = mykb.add_alias(
+        alias="douglas", entities=["Q2", "Q3"], probabilities=[0.4, 0.1]
+    )
     adam_hash = mykb.add_alias(alias="adam", entities=["Q2"], probabilities=[0.9])
 
     candidates = mykb.get_alias_candidates("adam")
@@ -499,19 +502,32 @@ def test_overfitting_IO():
                 predictions.append(ent.kb_id_)
         assert predictions == GOLD_entities
 
+
 def test_scorer_links():
     train_examples = []
     nlp = English()
     ref1 = nlp("Julia lives in London happily.")
-    ref1.ents = [Span(ref1, 0, 1, label="PERSON", kb_id="Q2"), Span(ref1, 3, 4, label="LOC", kb_id="Q3")]
+    ref1.ents = [
+        Span(ref1, 0, 1, label="PERSON", kb_id="Q2"),
+        Span(ref1, 3, 4, label="LOC", kb_id="Q3"),
+    ]
     pred1 = nlp("Julia lives in London happily.")
-    pred1.ents = [Span(pred1, 0, 1, label="PERSON", kb_id="Q70"), Span(pred1, 3, 4, label="LOC", kb_id="Q3")]
+    pred1.ents = [
+        Span(pred1, 0, 1, label="PERSON", kb_id="Q70"),
+        Span(pred1, 3, 4, label="LOC", kb_id="Q3"),
+    ]
     train_examples.append(Example(pred1, ref1))
 
     ref2 = nlp("She loves London.")
-    ref2.ents = [Span(ref2, 0, 1, label="PERSON", kb_id="Q2"), Span(ref2, 2, 3, label="LOC", kb_id="Q13")]
+    ref2.ents = [
+        Span(ref2, 0, 1, label="PERSON", kb_id="Q2"),
+        Span(ref2, 2, 3, label="LOC", kb_id="Q13"),
+    ]
     pred2 = nlp("She loves London.")
-    pred2.ents = [Span(pred2, 0, 1, label="PERSON", kb_id="Q2"), Span(pred2, 2, 3, label="LOC", kb_id="NIL")]
+    pred2.ents = [
+        Span(pred2, 0, 1, label="PERSON", kb_id="Q2"),
+        Span(pred2, 2, 3, label="LOC", kb_id="NIL"),
+    ]
     train_examples.append(Example(pred2, ref2))
 
     ref3 = nlp("London is great.")
@@ -521,10 +537,10 @@ def test_scorer_links():
     train_examples.append(Example(pred3, ref3))
 
     scores = Scorer().score_links(train_examples, negative_labels=["NIL"])
-    assert scores["nel_f_per_type"]["PERSON"]["p"] == 1/2
-    assert scores["nel_f_per_type"]["PERSON"]["r"] == 1/2
-    assert scores["nel_f_per_type"]["LOC"]["p"] == 1/1
-    assert scores["nel_f_per_type"]["LOC"]["r"] == 1/2
+    assert scores["nel_f_per_type"]["PERSON"]["p"] == 1 / 2
+    assert scores["nel_f_per_type"]["PERSON"]["r"] == 1 / 2
+    assert scores["nel_f_per_type"]["LOC"]["p"] == 1 / 1
+    assert scores["nel_f_per_type"]["LOC"]["r"] == 1 / 2
 
-    assert scores["nel_micro_p"] == 2/3
-    assert scores["nel_micro_r"] == 2/4
+    assert scores["nel_micro_p"] == 2 / 3
+    assert scores["nel_micro_r"] == 2 / 4
