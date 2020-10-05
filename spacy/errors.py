be99f1e4de604417bcee07602ae08178a23f6ede
Ines Montani <ines@ines.io>
2020-10-05 20:11:16 +0200

    Remove output dirs before training (#6204)
    * Remove output dirs before training
    
    * Re-raise error if cleaning fails

--- spacy/errors.py
+++ spacy/errors.py
@@ -456,6 +456,10 @@ class Errors:
             "issue tracker: http://github.com/explosion/spaCy/issues")
 
     # TODO: fix numbering after merging develop into master
+    E901 = ("Failed to remove existing output directory: {path}. If your "
+            "config and the components you train change between runs, a "
+            "non-empty output directory can lead to stale pipeline data. To "
+            "solve this, remove the existing directories in the output directory.")
     E902 = ("The sentence-per-line IOB/IOB2 file is not formatted correctly. "
             "Try checking whitespace and delimiters. See "
             "https://nightly.spacy.io/api/cli#convert")
