7ba2af22d7880a6aebe26649d88c1197a72476b2
Duygu <duygu.altinok12@gmail.com>
2020-10-05 15:32:26 +0200

    more tests with flat

--- spacy/tests/lang/tr/test_parser.py
+++ spacy/tests/lang/tr/test_parser.py
@@ -512,3 +512,31 @@ def test_tr_noun_chunks_flat_name_lastname_and_title(tr_tokenizer):
     chunks = list(doc.noun_chunks)
     assert len(chunks) == 1
     assert chunks[0].text_with_ws == "Cumhurbaşkanı Ahmet Necdet Sezer "
+
+
+def test_tr_noun_chunks_flat_in_nmod(tr_tokenizer):
+    text = "Ahmet Sezer adında bir ögrenci"
+    heads = [2, -1, 2, 1, 0]
+    deps = ["nmod", "flat", "nmod", "det", "ROOT"]
+    tags = ["PROPN", "PROPN", "NOUN", "DET", "NOUN"]
+    tokens = tr_tokenizer(text)
+    doc = get_doc(
+        tokens.vocab, words=[t.text for t in tokens], tags=tags, heads=heads, deps=deps
+    )
+    chunks = list(doc.noun_chunks)
+    assert len(chunks) == 1
+    assert chunks[0].text_with_ws == "Ahmet Sezer adında bir ögrenci "
+
+
+def test_tr_noun_chunks_flat_and_chain_nmod(tr_tokenizer):
+    text = "Batı Afrika ülkelerinden Sierra Leone"
+    heads = [1, 1, 1, 0, -1]
+    deps = ["nmod", "nmod", "nmod", "ROOT", "flat"]
+    tags = ["NOUN", "PROPN", "NOUN", "PROPN", "PROPN"]
+    tokens = tr_tokenizer(text)
+    doc = get_doc(
+        tokens.vocab, words=[t.text for t in tokens], tags=tags, heads=heads, deps=deps
+    )
+    chunks = list(doc.noun_chunks)
+    assert len(chunks) == 1
+    assert chunks[0].text_with_ws == "Batı Afrika ülkelerinden Sierra Leone "
