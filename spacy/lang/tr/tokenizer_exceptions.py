cadb4505548d31b84124785af1002483e699fe15
Duygu <duygu.altinok12@gmail.com>
2020-10-23 16:16:43 +0200

    removed url match import

--- spacy/lang/tr/tokenizer_exceptions.py
+++ spacy/lang/tr/tokenizer_exceptions.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 import re
 
 from ..punctuation import ALPHA_LOWER, ALPHA
-from ..tokenizer_exceptions import URL_PATTERN
 from ...symbols import ORTH, LEMMA, NORM
 
 _exc = {}
