5a2e3d4092f01626167dfeba4b0f0b5930d3b294
Matthew Honnibal <honnibal+gh@gmail.com>
2020-11-10 12:15:22 +0800

    Test beam density

--- spacy/tests/parser/test_nn_beam.py
+++ spacy/tests/parser/test_nn_beam.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 import pytest
+import hypothesis
+import hypothesis.strategies
 import numpy
 from spacy.vocab import Vocab
 from spacy.language import Language
@@ -11,26 +13,30 @@ from spacy.tokens import Doc
 from spacy.pipeline._parser_internals._beam_utils import ParserBeam
 from spacy.pipeline._parser_internals.stateclass import StateClass
 from spacy.training import Example
+from thinc.tests.strategies import ndarrays_of_shape
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def vocab():
     return Vocab()
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def moves(vocab):
     aeager = ArcEager(vocab.strings, {})
     aeager.add_action(0, "")
     aeager.add_action(1, "")
     aeager.add_action(2, "nsubj")
+    aeager.add_action(2, "punct")
+    aeager.add_action(2, "aux")
+    aeager.add_action(2, "nsubjpass")
     aeager.add_action(3, "dobj")
     aeager.add_action(2, "aux")
     aeager.add_action(4, "ROOT")
     return aeager
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def docs(vocab):
     return [
         Doc(
@@ -43,7 +49,7 @@ def docs(vocab):
     ]
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def examples(docs):
     return [Example(doc, doc.copy()) for doc in docs]
 
@@ -62,15 +68,18 @@ def tokvecs(docs, vector_size):
     return output
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def batch_size(docs):
     return len(docs)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def beam_width():
     return 4
 
+@pytest.fixture(params=[0.0, 0.5, 1.0])
+def beam_density(request):
+    return request.param
 
 @pytest.fixture
 def vector_size():
@@ -80,7 +89,6 @@ def vector_size():
 @pytest.fixture
 def beam(moves, examples, beam_width):
     states, golds, _ = moves.init_gold_batch(examples)
-    print("States", len(states), "Examples", len(examples))
     return ParserBeam(moves, states, golds, width=beam_width, density=0.0)
 
 
@@ -88,9 +96,9 @@ def beam(moves, examples, beam_width):
 def scores(moves, batch_size, beam_width):
     return [
         numpy.asarray(
-            numpy.random.uniform(-0.1, 0.1, (batch_size, moves.n_moves)), dtype="f"
+            numpy.random.uniform(-0.1, 0.1, (beam_width, moves.n_moves)), dtype="f"
         )
-        for _ in range(beam_width)
+        for _ in range(batch_size)
     ]
 
 
@@ -117,3 +125,22 @@ def test_beam_parse(examples, beam_width):
     parser.initialize(lambda: examples)
     doc = nlp.make_doc("Australia is a country")
     parser(doc)
+
+
+
+
+@hypothesis.given(hyp=hypothesis.strategies.data())
+def test_beam_density(moves, examples, beam_width, hyp):
+    beam_density = float(hyp.draw(hypothesis.strategies.floats(0.0, 1.0, width=32)))
+    states, golds, _ = moves.init_gold_batch(examples)
+    beam = ParserBeam(moves, states, golds, width=beam_width, density=beam_density)
+    scores = [
+        hyp.draw(ndarrays_of_shape((len(b), moves.n_moves)))
+        for b in beam
+    ]
+    beam.advance(scores)
+    print("Draw")
+    for b in beam:
+        beam_probs = b.probs
+        assert b.min_density == beam_density
+        assert beam_probs[-1] >= beam_probs[0] * beam_density
