c6790b961bd0591d6358382ee10951b8b067b091
Matthew Honnibal <honnibal+gh@gmail.com>
2020-11-30 14:51:11 +0800

    Add test for ae oracle when bad sentence starts

--- spacy/tests/parser/test_arc_eager_oracle.py
+++ spacy/tests/parser/test_arc_eager_oracle.py
@@ -65,7 +65,7 @@ def test_oracle_four_words(arc_eager, vocab):
     for i, state_costs in enumerate(cost_history):
         # Check gold moves is 0 cost
         golds = [act for act, cost in state_costs.items() if cost < 1]
-        assert golds == expected_gold[i]
+        assert golds == expected_gold[i], (i, golds, expected_gold[i])
 
 
 annot_tuples = [
@@ -241,3 +241,35 @@ def test_oracle_dev_sentence(vocab, arc_eager):
     ae_oracle_actions = arc_eager.get_oracle_sequence(example, _debug=False)
     ae_oracle_actions = [arc_eager.get_class_name(i) for i in ae_oracle_actions]
     assert ae_oracle_actions == expected_transitions
+
+
+def test_oracle_bad_tokenization(vocab, arc_eager):
+    words_deps_heads = """
+        [catalase] dep is
+        : punct is
+        that nsubj is
+        is root is
+        bad comp is
+    """
+ 
+    gold_words = []
+    gold_deps = []
+    gold_heads = []
+    for line in words_deps_heads.strip().split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        word, dep, head = line.split()
+        gold_words.append(word)
+        gold_deps.append(dep)
+        gold_heads.append(head)
+    gold_heads = [gold_words.index(head) for head in gold_heads]
+    for dep in gold_deps:
+        arc_eager.add_action(2, dep)  # Left
+        arc_eager.add_action(3, dep)  # Right
+    reference = Doc(Vocab(), words=gold_words, deps=gold_deps, heads=gold_heads)
+    predicted = Doc(reference.vocab, words=["[", "catalase", "]", ":", "that", "is", "bad"])
+    example = Example(predicted=predicted, reference=reference)
+    ae_oracle_actions = arc_eager.get_oracle_sequence(example, _debug=False)
+    ae_oracle_actions = [arc_eager.get_class_name(i) for i in ae_oracle_actions]
+    assert ae_oracle_actions
