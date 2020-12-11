1c2cf413e25b2265be4fd16cb84eccd457b5589d
Matthew Honnibal <honnibal+gh@gmail.com>
2020-12-11 16:41:22 +1100

    Upd test

--- spacy/tests/parser/test_state.py
+++ spacy/tests/parser/test_state.py
@@ -23,14 +23,14 @@ def test_push_pop(doc):
     state = StateClass(doc)
     state.push()
     assert state.buffer_length() == 3
-    assert state.stack == {0}
+    assert state.stack == [0]
     assert 0 not in state.queue
     state.push()
-    assert state.stack == {0, 1}
+    assert state.stack == [0, 1]
     assert 1 not in state.queue
     assert state.buffer_length() == 2
     state.pop()
-    assert state.stack == {0}
+    assert state.stack == [0]
     assert 1 not in state.queue
 
 def test_stack_depth(doc):
