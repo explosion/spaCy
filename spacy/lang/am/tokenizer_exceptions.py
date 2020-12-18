208ea11ab4ddd0cf61fa03802bffc16f37549981
tewodros <tedmaam2006@gmail.com>
2020-12-18 13:54:36 +0000

    Update tokenizer_exceptions.py

--- spacy/lang/am/tokenizer_exceptions.py
+++ spacy/lang/am/tokenizer_exceptions.py
@@ -8,7 +8,12 @@ _exc = {}
 
 
 for exc_data in [
-    {ORTH: "ት/ቤት", LEMMA: "ትምህርት ቤት"},    
+    {ORTH: "ት/ቤት", LEMMA: "ትምህርት ቤት"},
+    {ORTH: "ጽ/ቤት", LEMMA: "ጽህፈት ቤት", NORM: "ጽህፈት ቤት"},
+    {ORTH: "ፅ/ቤት", LEMMA: "ጽህፈት ቤት", NORM: "ጽህፈት ቤት"},
+    {ORTH: "መ/ቤት", LEMMA: "መስሪያ ቤት", NORM: "መስሪያ ቤት"},
+    {ORTH: "ም/ቤት", LEMMA: "ምክር ቤት", NORM: "ምክር ቤት"},
+    {ORTH: "ፍ/ቤት", LEMMA: "ፍርድ ቤት", NORM: "ፍርድ ቤት"},    
     {ORTH: "ወ/ሮ", LEMMA: PRON_LEMMA, NORM: "ወይዘሮ"},
 
 ]:
