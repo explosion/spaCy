fd87a9e7e92296a7be137b994510c758a35c0de3
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-10 01:47:49 +1100

    Add deserialization for SpanGroup

--- spacy/tokens/span_group.pyx
+++ spacy/tokens/span_group.pyx
@@ -1,5 +1,8 @@
 import weakref
+import struct
+import srsly
 from .span cimport Span
+from libc.stdint cimport uint64_t, uint32_t, int32_t
 
 
 cdef class SpanGroup:
@@ -45,22 +48,49 @@ cdef class SpanGroup:
     def to_list(self):
         return [self[i] for i in range(len(self))]
 
-    def to_json(self):
+    def to_bytes(self):
         output = {"name": self.name, "attrs": self.attrs, "spans": []}
         for i in range(self.c.size()):
             span = self.c[i]
-            output["spans"].append(
-                {
-                    "id": span.id,
-                    "kb_id": span.kb_id,
-                    "label": span.label,
-                    "start": span.start,
-                    "end": span.end,
-                    "start_char": span.start_char,
-                    "end_char": span.end_char,
-                }
-            )
-        return output
+            # The struct.pack here is probably overkill, but it might help if
+            # you're saving tonnes of spans, and it doesn't really add any
+            # complexity. We do take care to specify little-endian byte order
+            # though, to ensure the message can be loaded back on a different
+            # arch.
+            # Q: uint64_t
+            # q: int64_t
+            # L: uint32_t
+            # l: int32_t
+            output["spans"].append(struct.pack(
+                ">QQQllll",
+                span.id,
+                span.kb_id,
+                span.label,
+                span.start,
+                span.end,
+                span.start_char,
+                span.end_char
+            ))
+        return srsly.msgpack_dumps(output)
+
+    def from_bytes(self, byte_string):
+        msg = srsly.msgpack_loads(byte_string)
+        self.name = msg["name"]
+        self.attrs = dict(msg["attrs"])
+        self.c.clear()
+        self.c.reserve(len(msg["spans"]))
+        cdef SpanC span
+        for span_data in msg["spans"]:
+            items = struct.unpack(">QQQllll", span_data)
+            span.id = items[0]
+            span.kb_id = items[1]
+            span.label = items[2]
+            span.start = items[3]
+            span.end = items[4]
+            span.start_char = items[5]
+            span.end_char = items[6]
+            self.c.push_back(span)
+        return self
 
     cdef void push_back(self, SpanC span) nogil:
         self.c.push_back(span)
