bf0cdae8d41f2cc458739c2512c8bdb625757770
Adriane Boyd <adrianeboyd@gmail.com>
2021-01-17 12:54:41 +0100

    Add token_splitter component (#6726)
    * Add long_token_splitter component
    
    Add a `long_token_splitter` component for use with transformer
    pipelines. This component splits up long tokens like URLs into smaller
    tokens. This is particularly relevant for pretrained pipelines with
    `strided_spans`, since the user can't change the length of the span
    `window` and may not wish to preprocess the input texts.
    
    The `long_token_splitter` splits tokens that are at least
    `long_token_length` tokens long into smaller tokens of `split_length`
    size.
    
    Notes:
    
    * Since this is intended for use as the first component in a pipeline,
    the token splitter does not try to preserve any token annotation.
    * API docs to come when the API is stable.
    
    * Adjust API, add test
    
    * Fix name in factory

--- spacy/pipeline/functions.py
+++ spacy/pipeline/functions.py
@@ -1,7 +1,11 @@
+import srsly
+from thinc.api import Config
+from typing import Dict, Any
 from ..language import Language
 from ..matcher import Matcher
 from ..tokens import Doc
 from ..util import filter_spans
+from .. import util
 
 
 @Language.component(
@@ -65,3 +69,77 @@ def merge_subtokens(doc: Doc, label: str = "subtok") -> Doc:
         for span in spans:
             retokenizer.merge(span)
     return doc
+
+
+@Language.factory(
+    "token_splitter",
+    default_config={"min_length": 25, "split_length": 10},
+    retokenizes=True,
+)
+def make_token_splitter(
+    nlp: Language,
+    name: str,
+    *,
+    min_length=0,
+    split_length=0,
+):
+    return TokenSplitter(
+        min_length=min_length, split_length=split_length
+    )
+
+
+class TokenSplitter:
+    def __init__(self, min_length: int = 0, split_length: int = 0):
+        self.min_length = min_length
+        self.split_length = split_length
+
+    def __call__(self, doc: Doc) -> Doc:
+        if self.min_length > 0 and self.split_length > 0:
+            with doc.retokenize() as retokenizer:
+                for t in doc:
+                    if len(t.text) >= self.min_length:
+                        orths = []
+                        heads = []
+                        attrs = {}
+                        for i in range(0, len(t.text), self.split_length):
+                            orths.append(t.text[i : i + self.split_length])
+                            heads.append((t, i / self.split_length))
+                        retokenizer.split(t, orths, heads, attrs)
+        return doc
+
+    def _get_config(self) -> Dict[str, Any]:
+        return {
+            "min_length": self.min_length,
+            "split_length": self.split_length,
+        }
+
+    def _set_config(self, config: Dict[str, Any] = {}) -> None:
+        self.min_length = config.get("min_length", 0)
+        self.split_length = config.get("split_length", 0)
+
+    def to_bytes(self, **kwargs):
+        serializers = {
+            "cfg": lambda: srsly.json_dumps(self._get_config()),
+        }
+        return util.to_bytes(serializers, [])
+
+    def from_bytes(self, data, **kwargs):
+        deserializers = {
+            "cfg": lambda b: self._set_config(srsly.json_loads(b)),
+        }
+        util.from_bytes(data, deserializers, [])
+        return self
+
+    def to_disk(self, path, **kwargs):
+        path = util.ensure_path(path)
+        serializers = {
+            "cfg": lambda p: srsly.write_json(p, self._get_config()),
+        }
+        return util.to_disk(path, serializers, [])
+
+    def from_disk(self, path, **kwargs):
+        path = util.ensure_path(path)
+        serializers = {
+            "cfg": lambda p: self._set_config(srsly.read_json(p)),
+        }
+        util.from_disk(path, serializers, [])
