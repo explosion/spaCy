d8bb964dd345806f6c894b6178062bc850970fbf
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-17 18:10:46 +1100

    Upd spancat model

--- spacy/ml/models/spancat.py
+++ spacy/ml/models/spancat.py
@@ -1,19 +1,26 @@
 from typing import List, Tuple
-from thinc.api import Model, with_getitem, chain, list2ragged
+from thinc.api import Model, with_getitem, chain, list2ragged, Logistic
+from thinc.api import Maxout
 from thinc.types import Ragged, Floats2d
+from ...util import registry
 from ...tokens import Doc
 from ..extract_spans import extract_spans
 
 
+@registry.architectures.register("spacy.MaxoutLogistic.v1")
+def build_MaxoutLogistic(nI=None, nO=None, pieces=3):
+    return chain(Maxout(nI=nI, nO=nO, pieces=pieces), Logistic())
+
+
+@registry.architectures.register("spacy.SpanCategorizer.v1")
 def build_spancat_model(
     tok2vec: Model[List[Doc], List[Floats2d]],
     reducer: Model[Ragged, Floats2d],
     scorer: Model[Floats2d, Floats2d]
 ) -> Model[Tuple[List[Doc], Ragged], Floats2d]:
-    """Constrct a span categorizer model out of three submodels: a tok2vec model,
-    to map each token in the batch to a vector, a reducer, to map each subsequence
-    to a single vector, and a scorer, to map the vectors to category probabilities.
-    The submodels are all set as named references.
+    """Build a span categorizer model, given a token-to-vector model, a 
+    reducer model to map the sequence of vectors for each span down to a single
+    vector, and a scorer model to map the vectors to probabilities.
 
     tok2vec (Model[List[Doc], List[Floats2d]]): The tok2vec model.
     reducer (Model[Ragged, Floats2d]): The reducer model.
@@ -23,7 +30,7 @@ def build_spancat_model(
         with_getitem(0, chain(tok2vec, list2ragged())),
         extract_spans(),
         reducer,
-        scorer
+        scorer,
     )
     model.set_ref("tok2vec", tok2vec)
     model.set_ref("reducer", reducer)
