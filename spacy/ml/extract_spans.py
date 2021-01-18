366a16395dfd07f382199e4b2e8e9cd5e74d470d
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-18 23:06:58 +1100

    Fix comment

--- spacy/ml/extract_spans.py
+++ spacy/ml/extract_spans.py
@@ -40,7 +40,7 @@ def forward(
 def _get_span_indices(ops, spans: Ragged, lengths: Ints1d) -> Ints1d:
     """Construct a flat array that has the indices we want to extract from the
     source data. For instance, if we want the spans (5, 9), (8, 10) the
-    indices will be [5, 6, 7, 8, 8, 9, 10].
+    indices will be [5, 6, 7, 8, 8, 9].
     """
     indices = []
     offset = 0
