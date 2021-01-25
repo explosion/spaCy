42b117e561fca0f156d3ea2aacdff7757dd0b150
Matthew Honnibal <honnibal+gh@gmail.com>
2021-01-26 00:40:18 +1100

    Fix Doc.copy bugs (#6809)
    * Dont let the Doc own LexemeC, to fix Doc.copy
    
    * Copy doc.spans
    
    * Copy doc.spans

--- spacy/tokens/_dict_proxies.py
+++ spacy/tokens/_dict_proxies.py
@@ -33,6 +33,9 @@ class SpanGroups(UserDict):
     def _make_span_group(self, name: str, spans: Iterable["Span"]) -> SpanGroup:
         return SpanGroup(self.doc_ref(), name=name, spans=spans)
 
+    def copy(self) -> "SpanGroups":
+        return SpanGroups(self.doc_ref()).from_bytes(self.to_bytes())
+
     def to_bytes(self) -> bytes:
         # We don't need to serialize this as a dict, because the groups
         # know their names.
