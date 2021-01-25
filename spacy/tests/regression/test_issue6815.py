f2bdbfd4d837039f8b921c0fe5f30d3f146051b7
Dhruv <dhruvrnaik@gmail.com>
2021-01-26 02:22:53 +0530

    add span.vector test for #6815

--- spacy/tests/regression/test_issue6815.py
+++ spacy/tests/regression/test_issue6815.py
@@ -24,3 +24,14 @@ def test_char_span_kb_id(sentence, start_idx, end_idx, kb_id):
     doc = nlp(sentence)
     span = doc[:].char_span(start_idx, end_idx, kb_id=kb_id)
     assert span.kb_id == kb_id
+
+@pytest.mark.parametrize(
+    "sentence, start_idx,end_idx,vector", [
+        ('Welcome to Mumbai, my friend',11,17,np.array([0.1,0.2,0.3]))
+    ]
+)
+def test_char_span_vector(sentence, start_idx, end_idx, vector):
+    nlp = English()
+    doc = nlp(sentence)
+    span = doc[:].char_span(start_idx, end_idx, vector=vector)
+    assert (span.vector == vector).all()
