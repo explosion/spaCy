f321d9864b221c886bc09e729d058e983e705d00
Adriane Boyd <adrianeboyd@gmail.com>
2021-01-25 14:22:17 +0100

    Improve verb+pron handling
    * Move verb+pron handling into its own method
    * Check for exceptions (primarily for se)
    * Collect pronouns in the same (not reversed) order

--- spacy/lang/es/lemmatizer.py
+++ spacy/lang/es/lemmatizer.py
@@ -327,30 +327,9 @@ class SpanishLemmatizer(Lemmatizer):
 
         RETURNS (List[str]): The list of lemmas.
         """
-        # Exceptions for pronouns
+        # Exceptions for verb+pronoun(s)
         if "PronType=Prs" in features:
-            # Strip and count pronouns
-            pron_patt = "^(.*?)([mts]e|l[aeo]s?|n?os)$"
-            prons = []
-            verb = word
-            m = re.search(pron_patt, verb)
-            while m is not None and len(prons) <= 3:
-                verb = re.sub(m.group(2) + "$", "", verb)
-                prons.append(m.group(2))
-                m = re.search(pron_patt, verb)
-            # Strip accents from verb form
-            for old, new in self.lookups.get_table("lemma_rules").get("accents", []):
-                verb = re.sub(old, new, verb)
-            # Lemmatize the verb and pronouns
-            rule = self.select_rule("verb", features)
-            verb_lemma = self.lemmatize_verb(
-                verb, features - {"PronType=Prs"}, rule, index
-            )[0]
-            pron_lemmas = []
-            for pron in prons:
-                rule = self.select_rule("pron", features)
-                pron_lemmas.append(self.lemmatize_pron(pron, features, rule, index)[0])
-            return [verb_lemma + " " + " ".join(pron_lemmas)]
+            return self.lemmatize_verb_pron(word, features, rule, index)
 
         # Initialize empty sets for the generated lemmas
         possible_lemmas = []
@@ -401,3 +380,37 @@ class SpanishLemmatizer(Lemmatizer):
             return possible_lemmas
         else:
             return [word]
+
+    def lemmatize_verb_pron(
+        self, word: str, features: List[str], rule: str, index: List[str]
+    ) -> List[str]:
+        # Strip and collect pronouns
+        pron_patt = "^(.*?)([mts]e|l[aeo]s?|n?os)$"
+        prons = []
+        verb = word
+        m = re.search(pron_patt, verb)
+        while m is not None and len(prons) <= 3:
+            verb = re.sub(m.group(2) + "$", "", verb)
+            prons = [m.group(2)] + prons
+            m = re.search(pron_patt, verb)
+        # Strip accents from verb form
+        for old, new in self.lookups.get_table("lemma_rules").get("accents", []):
+            verb = re.sub(old, new, verb)
+        # Lemmatize the verb and pronouns, checking for exceptions
+        exc = self.lookups.get_table("lemma_exc").get("verb", {}).get(verb)
+        if exc is not None:
+            verb_lemma = exc[0]
+        else:
+            rule = self.select_rule("verb", features)
+            verb_lemma = self.lemmatize_verb(
+                verb, features - {"PronType=Prs"}, rule, index
+            )[0]
+        pron_lemmas = []
+        for pron in prons:
+            exc = self.lookups.get_table("lemma_exc").get("pron", {}).get(pron)
+            if exc is not None:
+                pron_lemmas.append(exc[0])
+            else:
+                rule = self.select_rule("pron", features)
+                pron_lemmas.append(self.lemmatize_pron(pron, features, rule, index)[0])
+        return [verb_lemma + " " + " ".join(pron_lemmas)]
