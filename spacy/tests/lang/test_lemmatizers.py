d17afb4826229d8c1ec3c2191e8f8adfe53a6b06
Adriane Boyd <adrianeboyd@gmail.com>
2021-01-27 12:21:35 +0100

    Add Spanish rule-based lemmatizer (#6833)
    * Initial Spanish lemmatizer
    
    * Handle merged verb+pron(s) multi-word tokens
    
    * Use VERB for AUX rule lookup
    
    * Add morph to lemma cache key
    
    * Fix aux lookups, minor refactoring
    
    * Improve verb+pron handling
    
    * Move verb+pron handling into its own method
    * Check for exceptions (primarily for se)
    * Collect pronouns in the same (not reversed) order
    
    * Only add modified possible lemmas

--- spacy/tests/lang/test_lemmatizers.py
+++ spacy/tests/lang/test_lemmatizers.py
@@ -7,7 +7,7 @@ from spacy.util import get_lang_class
 # fmt: off
 # Only include languages with no external dependencies
 # excluded: ru, uk
-# excluded for custom tables: pl
+# excluded for custom tables: es, pl
 LANGUAGES = ["bn", "el", "en", "fa", "fr", "nb", "nl", "sv"]
 # fmt: on
 
