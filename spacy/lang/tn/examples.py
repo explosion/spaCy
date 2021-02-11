37ec67f868ec803423cd76af28f8116c326ebedd
Shumi <76557637+Shumie82@users.noreply.github.com>
2021-02-11 21:25:58 +0200

    Update examples.py
    I have removed two lines:
    # coding: utf8
    from __future__ import unicode_literals
    
    And updated: >>> from spacy.lang.tn.examples import sentences

--- spacy/lang/tn/examples.py
+++ spacy/lang/tn/examples.py
@@ -1,10 +1,6 @@
-# coding: utf8
-from __future__ import unicode_literals
-
-
 """
 Example sentences to test spaCy and its language models.
->>> from spacy.lang.en.examples import sentences
+>>> from spacy.lang.tn.examples import sentences
 >>> docs = nlp.pipe(sentences)
 """
 
