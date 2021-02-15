a3293efc4826c2528e7a740273d0bf424ba874ea
Adriane Boyd <adrianeboyd@gmail.com>
2021-02-15 14:09:33 +0100

    Add time and level to default logging formatter

--- spacy/util.py
+++ spacy/util.py
@@ -70,7 +70,7 @@ CONFIG_SECTION_ORDER = ["paths", "variables", "system", "nlp", "components", "co
 
 logger = logging.getLogger("spacy")
 logger_stream_handler = logging.StreamHandler()
-logger_stream_handler.setFormatter(logging.Formatter("%(message)s"))
+logger_stream_handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s"))
 logger.addHandler(logger_stream_handler)
 
 
