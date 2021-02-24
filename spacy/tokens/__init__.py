0563cd73d6b9dc9a5cc639f9a33e45691229d82a
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2021-02-24 11:06:16 +0100

    Fix SpanGroup import (#7182)
    * import SpanGroup from tokens module
    
    * revert edits from different PR
    
    * add to __all__

--- spacy/tokens/__init__.py
+++ spacy/tokens/__init__.py
@@ -1,7 +1,8 @@
 from .doc import Doc
 from .token import Token
 from .span import Span
+from .span_group import SpanGroup
 from ._serialize import DocBin
 from .morphanalysis import MorphAnalysis
 
-__all__ = ["Doc", "Token", "Span", "DocBin", "MorphAnalysis"]
+__all__ = ["Doc", "Token", "Span", "SpanGroup", "DocBin", "MorphAnalysis"]
