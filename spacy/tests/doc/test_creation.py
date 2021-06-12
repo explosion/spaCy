6d535eabd0059f17767640a7936f89909df5dfc7
Paul O'Leary McCann <polm@dampfkraft.com>
2021-06-12 19:21:39 +0900

    Add a test

--- spacy/tests/doc/test_creation.py
+++ spacy/tests/doc/test_creation.py
@@ -63,3 +63,9 @@ def test_create_from_words_and_text(vocab):
         words = [" ", " ", "'", "dogs", "'", "\n\n", "run"]
         text = "  'dogs'\n\nrun  "
         (words, spaces) = util.get_words_and_spaces(words + ["away"], text)
+
+def test_create_with_heads_and_no_deps(vocab):
+    words = "I like ginger".split()
+    heads = list(range(len(words)))
+    with pytest.raises(ValueError):
+        doc = Doc(vocab, words=words, heads=heads)
