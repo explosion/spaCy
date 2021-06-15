fbe7d92b143c64fef1e51634cbfabbc7dab5a1ec
svlandeg <sofie.vanlandeghem@gmail.com>
2021-06-15 18:33:09 +0200

    use our beloved tupley format with recent support for doc.spans

--- spacy/tests/pipeline/test_spancat.py
+++ spacy/tests/pipeline/test_spancat.py
@@ -3,17 +3,21 @@ from spacy.training import Example
 from spacy.util import fix_random_seed
 
 
+SPAN_KEY = "labeled_spans"
+
 TRAIN_DATA = [
-    ("Who is Shaka Khan?", {"entities": [(7, 17, "PERSON")]}),
-    ("I like London and Berlin.", {"entities": [(7, 13, "LOC"), (18, 24, "LOC")]}),
+    ("Who is Shaka Khan?", {"spans": {SPAN_KEY: [(7, 17, "PERSON")]}}),
+    (
+        "I like London and Berlin.",
+        {"spans": {SPAN_KEY: [(7, 13, "LOC"), (18, 24, "LOC")]}},
+    ),
 ]
 
 
-def make_get_examples(nlp, spans_key="spans"):
+def make_get_examples(nlp):
     train_examples = []
     for t in TRAIN_DATA:
         eg = Example.from_dict(nlp.make_doc(t[0]), t[1])
-        eg.reference.spans[spans_key] = list(eg.reference.ents)
         train_examples.append(eg)
 
     def get_examples():
@@ -25,8 +29,8 @@ def make_get_examples(nlp, spans_key="spans"):
 def test_simple_train():
     fix_random_seed(0)
     nlp = Language()
-    spancat = nlp.add_pipe("spancat")
-    get_examples = make_get_examples(nlp, spans_key=spancat.key)
+    spancat = nlp.add_pipe("spancat", config={"spans_key": SPAN_KEY})
+    get_examples = make_get_examples(nlp)
     nlp.initialize(get_examples)
     sgd = nlp.create_optimizer()
     assert len(spancat.labels) != 0
@@ -34,5 +38,6 @@ def test_simple_train():
         losses = {}
         nlp.update(list(get_examples()), losses=losses, drop=0.1, sgd=sgd)
     doc = nlp("I like London and Berlin.")
+    assert doc.spans[spancat.key] == doc.spans[SPAN_KEY]
     assert len(doc.spans[spancat.key]) == 2
     assert doc.spans[spancat.key][0].text == "London"
