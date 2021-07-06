5fd0b5207e34e849ca257c92966e2c7d72431149
Adriane Boyd <adrianeboyd@gmail.com>
2021-07-06 12:43:17 +0200

    Fix vectors check for sourced components (#8559)
    * Fix vectors check for sourced components
    
    Since vectors are not loaded when components are sourced, store a hash
    for the vectors of each sourced component and compare it to the loaded
    vectors after the vectors are loaded from the `[initialize]` block.
    
    * Pop temporary info
    
    * Remove stored hash in remove_pipe
    
    * Add default for pop
    
    * Add additional convert/debug/assemble CLI tests

--- spacy/language.py
+++ spacy/language.py
@@ -934,6 +934,7 @@ class Language:
         # because factory may be used for something else
         self._pipe_meta.pop(name)
         self._pipe_configs.pop(name)
+        self.meta.get("_sourced_vectors_hashes", {}).pop(name, None)
         # Make sure name is removed from the [initialize] config
         if name in self._config["initialize"]["components"]:
             self._config["initialize"]["components"].pop(name)
@@ -1680,6 +1681,8 @@ class Language:
         # If components are loaded from a source (existing models), we cache
         # them here so they're only loaded once
         source_nlps = {}
+        source_nlp_vectors_hashes = {}
+        nlp.meta["_sourced_vectors_hashes"] = {}
         for pipe_name in config["nlp"]["pipeline"]:
             if pipe_name not in pipeline:
                 opts = ", ".join(pipeline.keys())
@@ -1719,7 +1722,12 @@ class Language:
                                     name, source_name, pipe_cfg["replace_listeners"]
                                 )
                                 listeners_replaced = True
-                    nlp.add_pipe(source_name, source=source_nlps[model], name=pipe_name)
+                    with warnings.catch_warnings():
+                        warnings.filterwarnings("ignore", message="\\[W113\\]")
+                        nlp.add_pipe(source_name, source=source_nlps[model], name=pipe_name)
+                    if model not in source_nlp_vectors_hashes:
+                        source_nlp_vectors_hashes[model] = hash(source_nlps[model].vocab.vectors.to_bytes())
+                    nlp.meta["_sourced_vectors_hashes"][pipe_name] = source_nlp_vectors_hashes[model]
                     # Delete from cache if listeners were replaced
                     if listeners_replaced:
                         del source_nlps[model]
