62f1e77a09f3525d601ca262482163e5660ac00c
svlandeg <sofie.vanlandeghem@gmail.com>
2021-08-16 19:25:54 +0200

    fix _make_span_group

--- spacy/tests/pipeline/test_spancat.py
+++ spacy/tests/pipeline/test_spancat.py
@@ -71,6 +71,57 @@ def test_explicit_labels():
     assert spancat.labels == ("PERSON", "LOC")
 
 
+@pytest.mark.parametrize(
+    "max_positive,nr_results", [(None, 4), (1, 2), (2, 3), (3, 4), (4, 4)]
+)
+def test_make_spangroup(max_positive, nr_results):
+    import numpy
+
+    fix_random_seed(0)
+    nlp = Language()
+    spancat = nlp.add_pipe(
+        "spancat",
+        config={"spans_key": SPAN_KEY, "threshold": 0.5, "max_positive": max_positive},
+    )
+    doc = nlp.make_doc("Greater London")
+    ngram_suggester = registry.misc.get("spacy.ngram_suggester.v1")(sizes=[1, 2])
+    indices = ngram_suggester([doc])[0].dataXd
+    numpy.testing.assert_array_equal(indices, numpy.asarray([[0, 1], [1, 2], [0, 2]]))
+    labels = ["Thing", "City", "Person", "GreatCity"]
+    scores = numpy.asarray(
+        [[0.2, 0.4, 0.3, 0.1], [0.1, 0.6, 0.2, 0.4], [0.8, 0.7, 0.3, 0.9]], dtype="f"
+    )
+    spangroup = spancat._make_span_group(doc, indices, scores, labels)
+    assert len(spangroup) == nr_results
+
+    # first span is always the second token "London"
+    assert spangroup[0].text == "London"
+    assert spangroup[0].label_ == "City"
+    numpy.testing.assert_almost_equal(0.6, spangroup.attrs["scores"][0], 5)
+
+    # second span depends on the number of positives that were allowed
+    assert spangroup[1].text == "Greater London"
+    if max_positive == 1:
+        assert spangroup[1].label_ == "GreatCity"
+        numpy.testing.assert_almost_equal(0.9, spangroup.attrs["scores"][1], 5)
+    else:
+        assert spangroup[1].label_ == "Thing"
+        numpy.testing.assert_almost_equal(0.8, spangroup.attrs["scores"][1], 5)
+
+    if nr_results > 2:
+        assert spangroup[2].text == "Greater London"
+        if max_positive == 2:
+            assert spangroup[2].label_ == "GreatCity"
+            numpy.testing.assert_almost_equal(0.9, spangroup.attrs["scores"][2], 5)
+        else:
+            assert spangroup[2].label_ == "City"
+            numpy.testing.assert_almost_equal(0.7, spangroup.attrs["scores"][2], 5)
+
+    assert spangroup[-1].text == "Greater London"
+    assert spangroup[-1].label_ == "GreatCity"
+    numpy.testing.assert_almost_equal(0.9, spangroup.attrs["scores"][-1], 5)
+
+
 def test_simple_train():
     fix_random_seed(0)
     nlp = Language()
@@ -90,6 +141,9 @@ def test_simple_train():
     scores = nlp.evaluate(get_examples())
     assert f"spans_{SPAN_KEY}_f" in scores
     assert scores[f"spans_{SPAN_KEY}_f"] == 1.0
+    # also test that the spancat works for just a single entity in a sentence
+    doc = nlp("London")
+    assert len(doc.spans[spancat.key]) == 1
 
 
 def test_ngram_suggester(en_tokenizer):
