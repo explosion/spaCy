94f5f90a23d261d1ebd090fb5e9bd6ab37897f15
cayorodriguez <crodriguezp@gmail.com>
2021-08-30 09:06:11 +0200

    changed failed test in test/lang/ca
    changed failed test in test/lang/ca

--- spacy/tests/lang/ca/test_exception.py
+++ spacy/tests/lang/ca/test_exception.py
@@ -9,13 +9,7 @@ def test_ca_tokenizer_handles_abbr(ca_tokenizer, text, lemma):
     tokens = ca_tokenizer(text)
     assert len(tokens) == 1
 
-
-#def test_ca_tokenizer_handles_exc_in_text(ca_tokenizer):
-#    text = "La Núria i el Pere han vingut aprox. a les 7 de la tarda."
-#    tokens = ca_tokenizer(text)
-#    assert len(tokens) == 15
-#    assert tokens[7].text == "aprox."
-    
+   
 def test_ca_tokenizer_handles_exc_in_text(ca_tokenizer):
     text = "La Dra. Puig viu a la pl. dels Til·lers."
     tokens = ca_tokenizer(text) # [La, Dra., Puig, viu, a, la, pl., d, els, Til·lers]
