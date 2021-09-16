00836c2d7d46b0aa405eb1569856f7923da6d1f0
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2021-09-16 09:23:21 +0200

    Update spacy/displacy/templates.py

--- spacy/displacy/templates.py
+++ spacy/displacy/templates.py
@@ -51,8 +51,9 @@ TPL_ENTS = """
 TPL_ENT = """
 <mark class="entity" style="background: {bg}; padding: 0.45em 0.6em; margin: 0 0.25em; line-height: 1; border-radius: 0.35em;">
     {text}
-    <span style="font-size: 0.8em; font-weight: bold; line-height: 1; border-radius: 0.35em; vertical-align: middle; margin-left: 0.5rem">{label}</span>
+    <span style="font-size: 0.8em; font-weight: bold; line-height: 1; border-radius: 0.35em; vertical-align: middle; margin-left: 0.5rem">{label}
     <a style="text-decoration: none; color: black; font-weight: bold" href="{kb_url}">{kb_id}</a>
+    </span>
 </mark>
 """
 
