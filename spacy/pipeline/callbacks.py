071d930bf222a9768a903ff0bbacb5411c68222d
Daniël de Kok <me@danieldk.eu>
2021-09-21 12:22:15 +0200

    NVTX range: wrap models of all trainable pipes jointly
    This avoids that (sub-)models that are shared between pipes get wrapped
    twice.

--- spacy/pipeline/callbacks.py
+++ spacy/pipeline/callbacks.py
@@ -1,7 +1,7 @@
 from typing import Type, Callable
 
 from thinc.layers import with_nvtx_range
-from thinc.model import wrap_model_recursive
+from thinc.model import Model, wrap_model_recursive
 
 from ..language import Language
 from ..pipeline import TrainablePipe
@@ -11,9 +11,22 @@ from ..util import registry
 @registry.callbacks("spacy.models_with_nvtx_range.v1")
 def create_models_with_nvtx_range() -> Callable[[Language], Language]:
     def models_with_nvtx_range(nlp):
-        for _, component in nlp.components:
-            if isinstance(component, TrainablePipe):
-                component.model = wrap_model_recursive(component.model, with_nvtx_range)
+        pipes = [pipe for _, pipe in nlp.components if isinstance(pipe, TrainablePipe)]
+
+        # We need to wrap all models jointly to avoid double-wrapping.
+        models = Model(
+            "wrap_with_nvtx_range",
+            forward=lambda model, X, is_train: ...,
+            layers=[pipe.model for pipe in pipes],
+        )
+        models = wrap_model_recursive(models, with_nvtx_range)
+
+        wrapped_models = models.layers[0].layers
+
+        assert len(wrapped_models) == len(pipes)
+
+        for (model, pipe) in zip(wrapped_models, pipes):
+            pipe.model = model
 
         return nlp
 
