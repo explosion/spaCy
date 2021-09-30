8fe525beb5b9d0948e52de0f0aeb87df01287e65
Jim O’Regan <jaoregan@tcd.ie>
2021-09-30 13:18:47 +0100

    Add an Irish lemmatiser, based on BuNaMo (#9102)
    * add tréis/théis
    
    * remove previous contents, add demutate/unponc
    
    * fmt off/on wrapping
    
    * type hints
    
    * IrishLemmatizer (sic)
    
    * Use spacy-lookups-data>=1.0.3
    
    * Minor bug fixes, refactoring for IrishLemmatizer
    
    * Fix return type for ADP list lookups
    * Fix and refactor lookup table lookups for missing/string/list
    * Remove unused variables
    
    * skip lookup of verbal substantives and adjectives; just demutate
    
    * Fix morph checks API details
    
    * Add types and format
    
    * Move helper methods into lemmatizer
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- spacy/lang/ga/__init__.py
+++ spacy/lang/ga/__init__.py
@@ -1,6 +1,11 @@
+from typing import Optional
+
+from thinc.api import Model
+
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
 from ...language import Language
+from .lemmatizer import IrishLemmatizer
 
 
 class IrishDefaults(Language.Defaults):
@@ -11,6 +16,18 @@ class IrishDefaults(Language.Defaults):
 class Irish(Language):
     lang = "ga"
     Defaults = IrishDefaults
+
+
+@Irish.factory(
+    "lemmatizer",
+    assigns=["token.lemma"],
+    default_config={"model": None, "mode": "pos_lookup", "overwrite": False},
+    default_score_weights={"lemma_acc": 1.0},
+)
+def make_lemmatizer(
+    nlp: Language, model: Optional[Model], name: str, mode: str, overwrite: bool
+):
+    return IrishLemmatizer(nlp.vocab, model, name, mode=mode, overwrite=overwrite)
 
 
 __all__ = ["Irish"]
