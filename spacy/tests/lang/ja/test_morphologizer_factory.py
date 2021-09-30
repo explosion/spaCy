fc2d9ce37bb281fa8e876a94cdca7257601d52cf
Adriane Boyd <adrianeboyd@gmail.com>
2021-09-30 16:11:24 +0200

    Skip test if sudachipy is not installed

--- spacy/tests/lang/ja/test_morphologizer_factory.py
+++ spacy/tests/lang/ja/test_morphologizer_factory.py
@@ -2,6 +2,7 @@ from spacy.lang.ja import Japanese
 
 
 def test_ja_morphologizer_factory():
+    pytest.importorskip("sudachipy")
     nlp = Japanese()
     morphologizer = nlp.add_pipe("morphologizer")
     assert morphologizer.cfg["extend"] is True
