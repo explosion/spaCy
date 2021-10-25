e593e08b5ca499cf8bde950029c51416f3fa54db
Duygu <duygu@explosion.ai>
2021-10-25 17:50:14 +0200

    updated syntax iters

--- spacy/lang/es/syntax_iterators.py
+++ spacy/lang/es/syntax_iterators.py
@@ -1,58 +1,42 @@
 from typing import Union, Iterator, Tuple
 
-from ...symbols import NOUN, PROPN, PRON, VERB, AUX
+from ...symbols import NOUN, PROPN, PRON
 from ...errors import Errors
-from ...tokens import Doc, Span, Token
+from ...tokens import Doc, Span
 
 
 def noun_chunks(doclike: Union[Doc, Span]) -> Iterator[Tuple[int, int, int]]:
-    """Detect base noun phrases from a dependency parse. Works on Doc and Span."""
-    doc = doclike.doc
+    """
+    Detect base noun phrases from a dependency parse. Works on both Doc and Span.
+    """
+    labels = [
+        "nsubj",
+        "nsubjpass",
+        "obj",
+        "pcomp",
+        "appos",
+        "ROOT",
+    ]
+    doc = doclike.doc  # Ensure works on both Doc and Span.
     if not doc.has_annotation("DEP"):
         raise ValueError(Errors.E029)
-    if not len(doc):
-        return
+    np_deps = [doc.vocab.strings.add(label) for label in labels]
     np_label = doc.vocab.strings.add("NP")
-    left_labels = ["det", "fixed", "neg"]  # ['nunmod', 'det', 'appos', 'fixed']
-    right_labels = ["flat", "fixed", "compound", "neg"]
-    stop_labels = ["punct"]
-    np_left_deps = [doc.vocab.strings.add(label) for label in left_labels]
-    np_right_deps = [doc.vocab.strings.add(label) for label in right_labels]
-    stop_deps = [doc.vocab.strings.add(label) for label in stop_labels]
-
-    prev_right = -1
-    for token in doclike:
-        if token.pos in [PROPN, NOUN, PRON]:
-            left, right = noun_bounds(
-                doc, token, np_left_deps, np_right_deps, stop_deps
-            )
-            if left.i <= prev_right:
-                continue
-            yield left.i, right.i + 1, np_label
-            prev_right = right.i
+    prev_end = -1
+    for i, word in enumerate(doclike):
+        if word.pos not in (NOUN, PROPN, PRON):
+            continue
+        # Prevent nested chunks from being produced
+        if word.left_edge.i <= prev_end:
+            continue
+        if word.dep in np_deps:
+            right_edge = word.right_edge
+            right_end = right_edge if right_edge.dep_ == "amod" else word
+            prev_end = right_end.i
+            yield word.left_edge.i, right_end.i + 1, np_label
 
 
-def is_verb_token(token: Token) -> bool:
-    return token.pos in [VERB, AUX]
-
-
-def noun_bounds(doc, root, np_left_deps, np_right_deps, stop_deps):
-    left_bound = root
-    for token in reversed(list(root.lefts)):
-        if token.dep in np_left_deps:
-            left_bound = token
-    right_bound = root
-    for token in root.rights:
-        if token.dep in np_right_deps:
-            left, right = noun_bounds(
-                doc, token, np_left_deps, np_right_deps, stop_deps
-            )
-            filter_func = lambda t: is_verb_token(t) or t.dep in stop_deps
-            if list(filter(filter_func, doc[left_bound.i : right.i])):
-                break
-            else:
-                right_bound = right
-    return left_bound, right_bound
 
 
 SYNTAX_ITERATORS = {"noun_chunks": noun_chunks}
+
