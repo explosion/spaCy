abcbd41337152cc9c328600d3fab63c277be0977
Duygu <duygu@explosion.ai>
2021-10-26 11:01:16 +0200

    added basic tests

--- spacy/tests/lang/es/test_noun_chunks.py
+++ spacy/tests/lang/es/test_noun_chunks.py
@@ -1,4 +1,23 @@
+from spacy.tokens import Doc
 import pytest
+
+@pytest.fixture
+def doc_basic(es_vocab):
+    words = ["un", "gato"]
+    heads = [1, 1]
+    deps = ["det", "ROOT"]
+    pos = ["DET", "NOUN"]
+    return Doc(en_vocab, words=words, heads=heads, deps=deps, pos=pos)
+
+@pytest.fixture
+def doc_basic_adj(es_vocab):
+    words = ["la", "camisa", "negra"]
+    heads = [1, 1, 1]
+    deps = ["det", "ROOT", "amod"]
+    pos = ["DET", "NOUN", "ADJ"]
+    return Doc(en_vocab, words=words, heads=heads, deps=deps, pos=pos)
+
+Tengo un gato y un perro.
 
 
 def test_noun_chunks_is_parsed_es(es_tokenizer):
@@ -6,3 +25,25 @@ def test_noun_chunks_is_parsed_es(es_tokenizer):
     doc = es_tokenizer("en Oxford este verano")
     with pytest.raises(ValueError):
         list(doc.noun_chunks)
+
+
+def test_noun_chunks_basic(doc_basic, es_tokenizer):
+    """Test that noun_chunks for basic np determiner+noun"""
+    doc_chunks = list(doc.noun_chunks)
+    assert len(doc_chunks) == 1
+    chunk = doc_chunks[0]
+    assert chunk in doc_chunks
+    assert chunk.start == 0
+    assert chunk.end ==2
+    assert chunk.text == "un gato"
+
+
+def test_noun_chunks_basic_adj(doc_basic_adj, es_tokenizer):
+    """Test that noun_chunks for basic np determiner+noun+adj"""
+    doc_chunks = list(doc.noun_chunks)
+    assert len(doc_chunks) == 1
+    chunk = doc_chunks[0]
+    assert chunk in doc_chunks
+    assert chunk.start == 0
+    assert chunk.end ==3
+    assert chunk.text == "la camisa negra"
