4d5db737e9fb815a24b2d071345392185e44abf2
Adriane Boyd <adrianeboyd@gmail.com>
2021-11-02 14:24:06 +0100

    Revert "Temporarily skip compat tests (#9594)"
    This reverts commit 667572adca3a95cbcfd92cea43746b2625a40831.

--- spacy/tests/test_cli.py
+++ spacy/tests/test_cli.py
@@ -492,7 +492,6 @@ def test_string_to_list_intify(value):
     assert string_to_list(value, intify=True) == [1, 2, 3]
 
 
-@pytest.mark.skip(reason="Temporarily skip until v3.2.0 release")
 def test_download_compatibility():
     spec = SpecifierSet("==" + about.__version__)
     spec.prereleases = False
@@ -503,7 +502,6 @@ def test_download_compatibility():
         assert get_minor_version(about.__version__) == get_minor_version(version)
 
 
-@pytest.mark.skip(reason="Temporarily skip until v3.2.0 release")
 def test_validate_compatibility_table():
     spec = SpecifierSet("==" + about.__version__)
     spec.prereleases = False
