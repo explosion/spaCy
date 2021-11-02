1a83752f3d2c3fb3c5ee192becb39cd709c8f73f
Adriane Boyd <adrianeboyd@gmail.com>
2021-11-02 09:03:45 +0100

    Update accuracy labels and exclude morph_acc for ja

--- website/src/templates/models.js
+++ website/src/templates/models.js
@@ -41,14 +41,16 @@ const MODEL_META = {
     web: 'written text (blogs, news, comments)',
     news: 'written text (news, media)',
     wiki: 'Wikipedia',
-    uas: 'Unlabelled dependencies',
-    las: 'Labelled dependencies',
-    dep_uas: 'Unlabelled dependencies',
-    dep_las: 'Labelled dependencies',
+    uas: 'Unlabeled dependencies',
+    las: 'Labeled dependencies',
+    dep_uas: 'Unlabeled dependencies',
+    dep_las: 'Labeled dependencies',
     token_acc: 'Tokenization',
     tok: 'Tokenization',
     lemma: 'Lemmatization',
     morph: 'Morphological analysis',
+    lemma_acc: 'Lemmatization',
+    morph_acc: 'Morphological analysis',
     tags_acc: 'Part-of-speech tags (fine grained tags, Token.tag)',
     tag_acc: 'Part-of-speech tags (fine grained tags, Token.tag)',
     tag: 'Part-of-speech tags (fine grained tags, Token.tag)',
@@ -115,8 +117,8 @@ function formatVectors(data) {
     return `${abbrNum(keys)} keys, ${abbrNum(vectors)} unique vectors (${width} dimensions)`
 }
 
-function formatAccuracy(data) {
-    const exclude = ['speed']
+function formatAccuracy(data, lang) {
+    const exclude = (lang !== "ja") ? ['speed'] : ['speed', 'morph_acc']
     if (!data) return []
     return Object.keys(data)
         .map(label => {
@@ -147,8 +149,7 @@ function formatModelMeta(data) {
         license: data.license,
         labels: isEmptyObj(data.labels) ? null : data.labels,
         vectors: formatVectors(data.vectors),
-        // TODO: remove accuracy fallback
-        accuracy: formatAccuracy(data.accuracy || data.performance),
+        accuracy: formatAccuracy(data.performance, data.lang),
     }
 }
 
