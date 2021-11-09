5632e186ac40328e0ae75f67c5f47393513f3d42
Duygu <duygu@explosion.ai>
2021-11-09 17:31:11 +0100

    set ents of moc docs

--- spacy/tests/matcher/test_matcher_api.py
+++ spacy/tests/matcher/test_matcher_api.py
@@ -649,9 +649,17 @@ def test_matcher_ent_iob_key(en_vocab):
     matcher = Matcher(en_vocab)
     matcher.add("Rule", [[{"ENT_IOB": "I"}]])
     doc1 = Doc(en_vocab, words=["I", "visited", "New", "York", "and", "California"])
+    doc1.ents = [Span(doc1, 2, 4, label="GPE"), Span(doc1, 5, 6, label="GPE")]
     doc2 = Doc(en_vocab, words=["I", "visited", "my", "friend", "Alicia"])
+    doc2.ents = [Span(doc2, 4, 5, label="PERSON")]
     matches1 = [doc1[start:end].text for _, start, end in matcher(doc1)]
     matches2 = [doc2[start:end].text for _, start, end in matcher(doc2)]
     assert len(matches1) == 1
     assert matches1[0] == "York"
     assert len(matches2) == 0
+
+    matcher = Matcher(en_vocab)
+    matcher.add("Rule", [[{"ENT_IOB": "I", "OP": "+"}]])
+    doc = Doc(en_vocab, words=["I", "visited", "my", "friend", "Anna", "Maria", "Esperanza"])
+    doc.ents = [Span(doc, 4, 7, label="PERSON")]
+    matches = [doc[start:end].text for _, start, end in matcher(doc)]
