23435670500f2dc38bf94a4ab0ea1f16b404a1aa
Narayan Acharya <narayan.acharya6@gmail.com>
2021-11-26 14:36:30 +0530

    Add support for kb_id to be displayed via displacy.serve. The current support is only limited to the manual option in displacy.render

--- spacy/displacy/__init__.py
+++ spacy/displacy/__init__.py
@@ -184,8 +184,12 @@ def parse_ents(doc: Doc, options: Dict[str, Any] = {}) -> Dict[str, Any]:
     doc (Doc): Document do parse.
     RETURNS (dict): Generated entities keyed by text (original text) and ents.
     """
+    kb_url_format_template = options.get("kb_url_format_template", None)
     ents = [
-        {"start": ent.start_char, "end": ent.end_char, "label": ent.label_}
+        {"start": ent.start_char, "end": ent.end_char, "label": ent.label_,
+         "kb_id": ent.kb_id_ if ent.kb_id_ else "",
+         "kb_url": kb_url_format_template.format(ent.kb_id_) if kb_url_format_template else "#"
+        }
         for ent in doc.ents
     ]
     if not ents:
