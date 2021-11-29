f7a12c37708194bbcd25dc8d8ee09b7757364a43
Lj Miranda <ljvmiranda@gmail.com>
2021-11-29 08:45:49 +0800

    Remove lemmatizer tests in el lang

--- spacy/tests/lang/el/test_text.py
+++ spacy/tests/lang/el/test_text.py
@@ -1,15 +1,4 @@
 import pytest
-from spacy.lang.el import Greek
-
-
-@pytest.mark.skip(reason="lemmatizer lookups no longer in vocab")
-@pytest.mark.issue(4272)
-def test_issue4272():
-    """Test that lookup table can be accessed from Token.lemma if no POS tags
-    are available."""
-    nlp = Greek()
-    doc = nlp("Χθες")
-    assert doc[0].lemma_
 
 
 def test_el_tokenizer_handles_long_text(el_tokenizer):
