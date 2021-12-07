77308395b969636ac41ddb8904e399f89402d71d
Duygu <duygu@explosion.ai>
2021-12-07 11:50:16 +0100

    added error

--- spacy/errors.py
+++ spacy/errors.py
@@ -889,6 +889,7 @@ class Errors(metaclass=ErrorsWithCodes):
              "Non-UD tags should use the `tag` property.")
     E1022 = ("Words must be of type str or int, but input is of type '{wtype}'")
     E1023 = ("Couldn't read EntityRuler from the {path}. This file doesn't exist.")
+    E1025 = ("Cannot intify the value '{value}' as an IOB string. Only possible values are I, O, B and the empty string.")
 
 
 # Deprecated model shortcuts, only used in errors and warnings
