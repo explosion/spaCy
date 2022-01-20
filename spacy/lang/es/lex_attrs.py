832c2424eeb62b62298b7201bfc8fd5374fa9b0d
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2022-01-20 11:25:11 +0100

    black formatting

--- spacy/lang/es/lex_attrs.py
+++ spacy/lang/es/lex_attrs.py
@@ -48,11 +48,37 @@ _num_words = [
 
 
 _ordinal_words = [
-    "primero", "segundo", "tercero", "cuarto", "quinto", "sexto", "séptimo", "octavo",
-    "noveno", "décimo", "undécimo", "duodécimo", "decimotercero", "decimocuarto",
-    "decimoquinto", "decimosexto", "decimoséptimo", "decimoctavo", "decimonoveno",
-    "vigésimo", "trigésimo", "cuadragésimo", "quincuagésimo", "sexagésimo", "septuagésimo",
-    "octogésima", "nonagésima", "centésima", "milésima", "millonésima", "billonésima",
+    "primero",
+    "segundo",
+    "tercero",
+    "cuarto",
+    "quinto",
+    "sexto",
+    "séptimo",
+    "octavo",
+    "noveno",
+    "décimo",
+    "undécimo",
+    "duodécimo",
+    "decimotercero",
+    "decimocuarto",
+    "decimoquinto",
+    "decimosexto",
+    "decimoséptimo",
+    "decimoctavo",
+    "decimonoveno",
+    "vigésimo",
+    "trigésimo",
+    "cuadragésimo",
+    "quincuagésimo",
+    "sexagésimo",
+    "septuagésimo",
+    "octogésima",
+    "nonagésima",
+    "centésima",
+    "milésima",
+    "millonésima",
+    "billonésima",
 ]
 
 
@@ -73,7 +99,6 @@ def like_num(text):
     if text_lower in _ordinal_words:
         return True
     return False
-
 
 
 LEX_ATTRS = {LIKE_NUM: like_num}
