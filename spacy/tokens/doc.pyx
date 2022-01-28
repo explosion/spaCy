e52ffa761d72ecd79899ba697cdd6bb07a534c1f
svlandeg <svlandeg@github.com>
2022-01-28 16:51:38 +0100

    elif instead of if

--- spacy/tokens/doc.pyx
+++ spacy/tokens/doc.pyx
@@ -420,7 +420,7 @@ cdef class Doc:
         cdef int range_start = 0
         if attr == "IS_SENT_START" or attr == self.vocab.strings["IS_SENT_START"]:
             attr = SENT_START
-        if attr == "IS_SENT_END" or attr == self.vocab.strings["IS_SENT_END"]:
+        elif attr == "IS_SENT_END" or attr == self.vocab.strings["IS_SENT_END"]:
             attr = SENT_START
         attr = intify_attr(attr)
         # adjust attributes
