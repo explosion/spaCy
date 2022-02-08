19358dd120b8e86ebbef8352961a209f6892377a
Paul O'Leary McCann <polm@dampfkraft.com>
2022-02-08 16:47:05 +0900

    Add back lang/is to avoid breaking current workaround

---
+++ spacy/lang/is/__init__.py
@@ -0,0 +1,5 @@
+from ..isl import *
+import warnings
+warnings.warn("spacy.lang.is is deprecated, use lang.isl instead", DeprecationWarning)
+
+__all__ = ["Icelandic"]
---
+++ spacy/lang/is/stop_words.py
@@ -0,0 +1,4 @@
+from ..isl.stop_words import *
+import warnings
+
+warnings.warn("spacy.lang.is is deprecated, use lang.isl instead", DeprecationWarning)
