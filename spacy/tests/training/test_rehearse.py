be8c64590385c4275c5d59d7852136ca87f80825
Kádár Ákos <akos@onyx.uvt.nl>
2022-02-23 11:36:25 +0100

    test for multilabel textcat

--- spacy/tests/training/test_rehearse.py
+++ spacy/tests/training/test_rehearse.py
@@ -12,7 +12,9 @@ TRAIN_DATA = [
             'entities': [(7, 18, 'PERSON')],
             'tags': ['PRON', 'AUX', 'PROPN', 'PRON', 'PUNCT'],
             'heads': [1, 1, 3, 1, 1],
-            'deps': ['attr', 'ROOT', 'compound', 'nsubj', 'punct']
+            'deps': ['attr', 'ROOT', 'compound', 'nsubj', 'punct'],
+            'morphs': ['', 'Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin', 'Number=Sing', 'Number=Sing', 'PunctType=Peri'],
+            'cats': {'question': 1.0}
         }
     ),
     (
@@ -21,7 +23,9 @@ TRAIN_DATA = [
             'entities': [(7, 17, 'PERSON')],
             'tags': ['PRON', 'AUX', 'PROPN', 'PRON', 'PUNCT'],
             'heads': [1, 1, 3, 1, 1],
-            'deps': ['attr', 'ROOT', 'compound', 'nsubj', 'punct']
+            'deps': ['attr', 'ROOT', 'compound', 'nsubj', 'punct'],
+            'morphs': ['', 'Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin', 'Number=Sing', 'Number=Sing', 'PunctType=Peri'],
+            'cats': {'question': 1.0}
         }
     ),
     (
@@ -30,8 +34,10 @@ TRAIN_DATA = [
             'entities': [(0, 3, 'PERSON')],
             'tags': ['PROPN', 'AUX', 'DET', 'ADJ', 'NOUN', 'PUNCT'],
             'heads': [1, 1, 4, 4, 1, 1],
-            'deps': ['nsubj', 'ROOT', 'det', 'amod', 'attr', 'punct']
-        }
+            'deps': ['nsubj', 'ROOT', 'det', 'amod', 'attr', 'punct'],
+            'morphs': ['Number=Sing', 'Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin', 'Definite=Ind|PronType=Art', 'Degree=Pos', 'Number=Sing', 'PunctType=Peri'],
+            'cats': {'statement': 1.0}
+        },
     ),
     (
         'Hi Anil, how are you?',
@@ -39,7 +45,9 @@ TRAIN_DATA = [
             'entities': [(3, 7, 'PERSON')],
             'tags': ['INTJ', 'PROPN', 'PUNCT', 'ADV', 'AUX', 'PRON', 'PUNCT'],
             'deps': ['intj', 'npadvmod', 'punct', 'advmod', 'ROOT', 'nsubj', 'punct'],
-            'heads': [4, 0, 4, 4, 4, 4, 4]
+            'heads': [4, 0, 4, 4, 4, 4, 4],
+            'morphs': ['', 'Number=Sing', 'PunctType=Comm', '', 'Mood=Ind|Tense=Pres|VerbForm=Fin', 'Case=Nom|Person=2|PronType=Prs', 'PunctType=Peri'],
+            'cats': {'greeting': 1.0, 'question': 1.0}
         }
     ),
     (
@@ -48,7 +56,9 @@ TRAIN_DATA = [
             'entities': [(7, 13, 'LOC'), (18, 24, 'LOC')],
             'tags': ['PROPN', 'VERB', 'PROPN', 'CCONJ', 'PROPN', 'PUNCT'],
             'deps': ['nsubj', 'ROOT', 'dobj', 'cc', 'conj', 'punct'],
-            'heads': [1, 1, 1, 2, 2, 1]
+            'heads': [1, 1, 1, 2, 2, 1],
+            'morphs': ['Case=Nom|Number=Sing|Person=1|PronType=Prs', 'Tense=Pres|VerbForm=Fin', 'Number=Sing', 'ConjType=Cmp', 'Number=Sing', 'PunctType=Peri'],
+            'cats': {'statement': 1.0}
         }
     )
 ]
@@ -60,7 +70,9 @@ REHEARSE_DATA = [
             'entities': [(3, 7, 'PERSON')],
             'tags': ['INTJ', 'PROPN'],
             'deps': ['ROOT', 'npadvmod'],
-            'heads': [0, 0]
+            'heads': [0, 0],
+            'morphs': ['', 'Number=Sing'],
+            'cats': {'greeting': 1.0}
         }
     ),
     (
@@ -69,7 +81,9 @@ REHEARSE_DATA = [
             'entities': [(3, 9, 'PERSON')],
             'tags': ['INTJ', 'PROPN', 'PUNCT', 'ADV', 'AUX', 'PRON', 'PUNCT'],
             'deps': ['intj', 'ROOT', 'punct', 'advmod', 'nsubj', 'advcl', 'punct'],
-            'heads': [1, 1, 1, 5, 5, 1, 1]
+            'heads': [1, 1, 1, 5, 5, 1, 1],
+            'morphs': ['', 'VerbForm=Inf', 'PunctType=Comm', '', 'Case=Nom|Person=2|PronType=Prs', 'Aspect=Prog|Tense=Pres|VerbForm=Part', 'PunctType=Peri'],
+            'cats': {'greeting': 1.0, 'question': 1.0}
         }
     ),
     # UTENCIL new label
@@ -79,7 +93,9 @@ REHEARSE_DATA = [
             'entities': [(0, 7, 'PERSON'), (19, 24, 'UTENSIL')],
             'tags': ['PROPN', 'VERB', 'ADJ', 'NOUN', 'PUNCT'],
             'deps': ['nsubj', 'ROOT', 'amod', 'dobj', 'punct'],
-            'heads': [1, 1, 3, 1, 1]
+            'heads': [1, 1, 3, 1, 1],
+            'morphs': ['Number=Sing', 'Tense=Past|VerbForm=Fin', 'Degree=Pos', 'Number=Plur', 'PunctType=Peri'],
+            'cats': {'statement': 1.0}
         }
     )
 ]
@@ -87,23 +103,29 @@ REHEARSE_DATA = [
 
 def _add_ner_label(ner, data):
     for _, annotations in data:
-        for ent in annotations.get('entities'):
+        for ent in annotations['entities']:
             ner.add_label(ent[2])
 
 
 def _add_tagger_label(tagger, data):
     for _, annotations in data:
-        for tag in annotations.get('tags'):
+        for tag in annotations['tags']:
             tagger.add_label(tag)
 
 
 def _add_parser_label(parser, data):
     for _, annotations in data:
-        for dep in annotations.get('deps'):
+        for dep in annotations['deps']:
             parser.add_label(dep)
 
 
-def _opt_loop(
+def _add_textcat_label(textcat, data):
+    for _, annotations in data:
+        for cat in annotations['cats']:
+            textcat.add_label(cat)
+
+
+def _optimize(
         nlp,
         component: str,
         data: List,
@@ -117,6 +139,8 @@ def _opt_loop(
         _add_tagger_label(pipe, data)
     elif component == 'parser':
         _add_tagger_label(pipe, data)
+    elif component == 'textcat_multilabel':
+        _add_textcat_label(pipe, data)
     else:
         raise NotImplementedError
 
@@ -136,12 +160,13 @@ def _opt_loop(
     return nlp
 
 
-@pytest.mark.parametrize("component", ['ner', 'tagger', 'parser'])
+@pytest.mark.parametrize("component", ['ner', 'tagger', 'parser', 'textcat_multilabel'])
 def test_rehearse(component):
+    print(component)
     nlp = spacy.blank("en")
     nlp.add_pipe(component)
-    nlp = _opt_loop(nlp, component, TRAIN_DATA, False)
-    _opt_loop(nlp, component, REHEARSE_DATA, True)
+    nlp = _optimize(nlp, component, TRAIN_DATA, False)
+    _optimize(nlp, component, REHEARSE_DATA, True)
 
 
-test_rehearse('parser')
+test_rehearse('textcat_multilabel')
