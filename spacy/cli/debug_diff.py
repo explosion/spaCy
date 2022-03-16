998fee867f494823f50c57c91b1672f2e7aa88b8
Lj Miranda <ljvmiranda@gmail.com>
2022-03-16 14:11:48 +0800

    Include gpu as a user option for CLI

--- spacy/cli/debug_diff.py
+++ spacy/cli/debug_diff.py
@@ -18,19 +18,28 @@ def debug_diff_cli(
     # fmt: off
     ctx: typer.Context,
     config_path: Path = Arg(..., help="Path to config file", exists=True, allow_dash=True),
-    compare_to: Optional[Path] = Opt(..., help="Path to another config file to diff against", exists=True, allow_dash=True),
+    compare_to: Optional[Path] = Opt(None, help="Path to another config file to diff against", exists=True, allow_dash=True),
     optimize: Optimizations = Opt(Optimizations.efficiency.value, "--optimize", "-o", help="Whether the user config was optimized for efficiency or accuracy."),
+    gpu: bool = Opt(False, "--gpu", "-G", help="Whether the original config can run on a GPU"),
     pretraining: bool = Opt(False, "--pretraining", "--pt", help="Whether to compare on a config with pretraining involved")
     # fmt: on
 ):
     """Show a diff of a config file with respect to a default configuration."""
     debug_diff(
-        config_path, compare_to=compare_to, optimize=optimize, pretraining=pretraining
+        config_path,
+        compare_to=compare_to,
+        gpu=gpu,
+        optimize=optimize,
+        pretraining=pretraining,
     )
 
 
 def debug_diff(
-    config_path: Path, compare_to: Optional[Path], optimize: str, pretraining: bool
+    config_path: Path,
+    compare_to: Optional[Path],
+    gpu: bool,
+    optimize: str,
+    pretraining: bool,
 ):
     msg = Printer()
     with show_validation_error(hint_fill=False):
@@ -41,7 +50,8 @@ def debug_diff(
         else:
             other_config = init_config(
                 optimize=optimize.value,
-                lang=list(user_config["nlp"]["lang"]),
+                lang=user_config["nlp"]["lang"],
+                gpu=gpu,
                 pipeline=list(user_config["nlp"]["pipeline"]),
                 pretraining=pretraining,
                 silent=True,
@@ -52,4 +62,5 @@ def debug_diff(
 
     if user == other:
         msg.warn("No diff to show: configs are identical")
-    print(diff_strings(user, other))
+    else:
+        print(diff_strings(user, other))
