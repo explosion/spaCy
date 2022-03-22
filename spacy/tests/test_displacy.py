32a9e1f94098bba244140823fe1334dc88f06eb1
Joachim Fainberg <joachimfainberg@Joachims-MacBook-Pro.local>
2022-03-21 21:02:15 -0400

    Added test for overlapping arcs

--- spacy/tests/test_displacy.py
+++ spacy/tests/test_displacy.py
@@ -8,6 +8,26 @@ from spacy.lang.fa import Persian
 from spacy.tokens import Span, Doc
 
 
+@pytest.mark.issue(5447)
+def test_issue5447():
+    """Test that overlapping arcs get separate levels."""
+    renderer = DependencyRenderer()
+    words = [
+        {"text": "This", "tag": "DT"},
+        {"text": "is", "tag": "VBZ"},
+        {"text": "a", "tag": "DT"},
+        {"text": "sentence.", "tag": "NN"},
+    ]
+    arcs = [
+        {"start": 0, "end": 1, "label": "nsubj", "dir": "left"},
+        {"start": 2, "end": 3, "label": "det", "dir": "left"},
+        {"start": 2, "end": 3, "label": "overlap", "dir": "left"},
+        {"start": 1, "end": 3, "label": "attr", "dir": "left"},
+    ]
+    html = renderer.render([{"words": words, "arcs": arcs}])
+    assert renderer.highest_level == 3
+
+
 @pytest.mark.issue(2361)
 def test_issue2361(de_vocab):
     """Test if < is escaped when rendering"""
