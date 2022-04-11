4fb6acb5df3661d4eaf5877813b8666eb97e9b63
richardpaulhudson <richard@explosion.ai>
2022-04-11 22:20:32 +0200

    Display warning when spacy.explain() finds no term

--- spacy/errors.py
+++ spacy/errors.py
@@ -196,6 +196,9 @@ class Warnings(metaclass=ErrorsWithCodes):
             "surprising to you, make sure the Doc was processed using a model "
             "that supports span categorization, and check the `doc.spans[spans_key]` "
             "property manually if necessary.")
+    W118 = ("Term '{term}' not found in glossary. It may however be explained in documentation "
+            "for the corpora used to train the language. Please examine the contents of "
+            "`nlp.meta[\"sources\"]` where such documentation may be linked.")
 
 
 class Errors(metaclass=ErrorsWithCodes):
