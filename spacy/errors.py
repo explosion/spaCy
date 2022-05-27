b59c6a500f961a35d830fe7bf4df17a8b06d928c
shademe <shadeMe@users.noreply.github.com>
2022-05-27 17:50:47 +0200

    `Language.pipe()`: Serialize `Doc` objects to bytes when using multiprocessing to avoid pickling overhead

--- spacy/errors.py
+++ spacy/errors.py
@@ -919,6 +919,7 @@ class Errors(metaclass=ErrorsWithCodes):
     E1035 = ("Token index {i} out of bounds ({length})")
     E1036 = ("Cannot index into NoneNode")
     E1037 = ("Invalid attribute value '{attr}'.")
+    E1038 = ("Expected a string, 'Doc', or 'bytes' as input, but got: {type}.")
 
 
 # Deprecated model shortcuts, only used in errors and warnings
