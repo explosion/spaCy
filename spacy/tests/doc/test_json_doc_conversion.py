4e6c95286889ff14077012d67b5b78dfe1e14690
Raphael Mitsch <r.mitsch@outlook.com>
2022-05-27 12:28:31 +0200

    Implement Doc.from_json(): fixing bug in tests.
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- spacy/tests/doc/test_json_doc_conversion.py
+++ spacy/tests/doc/test_json_doc_conversion.py
@@ -150,10 +150,10 @@ def test_json_to_doc_sents(doc, doc_without_deps):
     for test_doc in (doc, doc_without_deps):
         json_doc = test_doc.to_json()
         new_doc = Doc(doc.vocab).from_json(json_doc, validate=True)
-        assert [sent.text for sent in doc.sents] == [
+        assert [sent.text for sent in test_doc.sents] == [
             sent.text for sent in new_doc.sents
         ]
-        assert [token.is_sent_start for token in doc] == [
+        assert [token.is_sent_start for token in test_doc] == [
             token.is_sent_start for token in new_doc
         ]
 
