06e7a56e20251411c4732488c2799b657a125b73
shademe <shadeMe@users.noreply.github.com>
2022-06-23 15:15:17 +0200

    `Morphology`: Refactor to use C types, reduce allocations, remove unused code

--- spacy/tokens/morphanalysis.pxd
+++ spacy/tokens/morphanalysis.pxd
@@ -1,9 +1,11 @@
 from ..vocab cimport Vocab
 from ..typedefs cimport hash_t
-from ..structs cimport MorphAnalysisC
+from ..morphology cimport MorphAnalysisC
 
 
 cdef class MorphAnalysis:
     cdef readonly Vocab vocab
     cdef readonly hash_t key
-    cdef MorphAnalysisC c
+    cdef const MorphAnalysisC* c
+
+    cdef void _init_c(self, hash_t key)
--- spacy/tokens/morphanalysis.pyx
+++ spacy/tokens/morphanalysis.pyx
@@ -5,7 +5,10 @@ from ..errors import Errors
 from ..morphology import Morphology
 from ..vocab cimport Vocab
 from ..typedefs cimport hash_t, attr_t
-from ..morphology cimport list_features, check_feature, get_by_field
+from ..morphology cimport list_features, check_feature, get_by_field, MorphAnalysisC
+
+
+cdef MorphAnalysisC EMPTY_MORPH_TAG = MorphAnalysisC()
 
 
 cdef class MorphAnalysis:
@@ -13,39 +16,44 @@ cdef class MorphAnalysis:
     def __init__(self, Vocab vocab, features=dict()):
         self.vocab = vocab
         self.key = self.vocab.morphology.add(features)
-        analysis = <const MorphAnalysisC*>self.vocab.morphology.tags.get(self.key)
+        self._init_c(self.key)
+
+    cdef void _init_c(self, hash_t key):
+        cdef const MorphAnalysisC* analysis = self.vocab.morphology.get_morph_c(key)
         if analysis is not NULL:
-            self.c = analysis[0]
+            self.c = analysis
         else:
-            memset(&self.c, 0, sizeof(self.c))
+            self.c = <const MorphAnalysisC*>&EMPTY_MORPH_TAG
+
+    @classmethod
+    def from_features(cls, Vocab vocab, features):
+        """Create a morphological analysis from the given features."""
+        cdef hash_t key = vocab.morphology.add(features)
+        return cls.from_id(vocab, key)
 
     @classmethod
     def from_id(cls, Vocab vocab, hash_t key):
         """Create a morphological analysis from a given ID."""
-        cdef MorphAnalysis morph = MorphAnalysis.__new__(MorphAnalysis, vocab)
+        cdef MorphAnalysis morph = MorphAnalysis(vocab)
         morph.vocab = vocab
         morph.key = key
-        analysis = <const MorphAnalysisC*>vocab.morphology.tags.get(key)
-        if analysis is not NULL:
-            morph.c = analysis[0]
-        else:
-            memset(&morph.c, 0, sizeof(morph.c))
+        morph._init_c(key)
         return morph
 
     def __contains__(self, feature):
         """Test whether the morphological analysis contains some feature."""
         cdef attr_t feat_id = self.vocab.strings.as_int(feature)
-        return check_feature(&self.c, feat_id)
+        return check_feature(self.c, feat_id)
 
     def __iter__(self):
         """Iterate over the features in the analysis."""
         cdef attr_t feature
-        for feature in list_features(&self.c):
+        for feature in list_features(self.c):
             yield self.vocab.strings[feature]
 
     def __len__(self):
         """The number of features in the analysis."""
-        return self.c.length
+        return self.c.features.size()
 
     def __hash__(self):
         return self.key
@@ -61,7 +69,7 @@ cdef class MorphAnalysis:
     def get(self, field):
         """Retrieve feature values by field."""
         cdef attr_t field_id = self.vocab.strings.as_int(field)
-        cdef np.ndarray results = get_by_field(&self.c, field_id)
+        cdef np.ndarray results = get_by_field(self.c, field_id)
         features = [self.vocab.strings[result] for result in results]
         return [f.split(Morphology.FIELD_SEP)[1] for f in features]
 
