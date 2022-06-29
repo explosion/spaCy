ee692088cbcb04e9f5b0c1078833c49e8445515b
Raphael Mitsch <r.mitsch@outlook.com>
2022-06-29 10:06:11 +0200

    Update spacy/pipeline/entity_linker.py
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/pipeline/entity_linker.py
+++ spacy/pipeline/entity_linker.py
@@ -96,7 +96,7 @@ def make_entity_linker(
     use_gold_ents (bool): Whether to copy entities from gold docs or not. If false, another
         component must provide entity annotations.
     threshold (Optional[float]): Confidence threshold for entity predictions. If confidence is below the threshold,
-        prediction is discarded. If None, predictions scores are not checked.
+        prediction is discarded. If None, predictions are not filtered by any threshold.
     """
 
     if not model.attrs.get("include_span_maker", False):
