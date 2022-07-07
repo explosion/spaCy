6ed4c93ea2081e8d9bb01b5b2797d9a73c126e83
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2022-07-07 17:47:56 +0530

    tidy up

--- .github/spacy_universe_alert.py
+++ .github/spacy_universe_alert.py
@@ -7,7 +7,6 @@ from slack_sdk.web.client import WebClient
 
 CHANNEL = "#alerts-universe"
 SLACK_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "ENV VAR not available!")
-print(SLACK_TOKEN)
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 client = WebClient(SLACK_TOKEN)
@@ -63,5 +62,6 @@ blocks = [
 
 client.chat_postMessage(
     channel=CHANNEL,
+    text="spaCy universe project PR alert",
     blocks=blocks
 )
