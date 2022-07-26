1f01b0f90e42e9e73cfc8271e7e6534971ca1e86
thomashacker <EdwardSchmuhl@web.de>
2022-07-26 19:02:09 +0200

    Init

--- spacy/training/loggers.py
+++ spacy/training/loggers.py
@@ -1,10 +1,13 @@
-from typing import TYPE_CHECKING, Dict, Any, Tuple, Callable, List, Optional, IO
+from typing import TYPE_CHECKING, Dict, Any, Tuple, Callable, List, Optional, IO, Union
 from wasabi import Printer
+from pathlib import Path
 import tqdm
 import sys
+import srsly
 
 from ..util import registry
 from ..errors import Errors
+from .. import util
 
 if TYPE_CHECKING:
     from ..language import Language  # noqa: F401
@@ -80,6 +83,108 @@ def console_logger(progress_bar: bool = False):
                 + scores
                 + ["{0:.2f}".format(float(info["score"]))]
             )
+            if progress is not None:
+                progress.close()
+            write(
+                msg.row(data, widths=table_widths, aligns=table_aligns, spacing=spacing)
+            )
+            if progress_bar:
+                # Set disable=None, so that it disables on non-TTY
+                progress = tqdm.tqdm(
+                    total=eval_frequency, disable=None, leave=False, file=stderr
+                )
+                progress.set_description(f"Epoch {info['epoch']+1}")
+
+        def finalize() -> None:
+            pass
+
+        return log_step, finalize
+
+    return setup_printer
+
+
+@registry.loggers("spacy.FileLogger.v1")
+def file_logger(progress_bar: bool = False, output_path: Union[str, Path] = "./"):
+    """The FileLogger.v1 is a modified ConsoleLogger.v1 that saves the training logs to a jsonl file"""
+
+    output_path = util.ensure_path(output_path)
+    output_file = output_path / "log.jsonl"
+    if output_file.exists():
+        output_file.unlink()
+    elif not output_path.exists():
+        output_path.mkdir(parents=True)
+
+    def setup_printer(
+        nlp: "Language", stdout: IO = sys.stdout, stderr: IO = sys.stderr
+    ) -> Tuple[Callable[[Optional[Dict[str, Any]]], None], Callable[[], None]]:
+        write = lambda text: print(text, file=stdout, flush=True)
+        msg = Printer(no_print=True)
+        # ensure that only trainable components are logged
+        logged_pipes = [
+            name
+            for name, proc in nlp.pipeline
+            if hasattr(proc, "is_trainable") and proc.is_trainable
+        ]
+        eval_frequency = nlp.config["training"]["eval_frequency"]
+        score_weights = nlp.config["training"]["score_weights"]
+        score_cols = [col for col, value in score_weights.items() if value is not None]
+        loss_cols = [f"Loss {pipe}" for pipe in logged_pipes]
+        spacing = 2
+        table_header, table_widths, table_aligns = setup_table(
+            cols=["E", "#"] + loss_cols + score_cols + ["Score"],
+            widths=[3, 6] + [8 for _ in loss_cols] + [6 for _ in score_cols] + [6],
+        )
+        write(msg.row(table_header, widths=table_widths, spacing=spacing))
+        write(msg.row(["-" * width for width in table_widths], spacing=spacing))
+        progress = None
+
+        def log_step(info: Optional[Dict[str, Any]]) -> None:
+            nonlocal progress
+
+            if info is None:
+                # If we don't have a new checkpoint, just return.
+                if progress is not None:
+                    progress.update(1)
+                return
+
+            losses = []
+            log_losses = {}
+            for pipe_name in logged_pipes:
+                losses.append("{0:.2f}".format(float(info["losses"][pipe_name])))
+                log_losses[pipe_name] = float(info["losses"][pipe_name])
+
+            scores = []
+            log_scores = {}
+            for col in score_cols:
+                score = info["other_scores"].get(col, 0.0)
+                try:
+                    score = float(score)
+                except TypeError:
+                    err = Errors.E916.format(name=col, score_type=type(score))
+                    raise ValueError(err) from None
+                if col != "speed":
+                    score *= 100
+                scores.append("{0:.2f}".format(score))
+                log_scores[str(col)] = score
+
+            data = (
+                [info["epoch"], info["step"]]
+                + losses
+                + scores
+                + ["{0:.2f}".format(float(info["score"]))]
+            )
+
+            # Write to log file per log_step
+            log_data = {
+                "epoch": info["epoch"],
+                "step": info["step"],
+                "losses": log_losses,
+                "scores": log_scores,
+                "score": float(info["score"]),
+            }
+            with (output_file).open("a") as file_:
+                file_.write(srsly.json_dumps(log_data) + "\n")
+
             if progress is not None:
                 progress.close()
             write(
