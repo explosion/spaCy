de5559420f15926f5e703f61980c1ca550b64d9b
Patrick J. Burns <patricks@diyclassics.org>
2022-07-27 18:05:18 -0400

    Add minimal lex_attrs

--- spacy/lang/la/__init__.py
+++ spacy/lang/la/__init__.py
@@ -1,9 +1,13 @@
+from ...language import Language, BaseDefaults
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
+
 
 class LatinDefaults(BaseDefaults):
     tokenizer_exceptions = TOKENIZER_EXCEPTIONS
     stop_words = STOP_WORDS    
+    lex_attr_getters = LEX_ATTRS
 
 
 class Latin(Language):
