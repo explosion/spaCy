d8cac354baa984d1e46b6605f6363460197e6f9f
svlandeg <svlandeg@github.com>
2022-08-09 17:16:49 +0200

    rename Python module to entity_ruler

--- spacy/pipeline/__init__.py
+++ spacy/pipeline/__init__.py
@@ -3,7 +3,7 @@ from .dep_parser import DependencyParser
 from .edit_tree_lemmatizer import EditTreeLemmatizer
 from .entity_linker import EntityLinker
 from .ner import EntityRecognizer
-from .entityruler import EntityRuler
+from .entity_ruler import EntityRuler
 from .lemmatizer import Lemmatizer
 from .morphologizer import Morphologizer
 from .pipe import Pipe
