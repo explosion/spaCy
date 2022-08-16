3b9581c84fb9a98d59f2b7fe759bf802a97ecfcb
shademe <shadeMe@users.noreply.github.com>
2022-08-16 14:31:30 +0200

    `mypy` fix

--- spacy/tests/parser/test_search.py
+++ spacy/tests/parser/test_search.py
@@ -6,11 +6,11 @@ import sys
 # list of Cython modules containing tests
 cython_test_modules = ["spacy.tests.parser._search"]
 
-for mod in cython_test_modules:
+for mod_name in cython_test_modules:
     try:
         # For each callable in `mod` with name `test_*`,
         # set the result as an attribute of this module.
-        mod = importlib.import_module(mod)
+        mod = importlib.import_module(mod_name)
         for name in dir(mod):
             item = getattr(mod, name)
             if callable(item) and name.startswith("test_"):
