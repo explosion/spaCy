73ae91ba939a9150fe4bf8337281b4738a1380f5
Adriane Boyd <adrianeboyd@gmail.com>
2022-08-24 09:56:12 +0200

    Remove lg stop words

--- spacy/lang/lg/__init__.py
+++ spacy/lang/lg/__init__.py
@@ -1,4 +1,3 @@
-from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
 from .punctuation import TOKENIZER_INFIXES
 from ...language import Language, BaseDefaults
@@ -7,7 +6,6 @@ from ...language import Language, BaseDefaults
 class LugandaDefaults(BaseDefaults):
     lex_attr_getters = LEX_ATTRS
     infixes = TOKENIZER_INFIXES
-    stop_words = STOP_WORDS
 
 
 class Luganda(Language):
