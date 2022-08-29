4bce8fa7557d86d61347da501fe2f62424f7d44c
Adriane Boyd <adrianeboyd@gmail.com>
2022-08-29 13:23:24 +0200

    Remove setup_requires from setup.cfg (#11384)
    * Remove setup_requires from setup.cfg
    
    * Update requirements test to ignore cython in setup.cfg

--- spacy/tests/package/test_requirements.py
+++ spacy/tests/package/test_requirements.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 def test_build_dependencies():
     # Check that library requirements are pinned exactly the same across different setup files.
-    # TODO: correct checks for numpy rather than ignoring
     libs_ignore_requirements = [
+        "cython",
         "pytest",
         "pytest-timeout",
         "mock",
