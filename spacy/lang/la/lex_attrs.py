092b1eef20d814aaa76b087092f6c3d6fd66ea34
Patrick J. Burns <diyclassics@users.noreply.github.com>
2022-08-29 15:21:41 -0400

    Update spacy/lang/la/lex_attrs.py
    Remove duplicate form in Latin lex_attrs
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/lang/la/lex_attrs.py
+++ spacy/lang/la/lex_attrs.py
@@ -6,7 +6,7 @@ roman_numerals_compile = re.compile(r'^(?=[MDCLXVI])M*D?C{0,4}L?X{0,4}V?I{0,4}$'
 
 _num_words = set(
     """
-unus una unum duo duae duo tres tria quattuor quinque sex septem octo novem decem
+unus una unum duo duae tres tria quattuor quinque sex septem octo novem decem
 """.split()
 )
 
