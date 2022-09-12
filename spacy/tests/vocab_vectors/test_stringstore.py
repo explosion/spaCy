9cc357a54d1b71ff1d45e4776b087342105c0d6d
shademe <shadeMe@users.noreply.github.com>
2022-09-12 12:50:59 +0200

    Add test for `StringStore.items()`

--- spacy/tests/vocab_vectors/test_stringstore.py
+++ spacy/tests/vocab_vectors/test_stringstore.py
@@ -24,6 +24,11 @@ def test_stringstore_from_api_docs(stringstore):
     stringstore.add("orange")
     all_strings = [s for s in stringstore]
     assert all_strings == ["apple", "orange"]
+    all_strings_and_hashes = list(stringstore.items())
+    assert all_strings_and_hashes == [
+        ("apple", 8566208034543834098),
+        ("orange", 2208928596161743350),
+    ]
     banana_hash = stringstore.add("banana")
     assert len(stringstore) == 3
     assert banana_hash == 2525716904149915114
