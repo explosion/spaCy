42434fa443ca7adccacd07a74f75f722dd0c5490
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2022-09-26 14:20:02 +0200

    rename unit test

--- spacy/tests/lang/grc/test_tokenizer.py
+++ spacy/tests/lang/grc/test_tokenizer.py
@@ -12,7 +12,7 @@ GRC_TOKEN_EXCEPTION_TESTS = [
 
 
 @pytest.mark.parametrize("text,expected_tokens", GRC_TOKEN_EXCEPTION_TESTS)
-def test_ga_tokenizer_handles_exception_cases(grc_tokenizer, text, expected_tokens):
+def test_grc_tokenizer(grc_tokenizer, text, expected_tokens):
     tokens = grc_tokenizer(text)
     token_list = [token.text for token in tokens if not token.is_space]
     assert expected_tokens == token_list
