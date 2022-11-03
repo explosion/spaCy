641f3501446a4c6f816446a8c55ff7cf1f9f1046
Adriane Boyd <adrianeboyd@gmail.com>
2022-11-03 15:19:58 +0100

    Simplify and add error message

--- spacy/cli/project/remote_storage.py
+++ spacy/cli/project/remote_storage.py
@@ -85,24 +85,17 @@ class RemoteStorage:
                     # This requires that the path is added correctly, relative
                     # to root. This is how we set things up in push()
                     def is_within_directory(directory, target):
-
                         abs_directory = os.path.abspath(directory)
                         abs_target = os.path.abspath(target)
-
                         prefix = os.path.commonprefix([abs_directory, abs_target])
-
                         return prefix == abs_directory
 
-                    def safe_extract(
-                        tar, path=".", members=None, *, numeric_owner=False
-                    ):
-
+                    def safe_extract(tar, path):
                         for member in tar.getmembers():
                             member_path = os.path.join(path, member.name)
                             if not is_within_directory(path, member_path):
-                                raise Exception("Attempted Path Traversal in Tar File")
-
-                        tar.extractall(path, members, numeric_owner=numeric_owner)
+                                raise ValueError(Errors.E852)
+                        tar.extractall(path)
 
                     safe_extract(tar_file, self.root)
         return url
