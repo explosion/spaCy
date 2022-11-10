1903af835200e83f1fc36152e754a5d795f908d5
Marcus Blättermann <marcus@essenmitsosse.de>
2022-11-10 01:01:24 +0100

    Apply prettier to all files

--- website/next.config.js
+++ website/next.config.js
@@ -1,12 +1,12 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-});
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withMDX({
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-});
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- website/pages/api/hello.ts
+++ website/pages/api/hello.ts
@@ -2,12 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+    name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    res.status(200).json({ name: 'John Doe' })
 }
