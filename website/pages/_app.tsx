7f0092c6e27534a8b325c780b7dbb7c282221f75
Marcus Blättermann <marcus@essenmitsosse.de>
2022-11-13 16:22:49 +0100

    Add import for custom components

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from 'next/app'
+import { MDXProvider } from '@mdx-js/react'
+import mdxComponents from '../components/mdxComponents'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <MDXProvider components={mdxComponents}>
+            <Component {...pageProps} />
+        </MDXProvider>
+    )
 }
