9977fe7a3c8a94218c0deab6456ff1ae7f32e32d
Marcus Blättermann <marcus@essenmitsosse.de>
2022-11-14 15:43:54 +0100

    Add plugin to fix problem with wrapped images
    For more details see this issue: https://github.com/mdx-js/mdx/issues/1798

--- website/plugins/index.mjs
+++ website/plugins/index.mjs
@@ -1,10 +1,12 @@
 import remarkGfm from 'remark-gfm'
+import remarkUnwrapImages from 'remark-unwrap-images'
 
 import remarkCustomAttrs from './remarkCustomAttrs.mjs'
 import remarkWrapSections from './remarkWrapSections.mjs'
 
 const remarkPlugins = [
     remarkGfm,
+    remarkUnwrapImages,
     remarkCustomAttrs,
     remarkWrapSections,
 ]
