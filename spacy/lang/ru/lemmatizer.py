d2045fa145037372b4f5b92c34dadec70e37bf41
Adriane Boyd <adrianeboyd@gmail.com>
2022-11-18 08:34:07 +0100

    Remove traces of previous behavior for uninflected POS

--- spacy/lang/ru/lemmatizer.py
+++ spacy/lang/ru/lemmatizer.py
@@ -60,9 +60,7 @@ class RussianLemmatizer(Lemmatizer):
         if univ_pos == "PUNCT":
             return [PUNCT_RULES.get(string, string)]
         if univ_pos not in ("ADJ", "DET", "NOUN", "NUM", "PRON", "PROPN", "VERB"):
-            # Skip unchangeable pos
             return self.pymorphy2_lookup_lemmatize(token)
-            return [string.lower()]
         analyses = self._morph.parse(string)
         filtered_analyses = []
         for analysis in analyses:
