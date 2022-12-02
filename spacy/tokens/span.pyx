29578026829ac6070f43d6c510c3d59abe9b0569
Edward <43848523+thomashacker@users.noreply.github.com>
2022-12-02 12:16:36 +0100

    adjust formatting
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/tokens/span.pyx
+++ spacy/tokens/span.pyx
@@ -801,7 +801,7 @@ cdef class Span:
             if label != self.span_c().label :
                 old_label = self.span_c().label
                 self.span_c().label = label
-                new = Underscore(Underscore.span_extensions, self, start=self.span_c().start_char, end=self.span_c().end_char, label = self.label, kb_id = self.kb_id, span_id=self.id)
+                new = Underscore(Underscore.span_extensions, self, start=self.span_c().start_char, end=self.span_c().end_char, label=self.label, kb_id=self.kb_id, span_id=self.id)
                 old = Underscore(Underscore.span_extensions, self, start=self.span_c().start_char, end=self.span_c().end_char, label=old_label, kb_id=self.kb_id, span_id=self.id)
                 Underscore._replace_keys(old, new)
             else:
