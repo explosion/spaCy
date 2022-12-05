8cc48746553e98e3bd2db12d49ed7a86b2cc79a8
Marcus Blättermann <marcus@essenmitsosse.de>
2022-12-05 22:36:14 +0100

    Add plugin to replace strings

--- website/plugins/index.mjs
+++ website/plugins/index.mjs
@@ -4,9 +4,11 @@ import remarkUnwrapImages from 'remark-unwrap-images'
 import remarkCustomAttrs from './remarkCustomAttrs.mjs'
 import remarkWrapSections from './remarkWrapSections.mjs'
 import remarkCodeBlocks from './remarkCodeBlocks.mjs'
+import remarkFindAndReplace from './remarkFindAndReplace.mjs'
 
 const remarkPlugins = [
     remarkGfm,
+    remarkFindAndReplace,
     remarkUnwrapImages,
     remarkCustomAttrs,
     remarkCodeBlocks,
