8d12c455a244cc2f61261f7d9e609a547573e9c6
Marcus Blättermann <marcus@essenmitsosse.de>
2022-12-12 17:33:25 +0100

    Add `lang="en"`

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="en">
             <Head />
             <body>
                 <Main />
