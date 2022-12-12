4e043b543018cdbb0d7bfc93e4c5a55d54182b6a
Adriane Boyd <adrianeboyd@gmail.com>
2022-12-12 08:45:35 +0100

    Cast to uint64 for all array-based doc representations (#11933)
    * Convert all individual values explicitly to uint64 for array-based doc representations
    
    * Temporarily test with latest numpy v1.24.0rc
    
    * Remove unnecessary conversion from attr_t
    
    * Reduce number of individual casts
    
    * Convert specifically from int32 to uint64
    
    * Revert "Temporarily test with latest numpy v1.24.0rc"
    
    This reverts commit eb0e3c5006515b9a7ff52bae59484c909b8a3f65.
    
    * Also use int32 in tests

--- spacy/tests/doc/test_array.py
+++ spacy/tests/doc/test_array.py
@@ -123,14 +123,14 @@ def test_doc_from_array_heads_in_bounds(en_vocab):
 
     # head before start
     arr = doc.to_array(["HEAD"])
-    arr[0] = -1
+    arr[0] = numpy.int32(-1).astype(numpy.uint64)
     doc_from_array = Doc(en_vocab, words=words)
     with pytest.raises(ValueError):
         doc_from_array.from_array(["HEAD"], arr)
 
     # head after end
     arr = doc.to_array(["HEAD"])
-    arr[0] = 5
+    arr[0] = numpy.int32(5).astype(numpy.uint64)
     doc_from_array = Doc(en_vocab, words=words)
     with pytest.raises(ValueError):
         doc_from_array.from_array(["HEAD"], arr)
