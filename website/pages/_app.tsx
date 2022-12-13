667e2848c168f70c0e008e69c03b2f566de03090
Marcus Blättermann <marcus@essenmitsosse.de>
2022-12-13 03:49:29 +0100

    Add sitemap

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../src/styles/layout.sass'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { MDXProvider } from '@mdx-js/react'
 import { remarkComponents } from '../src/remark'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <MDXProvider components={remarkComponents}>
-            <Component {...pageProps} />
-        </MDXProvider>
+        <>
+            <Head>
+                <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
+            </Head>
+            <MDXProvider components={remarkComponents}>
+                <Component {...pageProps} />
+            </MDXProvider>
+        </>
     )
 }
