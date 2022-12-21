f62cd9782edf4582d1b91e0e9d832cd83b233e14
Daniël de Kok <me@danieldk.eu>
2022-12-21 10:14:39 +0100

    Adjust to new `Schedule` class and pass scores to `Optimizer`
    Requires https://github.com/explosion/thinc/pull/804

--- spacy/training/loop.py
+++ spacy/training/loop.py
@@ -204,7 +204,7 @@ def train_while_improving(
         if before_update:
             before_update_args = {"step": step, "epoch": epoch}
             before_update(nlp, before_update_args)
-        dropout = next(dropouts)  # type: ignore
+        dropout = dropouts(optimizer.step)  # type: ignore
         for subbatch in subdivide_batch(batch, accumulate_gradient):
             nlp.update(
                 subbatch,
@@ -230,6 +230,7 @@ def train_while_improving(
                     score, other_scores = evaluate()
             else:
                 score, other_scores = evaluate()
+            optimizer.last_score = score
             results.append((score, step))
             is_best_checkpoint = score == max(results)[0]
         else:
