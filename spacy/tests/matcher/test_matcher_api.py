8ee6551348bf485a219288a28e76de17211622c3
Adriane Boyd <adrianeboyd@gmail.com>
2023-01-09 13:08:17 +0100

    Parameterize test over greedy
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy/tests/matcher/test_matcher_api.py
+++ spacy/tests/matcher/test_matcher_api.py
@@ -225,18 +225,24 @@ def test_matcher_match_fuzzyn_various_edits(en_vocab, fuzzyn):
     assert len(matcher(doc)) == fuzzyn + 1
 
 
+@pytest.mark.parametrize("greedy", ["FIRST", "LONGEST"])
 @pytest.mark.parametrize("set_op", ["IN", "NOT_IN"])
-def test_matcher_match_fuzzyn_set_op_longest(en_vocab, set_op):
+def test_matcher_match_fuzzyn_set_op_longest(en_vocab, greedy, set_op):
     rules = {
         "GoogleNow": [[{"ORTH": {"FUZZY2": {set_op: ["Google", "Now"]}}, "OP": "+"}]]
     }
     matcher = Matcher(en_vocab)
     for key, patterns in rules.items():
-        matcher.add(key, patterns, greedy="LONGEST")
+        matcher.add(key, patterns, greedy=greedy)
 
     words = ["They", "like", "Goggle", "Noo"]
     doc = Doc(matcher.vocab, words=words)
-    assert len(matcher(doc)) == 1
+    spans = matcher(doc, as_spans=True)
+    assert len(spans) == 1
+    if set_op == "IN":
+        assert spans[0].text == "Goggle Noo"
+    else:
+        assert spans[0].text == "They like"
 
 
 def test_matcher_match_fuzzyn_set_multiple(en_vocab):
