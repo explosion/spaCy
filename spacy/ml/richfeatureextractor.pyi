5d3cdd6a2fddc326cda4908a5177837821447224
richard@explosion.ai <richard@explosion.ai>
2023-02-01 18:31:30 +0100

    Fix mypy issue

--- spacy/ml/richfeatureextractor.pyi
+++ spacy/ml/richfeatureextractor.pyi
@@ -1,5 +1,14 @@
+from typing import List, Optional
+from thinc.types import Ints2d
+from thinc.api import Model
 from ..tokens import Doc
 
+def RichFeatureExtractor(
+    *,
+    case_sensitive: bool,
+    pref_lengths: Optional[List[int]] = None,
+    suff_lengths: Optional[List[int]] = None,
+) -> Model[List[Doc], List[Ints2d]]: ...
 def get_character_combination_hashes(
     *,
     doc: Doc,
