befacb589c217b8707bbfdd6b1c48a266679fb90
richard@explosion.ai <richard@explosion.ai>
2023-02-01 18:11:46 +0100

    Correct interface

--- spacy/ml/richfeatureextractor.pyi
+++ spacy/ml/richfeatureextractor.pyi
@@ -3,7 +3,7 @@ from ..tokens import Doc
 def get_character_combination_hashes(
     *,
     doc: Doc,
-    case_sensitive: bool, 
+    case_sensitive: bool,
     p_lengths: bytes,
     s_lengths: bytes,
-):
+): ...
