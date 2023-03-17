4adb849f97a4ff8e2d333c1cb09da4f05304db70
Adriane Boyd <adrianeboyd@gmail.com>
2023-03-17 08:32:11 +0100

    Support floret for PretrainVectors

--- spacy/errors.py
+++ spacy/errors.py
@@ -549,8 +549,8 @@ class Errors(metaclass=ErrorsWithCodes):
             "during training, make sure to include it in 'annotating components'")
 
     # New errors added in v3.x
-    E850 = ("The PretrainVectors objective currently only supports default "
-            "vectors, not {mode} vectors.")
+    E850 = ("The PretrainVectors objective currently only supports default or "
+            "floret vectors, not {mode} vectors.")
     E851 = ("The 'textcat' component labels should only have values of 0 or 1, "
             "but found value of '{val}'.")
     E852 = ("The tar file pulled from the remote attempted an unsafe path "
