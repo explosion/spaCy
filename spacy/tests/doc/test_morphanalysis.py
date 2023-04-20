58fd4bd1f258ed3bfcbf08b3331bafb0f3200c92
Adriane Boyd <adrianeboyd@gmail.com>
2023-04-20 13:05:20 +0200

    Restore test case

--- spacy/tests/doc/test_morphanalysis.py
+++ spacy/tests/doc/test_morphanalysis.py
@@ -32,6 +32,7 @@ def test_token_morph_key(i_has):
 
 def test_morph_props(i_has):
     assert i_has[0].morph.get("PronType") == ["prs"]
+    assert i_has[1].morph.get("PronType") == []
     assert i_has[1].morph.get("AsdfType", ["asdf"]) == ["asdf"]
     assert i_has[1].morph.get("AsdfType", default=["asdf", "qwer"]) == ["asdf", "qwer"]
 
