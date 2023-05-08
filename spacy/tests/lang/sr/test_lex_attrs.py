8fc4152297a2bbc14003e0132da4e7764a609d9a
Adriane Boyd <adrianeboyd@gmail.com>
2023-05-08 11:11:03 +0200

    Add tests for all custom lex attrs

---
+++ spacy/tests/lang/sr/test_lex_attrs.py
@@ -0,0 +1,18 @@
+import pytest
+from spacy.lang.sr.lex_attrs import like_num, norm, prefix, suffix
+
+
+@pytest.mark.parametrize(
+    "text,like_num,norm,prefix,suffix",
+    [
+        ("нула", True, "nula", "n", "ula"),
+        ("Казна", False, "kazna", "K", "zna"),
+    ],
+)
+def test_lex_attrs(sr_tokenizer, text, like_num, norm, prefix, suffix):
+    tokens = sr_tokenizer(text)
+    assert len(tokens) == 1
+    assert tokens[0].like_num == like_num
+    assert tokens[0].norm_ == norm
+    assert tokens[0].prefix_ == prefix
+    assert tokens[0].suffix_ == suffix
