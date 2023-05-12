c58e05ca63f17092fc1b32b6d34af5d9fba7af20
Adriane Boyd <adrianeboyd@gmail.com>
2023-05-12 11:18:57 +0200

    Set version to v3.5.3

--- spacy/about.py
+++ spacy/about.py
@@ -1,6 +1,6 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "3.5.2"
+__version__ = "3.5.3"
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
 __projects__ = "https://github.com/explosion/projects"
