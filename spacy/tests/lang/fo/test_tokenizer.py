a526b01e900dfaec83453b5310f13e0ae1e5351b
Adriane Boyd <adrianeboyd@gmail.com>
2023-11-15 09:43:42 +0100

    Make the tests a bit more compact

--- spacy/tests/lang/fo/test_tokenizer.py
+++ spacy/tests/lang/fo/test_tokenizer.py
@@ -1,70 +1,22 @@
 import pytest
 
 # examples taken from Basic LAnguage Resource Kit 1.0 for Faroese (https://maltokni.fo/en/resources) licensed with CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
+# fmt: off
 FO_TOKEN_EXCEPTION_TESTS = [
     (
         "Eftir løgtingslóg um samsýning og eftirløn landsstýrismanna v.m., skulu løgmaður og landsstýrismenn vanliga siga frá sær størv í almennari tænastu ella privatum virkjum, samtøkum ella stovnum. ",
         [
-            "Eftir",
-            "løgtingslóg",
-            "um",
-            "samsýning",
-            "og",
-            "eftirløn",
-            "landsstýrismanna",
-            "v.m.",
-            ",",
-            "skulu",
-            "løgmaður",
-            "og",
-            "landsstýrismenn",
-            "vanliga",
-            "siga",
-            "frá",
-            "sær",
-            "størv",
-            "í",
-            "almennari",
-            "tænastu",
-            "ella",
-            "privatum",
-            "virkjum",
-            ",",
-            "samtøkum",
-            "ella",
-            "stovnum",
-            ".",
+            "Eftir", "løgtingslóg", "um", "samsýning", "og", "eftirløn", "landsstýrismanna", "v.m.", ",", "skulu", "løgmaður", "og", "landsstýrismenn", "vanliga", "siga", "frá", "sær", "størv", "í", "almennari", "tænastu", "ella", "privatum", "virkjum", ",", "samtøkum", "ella", "stovnum", ".",
         ],
     ),
     (
         "Sambandsflokkurin gongur aftur við 2,7 prosentum í mun til valið í 1994, tá flokkurin fekk undirtøku frá 23,4 prosent av veljarunum.",
         [
-            "Sambandsflokkurin",
-            "gongur",
-            "aftur",
-            "við",
-            "2,7",
-            "prosentum",
-            "í",
-            "mun",
-            "til",
-            "valið",
-            "í",
-            "1994",
-            ",",
-            "tá",
-            "flokkurin",
-            "fekk",
-            "undirtøku",
-            "frá",
-            "23,4",
-            "prosent",
-            "av",
-            "veljarunum",
-            ".",
+            "Sambandsflokkurin", "gongur", "aftur", "við", "2,7", "prosentum", "í", "mun", "til", "valið", "í", "1994", ",", "tá", "flokkurin", "fekk", "undirtøku", "frá", "23,4", "prosent", "av", "veljarunum", ".",
         ],
     ),
 ]
+# fmt: on
 
 
 @pytest.mark.parametrize("text,expected_tokens", FO_TOKEN_EXCEPTION_TESTS)
