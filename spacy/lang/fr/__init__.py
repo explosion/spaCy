ef4c65598a0d28a67fa2f0ba6c08b75e3b295ede
thjbbvlt <thibault2@ik.me>
2024-03-15 11:55:27 +0100

    works modifié :         __init__.py modifié :         punctuation.py modifié :         tokenizer_exceptions.py

--- spacy/lang/fr/__init__.py
+++ spacy/lang/fr/__init__.py
@@ -5,10 +5,14 @@ from thinc.api import Model
 from ...language import BaseDefaults, Language
 from .lemmatizer import FrenchLemmatizer
 from .lex_attrs import LEX_ATTRS
-from .punctuation import TOKENIZER_INFIXES, TOKENIZER_PREFIXES, TOKENIZER_SUFFIXES
+from .punctuation import (
+    TOKENIZER_INFIXES,
+    TOKENIZER_PREFIXES,
+    TOKENIZER_SUFFIXES,
+)
 from .stop_words import STOP_WORDS
 from .syntax_iterators import SYNTAX_ITERATORS
-from .tokenizer_exceptions import TOKEN_MATCH, TOKENIZER_EXCEPTIONS
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 
 
 class FrenchDefaults(BaseDefaults):
@@ -16,7 +20,6 @@ class FrenchDefaults(BaseDefaults):
     prefixes = TOKENIZER_PREFIXES
     infixes = TOKENIZER_INFIXES
     suffixes = TOKENIZER_SUFFIXES
-    token_match = TOKEN_MATCH
     lex_attr_getters = LEX_ATTRS
     syntax_iterators = SYNTAX_ITERATORS
     stop_words = STOP_WORDS
@@ -47,7 +50,12 @@ def make_lemmatizer(
     scorer: Optional[Callable],
 ):
     return FrenchLemmatizer(
-        nlp.vocab, model, name, mode=mode, overwrite=overwrite, scorer=scorer
+        nlp.vocab,
+        model,
+        name,
+        mode=mode,
+        overwrite=overwrite,
+        scorer=scorer,
     )
 
 
--- spacy/lang/fr/tokenizer_exceptions.py
+++ spacy/lang/fr/tokenizer_exceptions.py
@@ -1,36 +1,19 @@
-import re
+from ...util import update_exc
+from ...symbols import NORM, ORTH
+from ..tokenizer_exceptions import BASE_EXCEPTIONS
 
-_hyphen = "-–—"
-_apostrophe = "'`´’"
 
-# fmt: off
-_suffix_inversion = r"|".join([
-    "je", "tu", "on", "il", "elle", "iel",
-    "nous", "vous", "elles", "ils", "iels",
-    # écoutons-les
-    "moi", "toi", "lui", "leur",
-    "eux",
-    fr"en(?![{_hyphen}])", "y",
-    # écoutons-les
-    fr"la(?![{_hyphen}])", fr"le(?![{_hyphen}])", fr"les(?![{_hyphen}])",
-    # a-t-il, pourra-t'on, dis-m'en plus
-    fr"t[{_hyphen}]??[{_apostrophe}]?", fr"m[{_apostrophe}]?",
-    "là", "ici",
-])
-_prefix_elision = r"|".join([
-    "n", "s", "c", "d", "j", "m", "t", "l", "qu",
-    # i exclude "quelqu'un"/"quelqu'un" because it's one token (and not quelque + un, which lack the idea of 'one person').
-    fr"quelqu(?![{_apostrophe}]un[ex]*\b)",  # quelque
-    "jusqu", "presqu", "lorsqu", "puisqu", "quoiqu",
-])
-# fmt: on
+_exc = {
+    "St": [{ORTH: "St", NORM: "Saint"}],
+    "Ste": [{ORTH: "Ste", NORM: "Sainte"}],
+    "Mme": [{ORTH: "Mme", NORM: "Madame"}],
+    "Mr.": [{ORTH: "Mr", NORM: "Monsieur"}],
+    "M.": [{ORTH: "M.", NORM: "Monsieur"}],
+    "Mlle": [{ORTH: "Mlle", NORM: "Mademoiselle"}],
+    "Dr": [{ORTH: "Dr", NORM: "Docteur"}],
+    "Dresse": [{ORTH: "Dresse", NORM: "Doctoresse"}],
+    "Drsse": [{ORTH: "Drsse", NORM: "Doctoresse"}],
+    "etc": [{ORTH: "etc", NORM: "etcaetera"}],
+}
 
-_elision = rf"(?:\b(?:{_prefix_elision})[{_apostrophe}])"
-_inversion = (
-    rf"(?:(?<=[^\W\d])[{_hyphen}]\b(?:{_suffix_inversion})\b)"
-)
-
-TOKEN_MATCH = re.compile(
-    r"(?iu)" + r"|".join([_inversion, _elision])
-)
-# _abbrevs = ["ste?", "mme", "mr?", "mlle", "dr", "etc", "cf"]
+TOKENIZER_EXCEPTIONS = update_exc(BASE_EXCEPTIONS, _exc)
