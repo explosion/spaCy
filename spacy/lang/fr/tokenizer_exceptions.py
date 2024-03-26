54fcc5bba2e7eacfc7882082dbc5770e8968e018
thjbbvlt <thibault2@ik.me>
2024-03-26 11:25:43 +0100

    add dot after some abbrevs

--- spacy/lang/fr/tokenizer_exceptions.py
+++ spacy/lang/fr/tokenizer_exceptions.py
@@ -4,15 +4,19 @@ from ..tokenizer_exceptions import BASE_EXCEPTIONS
 
 _exc = {
     "St": [{ORTH: "St", NORM: "Saint"}],
+    "St.": [{ORTH: "St.", NORM: "Saint"}],
     "Ste": [{ORTH: "Ste", NORM: "Sainte"}],
     "Mme": [{ORTH: "Mme", NORM: "Madame"}],
     "Mr": [{ORTH: "Mr", NORM: "Monsieur"}],
+    "Mr.": [{ORTH: "Mr.", NORM: "Monsieur"}],
     "M.": [{ORTH: "M.", NORM: "Monsieur"}],
     "Mlle": [{ORTH: "Mlle", NORM: "Mademoiselle"}],
     "Dr": [{ORTH: "Dr", NORM: "Docteur"}],
+    "Dr.": [{ORTH: "Dr.", NORM: "Docteur"}],
     "Dresse": [{ORTH: "Dresse", NORM: "Doctoresse"}],
     "Drsse": [{ORTH: "Drsse", NORM: "Doctoresse"}],
     "etc": [{ORTH: "etc", NORM: "etcaetera"}],
+    "etc.": [{ORTH: "etc.", NORM: "etcaetera"}],
     # months
     "jan.": [{ORTH: "jan.", NORM: "janvier"}],
     "janv.": [{ORTH: "janv.", NORM: "janvier"}],
