3c3d75015b6fac86d9c2161479fa77af61eec13c
Matthew Honnibal <honnibal+gh@gmail.com>
2024-09-14 11:27:32 +0200

    Set version to v3.7.7

--- spacy/about.py
+++ spacy/about.py
@@ -1,5 +1,5 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "3.9.0"
+__version__ = "3.7.7"
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
