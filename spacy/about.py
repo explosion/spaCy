d08f4e3b10f701e528dd933eff73ca6fb2ac3f90
Matthew Honnibal <matt@explosion.ai>
2025-05-22 13:58:00 +0200

    Increment version

--- spacy/about.py
+++ spacy/about.py
@@ -1,5 +1,5 @@
 # fmt: off
 __title__ = "spacy"
-__version__ = "3.8.6"
+__version__ = "3.8.7"
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
